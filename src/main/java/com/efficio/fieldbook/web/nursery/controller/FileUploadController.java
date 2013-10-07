d24af4a4f06cc9ccd7c27779fd5ccb0a62a51b6e
chezka <chezka@efficio.us.com>
2013-10-07 17:03:16 +0800

    GCP-5784

--- src/main/java/com/efficio/fieldbook/web/nursery/controller/FileUploadController.java
+++ src/main/java/com/efficio/fieldbook/web/nursery/controller/FileUploadController.java
@@ -28,13 +28,14 @@ import com.efficio.fieldbook.web.nursery.form.FileUploadForm;
 import com.efficio.fieldbook.web.AbstractBaseFieldbookController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 
 @Controller
 @RequestMapping({"/NurseryManager", FileUploadController.URL})
 public class FileUploadController extends AbstractBaseFieldbookController{
 
-    public static final String URL = "/fileUpload";
+    public static final String URL = "/NurseryManager/fileUpload";
     
     @Resource
     private FieldbookService fieldbookService;
@@ -46,12 +47,13 @@ public class FileUploadController extends AbstractBaseFieldbookController{
     private DataImportService dataImportService;
 	
     @RequestMapping(method = RequestMethod.GET)
-    public String show(@ModelAttribute("fileUploadForm") FileUploadForm uploadForm, Model model) {
+    public String show(@ModelAttribute("fileUploadForm") FileUploadForm uploadForm, Model model, HttpSession session) {
+    	session.invalidate();
     	return super.show(model);
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String uploadFile(@ModelAttribute("fileUploadForm") FileUploadForm uploadForm, BindingResult result, Model model) {
+    public String uploadFile(@ModelAttribute("fileUploadForm") FileUploadForm uploadForm, BindingResult result, Model model, HttpSession session) {
     	FileUploadFormValidator validator = new FileUploadFormValidator();
         validator.validate(uploadForm, result);
 
@@ -59,19 +61,20 @@ public class FileUploadController extends AbstractBaseFieldbookController{
             /**
              * Return the user back to form to show errors
              */
-        	return show(uploadForm,model);
+        	return show(uploadForm,model,session);
         } else {
 
 
             try {
             	String tempFileName = fieldbookService.storeUserWorkbook(uploadForm.getFile().getInputStream());
-            	uploadForm.setFileName(tempFileName);
+            	userSelection.setServerFileName(tempFileName);
+                userSelection.setActualFileName(uploadForm.getFile().getOriginalFilename());
             } catch (IOException e) {
                 e.printStackTrace();
                 result.reject("uploadForm.file", "Error occurred while uploading file.");
             }
             
-            return show(uploadForm,model);
+            return "redirect:" + NurseryDetailsController.URL;
         }
     }
     

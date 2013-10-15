02ab26099e10c901c4c82ecd8a9c2d700798fb22
daniel@efficio.us.com <daniel@efficio.us.com>
2013-10-15 18:04:53 +0800

    added common js for the movement of pagnation

---
+++ src/main/webapp/WEB-INF/static/js/fieldbook-common.js
@@ -0,0 +1,15 @@
+function showPage(paginationUrl, pageNum, sectionDiv){
+	//$('#imported-germplasm-list').html(pageNum); 	
+	Spinner.toggle();
+ 	$.ajax(
+         { url: paginationUrl+pageNum,
+           type: "GET",
+           data: "",
+           success: function(html) {
+        	   
+             $("#"+sectionDiv).empty().append(html);
+             Spinner.toggle();  
+           }
+         }
+       );
+}

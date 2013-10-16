5ffe337e56b958bd0e489106c4b073990ce28c83
chezka <chezka@efficio.us.com>
2013-10-16 17:56:28 +0800

    GCP-5842

--- src/test/java/com/efficio/fieldbook/web/nursery/controller/ManageNurseriesControllerTest.java
+++ src/test/java/com/efficio/fieldbook/web/nursery/controller/ManageNurseriesControllerTest.java
@@ -55,11 +55,17 @@ public class ManageNurseriesControllerTest extends AbstractJUnit4SpringContextTe
 	    } catch (MiddlewareQueryException e) {
 	        LOG.error(e.getMessage(), e);
 	    }
-            
-            form.setCurrentPage(1);            
-            assertEquals(form.getPaginatedNurseryDetailsList().size(), form.getResultPerPage());            
-            
-            form.setCurrentPage(2);
-            assertEquals(form.getPaginatedNurseryDetailsList().size(), form.getNurseryDetailsList().size()-10);
+	    
+            if (form.getNurseryDetailsList().size() > 0) {
+                form.setCurrentPage(1);
+                if (form.getNurseryDetailsList().size() > 10) {
+                    assertEquals(form.getPaginatedNurseryDetailsList().size(), form.getResultPerPage());
+                }
+            }
+                        
+            if (form.getNurseryDetailsList().size() > 10 && form.getNurseryDetailsList().size() < 20) {
+                form.setCurrentPage(2);
+                assertEquals(form.getPaginatedNurseryDetailsList().size(), form.getNurseryDetailsList().size()-10);
+            }
 	}
 }

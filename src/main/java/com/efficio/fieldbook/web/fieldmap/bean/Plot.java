23499f23dacce37c256250e5d0180b4eb749b1f5
daniel@efficio.us.com <daniel@efficio.us.com>
2013-11-15 14:19:58 +0800

    addeded comments

--- src/main/java/com/efficio/fieldbook/web/fieldmap/bean/Plot.java
+++ src/main/java/com/efficio/fieldbook/web/fieldmap/bean/Plot.java
@@ -15,19 +15,41 @@ import java.io.Serializable;
 
 
 /**
- * @author Efficio.Daniel
+ * The Class Plot.  This would be use track the plots 
  *
+ * @author Efficio.Daniel
  */
 public class Plot implements Serializable{
+    
+    /** The range. */
     private int range;
+    
+    /** The column. */
     private int column;
+    
+    /** The is upward. */
     private boolean isUpward;
+    
+    /** The display string. */
     private String displayString;
+    
+    /** The is plot deleted. */
     private boolean isPlotDeleted;
+    
+    /** The is not started. */
     private boolean isNotStarted;
+    
+    /** The is no more entries. */
     private boolean isNoMoreEntries;
     
 
+    /**
+     * Instantiates a new plot.
+     *
+     * @param column the column
+     * @param range the range
+     * @param displayString the display string
+     */
     public Plot( int column, int range, String displayString){
         setRange(range);
         setColumn(column);
@@ -36,70 +58,142 @@ public class Plot implements Serializable{
 
     
     
+    /**
+     * Checks if is plot deleted.
+     *
+     * @return true, if is plot deleted
+     */
     public boolean isPlotDeleted() {
         return isPlotDeleted;
     }
 
 
     
+    /**
+     * Sets the plot deleted.
+     *
+     * @param isPlotDeleted the new plot deleted
+     */
     public void setPlotDeleted(boolean isPlotDeleted) {
         this.isPlotDeleted = isPlotDeleted;
     }
 
 
     
+    /**
+     * Checks if is not started.
+     *
+     * @return true, if is not started
+     */
     public boolean isNotStarted() {
         return isNotStarted;
     }
 
 
     
+    /**
+     * Sets the not started.
+     *
+     * @param isNotStarted the new not started
+     */
     public void setNotStarted(boolean isNotStarted) {
         this.isNotStarted = isNotStarted;
     }
 
 
     
+    /**
+     * Checks if is no more entries.
+     *
+     * @return true, if is no more entries
+     */
     public boolean isNoMoreEntries() {
         return isNoMoreEntries;
     }
 
 
     
+    /**
+     * Sets the no more entries.
+     *
+     * @param isNoMoreEntries the new no more entries
+     */
     public void setNoMoreEntries(boolean isNoMoreEntries) {
         this.isNoMoreEntries = isNoMoreEntries;
     }
 
 
+    /**
+     * Gets the display string.
+     *
+     * @return the display string
+     */
     public String getDisplayString() {
         return displayString;
     }
 
+    /**
+     * Sets the display string.
+     *
+     * @param displayString the new display string
+     */
     public void setDisplayString(String displayString) {
         this.displayString = displayString;
     }
 
+    /**
+     * Checks if is upward.
+     *
+     * @return true, if is upward
+     */
     public boolean isUpward() {
         return isUpward;
     }
 
+    /**
+     * Sets the upward.
+     *
+     * @param upward the new upward
+     */
     public void setUpward(boolean upward) {
         isUpward = upward;
     }
 
+    /**
+     * Gets the range.
+     *
+     * @return the range
+     */
     public int getRange() {
         return range;
     }
 
+    /**
+     * Sets the range.
+     *
+     * @param range the new range
+     */
     public void setRange(int range) {
         this.range = range;
     }
 
+    /**
+     * Gets the column.
+     *
+     * @return the column
+     */
     public int getColumn() {
         return column;
     }
 
+    /**
+     * Sets the column.
+     *
+     * @param column the new column
+     */
     public void setColumn(int column) {
         this.column = column;
     }
+    
+    
 }

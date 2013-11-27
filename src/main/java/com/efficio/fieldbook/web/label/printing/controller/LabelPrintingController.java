e971f6ac2ae7a98cf1dd586b7969407b00969234
chezka <chezka@efficio.us.com>
2013-11-27 12:10:09 +0800

    GCP-6323, GCP-6324 initial UI changes

---
+++ src/main/java/com/efficio/fieldbook/web/label/printing/controller/LabelPrintingController.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2013, All Rights Reserved.
+ * 
+ * Generation Challenge Programme (GCP)
+ * 
+ * 
+ * This software is licensed for use under the terms of the GNU General Public
+ * License (http://bit.ly/8Ztv8M) and the provisions of Part F of the Generation
+ * Challenge Programme Amended Consortium Agreement (http://bit.ly/KQX1nL)
+ * 
+ *******************************************************************************/
+package com.efficio.fieldbook.web.label.printing.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.efficio.fieldbook.web.AbstractBaseFieldbookController;
+import com.efficio.fieldbook.web.label.printing.form.LabelPrintingForm;
+
+
+@Controller
+@RequestMapping({LabelPrintingController.URL})
+public class LabelPrintingController extends AbstractBaseFieldbookController{
+ 
+     /** The Constant LOG. */
+     private static final Logger LOG = LoggerFactory.getLogger(LabelPrintingController.class);
+    
+    /** The Constant URL. */
+    public static final String URL = "/LabelPrinting/specifyLabelDetails";
+    
+    @RequestMapping(value="/trial/{id}", method = RequestMethod.GET)
+    public String showTrialLabelDetails(@ModelAttribute("labelPrintingForm") LabelPrintingForm form, Model model) {
+        return super.show(model);
+    }
+    
+    @RequestMapping(value="/nursery/{id}", method = RequestMethod.GET)
+    public String showNurseryLabelDetails(@ModelAttribute("labelPrintingForm") LabelPrintingForm form, Model model) {
+        return super.show(model);
+    }
+    
+    
+    /**
+     * Submits the details.
+     *
+     * @param form the form
+     * @param result the result
+     * @param model the model
+     * @return the string
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public String submitDetails(@ModelAttribute("labelPrintingForm") LabelPrintingForm form, BindingResult result, Model model) {
+        return "redirect:" + LabelPrintingController.URL;
+    } 
+    
+    /* (non-Javadoc)
+     * @see com.efficio.fieldbook.web.AbstractBaseFieldbookController#getContentName()
+     */
+    @Override
+    public String getContentName() {
+        return "LabelPrinting/specifyLabelDetails";
+    }
+    
+}

ba84c3c6edc53ccd0888e79f01bc26440ba5642d
chezka <chezka@efficio.us.com>
2014-01-28 15:43:10 +0800

    GCP-7302

--- src/main/java/com/efficio/fieldbook/web/nursery/controller/NurseryDetailsController.java
+++ src/main/java/com/efficio/fieldbook/web/nursery/controller/NurseryDetailsController.java
@@ -12,19 +12,29 @@
 package com.efficio.fieldbook.web.nursery.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import org.codehaus.jackson.map.ObjectMapper;
+import org.generationcp.middleware.domain.dms.StandardVariable;
 import org.generationcp.middleware.domain.etl.MeasurementVariable;
 import org.generationcp.middleware.domain.etl.Workbook;
+import org.generationcp.middleware.exceptions.MiddlewareQueryException;
+import org.generationcp.middleware.service.api.OntologyService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.efficio.fieldbook.web.nursery.bean.UserSelection;
 import com.efficio.fieldbook.web.nursery.form.NurseryDetailsForm;
@@ -39,6 +49,13 @@ public class NurseryDetailsController extends AbstractBaseFieldbookController{
     
     /** The Constant URL. */
     public static final String URL = "/NurseryManager/nurseryDetails";
+    
+    /** The Constant LOG. */
+    private static final Logger LOG = LoggerFactory.getLogger(NurseryDetailsController.class);
+    
+    /** The ontology service. */
+    @Resource
+    private OntologyService ontologyService;
     
     /** The user selection. */
     @Resource
@@ -97,6 +114,56 @@ public class NurseryDetailsController extends AbstractBaseFieldbookController{
         
         form.setWorkbook(userSelection.getWorkbook());
     	return super.show(model);
+    }
+    
+    @ResponseBody
+    @RequestMapping(value="/showPlantHeightInfo/{id}", method = RequestMethod.GET)
+    public Map<String, String> getVariableDetails(@PathVariable String id) {
+        Map<String, String> result = new HashMap<String, String>();
+        try {
+            if (isNumeric(id)) {
+                result.put("stdVar", convertStandardVariableToJson(ontologyService.getStandardVariable(Integer.parseInt(id))));
+            } else {
+                //this part should be commented out when id is already used
+                List<StandardVariable> stdVariables = ontologyService.getStandardVariables(id);
+                
+                if (stdVariables != null & stdVariables.size() > 0) {
+                    result.put("stdVar", convertStandardVariableToJson(stdVariables.get(0)));
+                } else {
+                    result.put("stdVar", null);
+                }
+            }
+            result.put("error", "0");
+        } catch(MiddlewareQueryException e) {
+           result.put("error", "1"); 
+           LOG.error(e.getMessage(), e);
+        }
+        return result;
+    }
+    
+    private static String convertStandardVariableToJson(StandardVariable stdVariable) {
+        if (stdVariable != null) {
+            try {
+                ObjectMapper mapper = new ObjectMapper();
+                return mapper.writeValueAsString(stdVariable);
+            } catch(Exception e) {
+                LOG.error(e.getMessage(), e);
+            }
+        }
+        return "";
+    }
+    
+    private static boolean isNumeric(String str)  
+    {  
+      try  
+      {  
+        Double.parseDouble(str);  
+      }  
+      catch(NumberFormatException nfe)  
+      {  
+        return false;  
+      }  
+      return true;  
     }
 
     /**

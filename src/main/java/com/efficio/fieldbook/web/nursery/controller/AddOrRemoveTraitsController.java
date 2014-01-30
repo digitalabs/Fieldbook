1674a23efc6abe4b58d955c379466c1442ca4b83
daniel@efficio.us.com <daniel@efficio.us.com>
2014-01-30 17:38:37 +0800

    GCP-7317

--- src/main/java/com/efficio/fieldbook/web/nursery/controller/AddOrRemoveTraitsController.java
+++ src/main/java/com/efficio/fieldbook/web/nursery/controller/AddOrRemoveTraitsController.java
@@ -17,10 +17,12 @@ import java.util.List;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import org.generationcp.middleware.domain.dms.PhenotypicType;
 import org.generationcp.middleware.domain.etl.MeasurementData;
 import org.generationcp.middleware.domain.etl.MeasurementRow;
 import org.generationcp.middleware.domain.etl.MeasurementVariable;
 import org.generationcp.middleware.domain.etl.Workbook;
+import org.generationcp.middleware.domain.oms.TermId;
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.service.api.FieldbookService;
 import org.springframework.stereotype.Controller;
@@ -36,6 +38,7 @@ import com.efficio.fieldbook.web.nursery.form.AddOrRemoveTraitsForm;
 import com.efficio.fieldbook.web.nursery.form.ImportGermplasmListForm;
 import com.efficio.fieldbook.web.nursery.service.impl.ImportGermplasmFileServiceImpl;
 import com.efficio.fieldbook.web.AbstractBaseFieldbookController;
+import com.vaadin.terminal.Terminal;
 
 /**
  * The Class AddOrRemoveTraitsController.
@@ -85,43 +88,66 @@ public class AddOrRemoveTraitsController extends AbstractBaseFieldbookController
     		
     		for(MeasurementVariable var : getUserSelection().getWorkbook().getMeasurementDatasetVariables()){
     			MeasurementData measurementData =null;
-    			var.setFactor(true);    			
-    			if (var.getName().equalsIgnoreCase(ImportGermplasmFileServiceImpl.FACTOR_ENTRY)) {
-    				measurementData = new MeasurementData(var.getName(), Integer.toString(index), false, var.getDataType());
-                } else if (var.getName().equalsIgnoreCase(ImportGermplasmFileServiceImpl.FACTOR_DESIGNATION)
-                		|| var.getName().equalsIgnoreCase(ImportGermplasmFileServiceImpl.FACTOR_DESIG)) {
-                	measurementData = new MeasurementData(var.getName(), germplasm.getDesig(), false, var.getDataType());                	
-                } else if (var.getName().equalsIgnoreCase(ImportGermplasmFileServiceImpl.FACTOR_GID)) {
-                	//we need to check first if the germplasm is existing or not
-                	Integer dbGid = fieldbookMiddlewareService.getGermplasmIdByName(germplasm.getDesig());
-                	Integer gidToBeUse = null;
-                	if(dbGid == null){
-                		newGid--;
-                		gidToBeUse = Integer.valueOf(newGid);
-                	}else{
-                		gidToBeUse = dbGid;
-                	}
-                	
-                	
-                	measurementData = new MeasurementData(var.getName(), gidToBeUse.toString(), false, var.getDataType());                	
-                } else if (var.getName().equalsIgnoreCase(ImportGermplasmFileServiceImpl.FACTOR_CROSS)) {
-                	measurementData = new MeasurementData(var.getName(), "", false, var.getDataType());                	
-                } else if (var.getName().equalsIgnoreCase(ImportGermplasmFileServiceImpl.FACTOR_SOURCE)) {
-                	measurementData = new MeasurementData(var.getName(), "", false, var.getDataType());
-                } else if (var.getName().equalsIgnoreCase(ImportGermplasmFileServiceImpl.FACTOR_ENTRY_CODE)) {
-                	measurementData = new MeasurementData(var.getName(), Integer.toString(index), false, var.getDataType());
-                } else if (var.getName().equalsIgnoreCase(ImportGermplasmFileServiceImpl.FACTOR_PLOT)) {
-                	measurementData = new MeasurementData(var.getName(), Integer.toString(index), false, var.getDataType());
-                } else if (var.getName().equalsIgnoreCase(ImportGermplasmFileServiceImpl.FACTOR_CHECK)) {
-                	measurementData = new MeasurementData(var.getName(), germplasm.getCheck(), false, var.getDataType());
-                }
-                else{
-                	//meaning non factor
-                	measurementData = new MeasurementData(var.getName(), "", true, var.getDataType());
-                	//measurementData.setEditable(true);
+    			
+    			
+    			Integer termId = fieldbookMiddlewareService.getStandardVariableIdByPropertyScaleMethodRole(var.getProperty(), var.getScale(), var.getMethod(), PhenotypicType.getPhenotypicTypeForLabel(var.getLabel()));
+    			
+    			
+    			var.setFactor(true);    
+    			
+    			if(termId == null){
+    				//we default if null, but should not happen
+    				measurementData = new MeasurementData(var.getName(), "", true, var.getDataType());
                 	var.setFactor(false);
-                	//measurementData.set
-                }
+                	measurementData.setEditable(true);
+    			}else{
+    				
+	    			
+	    				if(termId.intValue() == TermId.ENTRY_NO.getId())
+	    					measurementData = new MeasurementData(var.getName(), Integer.toString(index), false, var.getDataType());
+	    				else if(termId.intValue() == TermId.SOURCE.getId())
+	    					measurementData = new MeasurementData(var.getName(), "", false, var.getDataType());
+	    				else if(termId.intValue() == TermId.CROSS.getId())	
+	    					measurementData = new MeasurementData(var.getName(), germplasm.getCross(), false, var.getDataType());
+	    				else if(termId.intValue() == TermId.DESIG.getId())	
+	    					measurementData = new MeasurementData(var.getName(), germplasm.getDesig(), false, var.getDataType());
+	    				else if(termId.intValue() == TermId.GID.getId()){	    					
+	    					//we need to check first if the germplasm is existing or not
+	                    	Integer dbGid = fieldbookMiddlewareService.getGermplasmIdByName(germplasm.getDesig());
+	                    	Integer gidToBeUse = null;
+	                    	if(dbGid == null){
+	                    		newGid--;
+	                    		gidToBeUse = Integer.valueOf(newGid);
+	                    	}else{
+	                    		gidToBeUse = dbGid;
+	                    	}
+	                    	
+	                    	
+	                    	measurementData = new MeasurementData(var.getName(), gidToBeUse.toString(), false, var.getDataType());
+	                    	
+	    				}else if(termId.intValue() == TermId.ENTRY_CODE.getId())	    					
+	    					measurementData = new MeasurementData(var.getName(), germplasm.getEntryCode(), false, var.getDataType());
+	    				else if(termId.intValue() == TermId.PLOT_NO.getId())
+	    					measurementData = new MeasurementData(var.getName(), Integer.toString(index), false, var.getDataType());
+	    				else if(termId.intValue() == TermId.CHECK.getId()){
+	    					
+	    				/*
+	    				 * NESTED_PLOT FOR TRIAL ONLY
+	    				 */
+	    				
+	    					measurementData = new MeasurementData(var.getName(), germplasm.getCheck(), false, var.getDataType());
+	    				}else{
+	    					//meaning non factor
+	                    	measurementData = new MeasurementData(var.getName(), "", true, var.getDataType());
+	                    	//measurementData.setEditable(true);
+	                    	var.setFactor(false);
+	                    	//measurementData.set
+	    					
+	    				}
+	    			}
+    			
+    						
+    			
     			dataList.add(measurementData);
     		}
     		measurementRow.setDataList(dataList);
@@ -136,7 +162,8 @@ public class AddOrRemoveTraitsController extends AbstractBaseFieldbookController
     	*/
     	return super.show(model);
     }
-
+    
+    
     /**
      * Show details.
      *

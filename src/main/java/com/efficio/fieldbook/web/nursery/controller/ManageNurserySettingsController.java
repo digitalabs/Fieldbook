b98a63f8cd23eae11f27783ad9b418a2252ff921
chezka <chezka@efficio.us.com>
2014-02-13 13:22:18 +0800

    GCP-7478 - initial UI changes

---
+++ src/main/java/com/efficio/fieldbook/web/nursery/controller/ManageNurserySettingsController.java
@@ -0,0 +1,102 @@
+/*******************************************************************************
+ * Copyright (c) 2013, All Rights Reserved.
+ * 
+ * Generation Challenge Programme (GCP)
+ * 
+ * 
+ * This software is licensed for use under the terms of the GNU General Public
+ * License (http://bit.ly/8Ztv8M) and the provisions of Part F of the Generation
+ * Challenge Programme Amended Consortium Agreement (http://bit.ly/KQX1nL)
+ * 
+ *******************************************************************************/
+package com.efficio.fieldbook.web.nursery.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang3.StringUtils;
+import org.generationcp.middleware.exceptions.MiddlewareQueryException;
+import org.generationcp.middleware.manager.GermplasmNameType;
+import org.generationcp.middleware.manager.api.WorkbenchDataManager;
+import org.generationcp.middleware.pojos.Germplasm;
+import org.generationcp.middleware.pojos.GermplasmList;
+import org.generationcp.middleware.pojos.GermplasmListData;
+import org.generationcp.middleware.pojos.Name;
+import org.generationcp.middleware.service.api.FieldbookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.efficio.fieldbook.web.nursery.bean.AdvancingNursery;
+import com.efficio.fieldbook.web.nursery.bean.ImportedGermplasm;
+import com.efficio.fieldbook.web.nursery.form.AdvancingNurseryForm;
+import com.efficio.fieldbook.web.util.AppConstants;
+import com.efficio.fieldbook.web.AbstractBaseFieldbookController;
+
+/**
+ * The Class SaveAdvanceNurseryController.
+ */
+@Controller
+@RequestMapping(ManageNurserySettingsController.URL)
+public class ManageNurserySettingsController extends AbstractBaseFieldbookController{
+
+    /** The Constant URL. */
+    public static final String URL = "/NurseryManager/manageNurserySettings";
+    
+    /** The Constant LOG. */
+    private static final Logger LOG = LoggerFactory.getLogger(ManageNurserySettingsController.class);
+         
+    /* (non-Javadoc)
+     * @see com.efficio.fieldbook.web.AbstractBaseFieldbookController#getContentName()
+     */
+    @Override
+    public String getContentName() {
+        return "NurseryManager/manageNurserySettings";
+    }    
+    
+    /**
+     * Shows the screen.
+     *
+     * @param form the form
+     * @param model the model
+     * @param session the session
+     * @return the string
+     * @throws MiddlewareQueryException the middleware query exception
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public String show(@ModelAttribute("advancingNurseryform") AdvancingNurseryForm form
+            , Model model, HttpSession session) throws MiddlewareQueryException{
+    	return super.show(model);
+    }
+          
+    /**
+     * Post advance nursery.
+     *
+     * @param form the form
+     * @param model the model
+     * @param session the session
+     * @return the map
+     * @throws MiddlewareQueryException the middleware query exception
+     */
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST)
+    public String postAdvanceNursery(@ModelAttribute("advancingNurseryform") AdvancingNurseryForm form
+            , Model model, HttpSession session) throws MiddlewareQueryException{
+
+        return super.show(model);
+    }
+    
+}

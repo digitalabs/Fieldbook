a2178fc9611497c69394d4f3e68c946e8a0a47e3
Daniel Jao <daniel@efficio.us.com>
2014-03-06 15:38:52 +0800

    fix the required fields

--- src/main/java/com/efficio/fieldbook/web/nursery/controller/ManageNurserySettingsController.java
+++ src/main/java/com/efficio/fieldbook/web/nursery/controller/ManageNurserySettingsController.java
@@ -17,6 +17,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
@@ -304,21 +305,30 @@ public class ManageNurserySettingsController extends AbstractBaseFieldbookContro
     
     private List<Integer> buildRequiredFactors() {
         List<Integer> requiredFactors = new ArrayList<Integer>();
-        requiredFactors.add(TermId.TRIAL_LOCATION.getId());
-        requiredFactors.add(TermId.PI_NAME.getId());
-        requiredFactors.add(TermId.STUDY_NAME.getId());
-        requiredFactors.add(TermId.STUDY_TITLE.getId());
-        requiredFactors.add(TermId.STUDY_OBJECTIVE.getId());
+        String createNurseryRequiredFields = AppConstants.CREATE_NURSERY_REQUIRED_FIELDS.getString();
+        StringTokenizer token = new StringTokenizer(createNurseryRequiredFields, ",");
+        while(token.hasMoreTokens()){
+        	requiredFactors.add(Integer.valueOf(token.nextToken()));
+        }        
         return requiredFactors;
     }
     
     private List<String> buildRequiredFactorsLabel() {
+    	
         List<String> requiredFactors = new ArrayList<String>();
+        /*
         requiredFactors.add(AppConstants.LOCATION.getString());
         requiredFactors.add(AppConstants.PRINCIPAL_INVESTIGATOR.getString());
         requiredFactors.add(AppConstants.STUDY_NAME.getString());
         requiredFactors.add(AppConstants.STUDY_TITLE.getString());
         requiredFactors.add(AppConstants.OBJECTIVE.getString());
+        */
+        String createNurseryRequiredFields = AppConstants.CREATE_NURSERY_REQUIRED_FIELDS.getString();
+        StringTokenizer token = new StringTokenizer(createNurseryRequiredFields, ",");
+        while(token.hasMoreTokens()){
+        	requiredFactors.add(AppConstants.getString(token.nextToken() + AppConstants.LABEL.getString()));
+        }        
+        
         return requiredFactors;
     }
 

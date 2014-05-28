462f99d5f3d6fd854f95cf8b35f7dac1e9634861
Naymesh Mistry <naymesh@gmail.com>
2014-05-28 14:23:00 +1200

    Cleaned up ManageNurseriesController to remove obsolete code related to the old paginated view. Cleaned up the ManageNurseriesControllerTest as well. Moved the test for Dataset/Workbook xml conversion to where it belongs (SettingsUtilTest).
    Reviewer: Daniel J
    Issue: none

--- src/main/java/com/efficio/fieldbook/web/nursery/controller/ManageNurseriesController.java
+++ src/main/java/com/efficio/fieldbook/web/nursery/controller/ManageNurseriesController.java
@@ -11,93 +11,26 @@
  *******************************************************************************/
 package com.efficio.fieldbook.web.nursery.controller;
 
-import java.util.List;
-
-import javax.annotation.Resource;
-
-import org.generationcp.middleware.domain.etl.StudyDetails;
-import org.generationcp.middleware.exceptions.MiddlewareQueryException;
-import org.generationcp.middleware.service.api.FieldbookService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.efficio.fieldbook.web.AbstractBaseFieldbookController;
-import com.efficio.fieldbook.web.nursery.bean.UserSelection;
-import com.efficio.fieldbook.web.nursery.form.ManageNurseriesForm;
 
-/**
- * The Class ManageNurseriesController.
- */
 @Controller
-@RequestMapping({"/NurseryManager", ManageNurseriesController.URL})
-public class ManageNurseriesController extends AbstractBaseFieldbookController{
-    
-    private static final Logger LOG = LoggerFactory.getLogger(ManageNurseriesController.class);
-    
-    /** The Constant URL. */
-    public static final String URL = "/NurseryManager/manageNurseries";
+@RequestMapping({ "/NurseryManager", ManageNurseriesController.URL })
+public class ManageNurseriesController extends AbstractBaseFieldbookController {
 
-    @Resource
-    private FieldbookService fieldbookMiddlewareService;
-    
-    /** The user selection. */
-    @Resource
-    private UserSelection userSelection;
-    
-    /**
-     * Shows the manage nurseries screen
-     *
-     * @param manageNurseriesForm the manage nurseries form
-     * @param model the model
-     * @param session the session
-     * @return the string
-     */
-    @RequestMapping(method = RequestMethod.GET)
-    public String show(@ModelAttribute("manageNurseriesForm") ManageNurseriesForm form, Model model) {
-        try {
-            List<StudyDetails> nurseryDetailsList = fieldbookMiddlewareService
-                    .getAllLocalNurseryDetails();
-            getUserSelection().setStudyDetailsList(nurseryDetailsList);
-            form.setNurseryDetailsList(getUserSelection().getStudyDetailsList());
-            form.setCurrentPage(1);
-        }
-        catch (MiddlewareQueryException e){
-            LOG.error(e.getMessage(), e);
-        }
-    	return super.show(model);
-    }
-          
-    
-    /* (non-Javadoc)
-     * @see com.efficio.fieldbook.web.AbstractBaseFieldbookController#getContentName()
-     */
-    @Override
-    public String getContentName() {
-        //return "NurseryManager/manageNurseries";
-    	return "NurseryManager/manageNurseries";
-    }
-    
-    /**
-     * Gets the form.
-     *
-     * @return the form
-     */
-    @ModelAttribute("form")
-    public ManageNurseriesForm getForm() {
-        return new ManageNurseriesForm();
-    }
-    
-    /* (non-Javadoc)
-     * @see com.efficio.fieldbook.web.AbstractBaseFieldbookController#getUserSelection()
-     */
-    public UserSelection getUserSelection() {
-        return this.userSelection;
-    }
-    
-   
+	public static final String URL = "/NurseryManager/manageNurseries";
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String show(Model model) {
+		return super.show(model);
+	}
+
+	@Override
+	public String getContentName() {
+		return "NurseryManager/manageNurseries";
+	}
 }
--- src/test/java/com/efficio/fieldbook/web/nursery/controller/ManageNurseriesControllerTest.java
+++ src/test/java/com/efficio/fieldbook/web/nursery/controller/ManageNurseriesControllerTest.java
@@ -11,130 +11,48 @@
  *******************************************************************************/
 package com.efficio.fieldbook.web.nursery.controller;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import junit.framework.Assert;
-
-import org.generationcp.middleware.domain.dms.PhenotypicType;
-import org.generationcp.middleware.domain.dms.ValueReference;
-import org.generationcp.middleware.domain.etl.StudyDetails;
-import org.generationcp.middleware.domain.etl.Workbook;
-import org.generationcp.middleware.domain.oms.TermId;
-import org.generationcp.middleware.exceptions.MiddlewareQueryException;
-import org.generationcp.middleware.pojos.workbench.settings.Condition;
-import org.generationcp.middleware.pojos.workbench.settings.Dataset;
-import org.generationcp.middleware.pojos.workbench.settings.Factor;
-import org.generationcp.middleware.pojos.workbench.settings.Variate;
-import org.generationcp.middleware.service.api.FieldbookService;
-import org.generationcp.middleware.util.Debug;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.ModelAndViewAssert;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
-import com.efficio.fieldbook.web.nursery.form.ManageNurseriesForm;
-import com.efficio.fieldbook.web.util.SettingsUtil;
-
-/**
- * The Class ManageNurseriesControllerTest.
- */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"file:src/test/resources/Fieldbook-servlet-test.xml"})
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/Fieldbook-servlet.xml" })
 public class ManageNurseriesControllerTest extends AbstractJUnit4SpringContextTests {
-	
-    /** The Constant LOG. */
-    private static final Logger LOG = LoggerFactory.getLogger(ManageNurseriesControllerTest.class);
-    
-    /** The fieldbook middleware service. */
-    @Autowired
-    FieldbookService fieldbookMiddlewareService;
 
-    /**
-     * Sets the up.
-     */
-    @Before
-    public void setUp() {
+	@Autowired
+	private RequestMappingHandlerAdapter handleAdapter;
 
-    }
-	
-	/**
-	 * Test manage nurseries pagination.
-	 */
+	@Autowired
+	private RequestMappingHandlerMapping handlerMapping;
+
 	@Test
-	public void testManageNurseriesPagination(){
-	    ManageNurseriesForm form = new ManageNurseriesForm();
-	    
-	    try {    
-            List<StudyDetails> nurseryDetailsList = fieldbookMiddlewareService.getAllLocalNurseryDetails();
-            form.setNurseryDetailsList(nurseryDetailsList);
-	    } catch (MiddlewareQueryException e) {
-	        LOG.error(e.getMessage(), e);
-	    }
-            
-	    if (form.getPaginatedNurseryDetailsList() != null) {
-            if (form.getPaginatedNurseryDetailsList().size() > 0) {
-                form.setCurrentPage(1);
-                if (form.getPaginatedNurseryDetailsList().size() > 10) {
-                    assertEquals(form.getPaginatedNurseryDetailsList().size(), form.getResultPerPage());
-                }
-            }
+	public void testGetReturnsCorrectModelAndView() throws Exception {
 
-            if (form.getPaginatedNurseryDetailsList().size() > 10 
-                    && form.getPaginatedNurseryDetailsList().size() <= 20) {
-                form.setCurrentPage(2);
-                assertEquals(form.getPaginatedNurseryDetailsList().size()
-                        , form.getNurseryDetailsList().size() - 10);
-            }
-	    }
-	}
-	
-	@Test
-	public void testConvertXmlDatasetToWorkbookAndBack() {
-		Dataset dataset = new Dataset();
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		MockHttpSession session = new MockHttpSession();
+		request.setSession(session);
 		
-		dataset.setConditions(new ArrayList<Condition>());
-		dataset.setFactors(new ArrayList<Factor>());
-		dataset.setVariates(new ArrayList<Variate>());
-		
-		dataset.getConditions().add(new Condition("CONDITION1", "CONDITION1", "PERSON", "DBCV", "ASSIGNED", PhenotypicType.STUDY.toString(), "C", "Meeh", null, null, null));
-		dataset.getFactors().add(new Factor("FACTOR1", "FACTOR1", "GERMPLASM ENTRY", "NUMBER", "ENUMERATED", PhenotypicType.GERMPLASM.toString(), "N", 0));
-		dataset.getVariates().add(new Variate("VARIATE1", "VARIATE1", "YIELD (GRAIN)", "Kg/ha", "Paddy Rice", PhenotypicType.VARIATE.toString(), "N", TermId.NUMERIC_VARIABLE.getId(), new ArrayList<ValueReference>(), 0.0, 0.0));
-		
-		Workbook workbook = SettingsUtil.convertXmlDatasetToWorkbook(dataset);
-		Debug.println(0, workbook);
-		
-		Dataset newDataset = (Dataset)SettingsUtil.convertWorkbookToXmlDataset(workbook);
-		Assert.assertEquals(dataset.getConditions().get(0).getName(), newDataset.getConditions().get(0).getName());
-		Assert.assertEquals(dataset.getConditions().get(0).getDescription(), newDataset.getConditions().get(0).getDescription());
-		Assert.assertEquals(dataset.getConditions().get(0).getProperty(), newDataset.getConditions().get(0).getProperty());
-		Assert.assertEquals(dataset.getConditions().get(0).getScale(), newDataset.getConditions().get(0).getScale());
-		Assert.assertEquals(dataset.getConditions().get(0).getMethod(), newDataset.getConditions().get(0).getMethod());
-		Assert.assertEquals(dataset.getConditions().get(0).getRole(), newDataset.getConditions().get(0).getRole());
-		Assert.assertEquals(dataset.getConditions().get(0).getDatatype(), newDataset.getConditions().get(0).getDatatype());
+		request.setRequestURI(ManageNurseriesController.URL);
+		request.setMethod("GET");
 
-		Assert.assertEquals(dataset.getFactors().get(0).getName(), newDataset.getFactors().get(0).getName());
-		Assert.assertEquals(dataset.getFactors().get(0).getDescription(), newDataset.getFactors().get(0).getDescription());
-		Assert.assertEquals(dataset.getFactors().get(0).getProperty(), newDataset.getFactors().get(0).getProperty());
-		Assert.assertEquals(dataset.getFactors().get(0).getScale(), newDataset.getFactors().get(0).getScale());
-		Assert.assertEquals(dataset.getFactors().get(0).getMethod(), newDataset.getFactors().get(0).getMethod());
-		Assert.assertEquals(dataset.getFactors().get(0).getRole(), newDataset.getFactors().get(0).getRole());
-		Assert.assertEquals(dataset.getFactors().get(0).getDatatype(), newDataset.getFactors().get(0).getDatatype());
+		Object handler = handlerMapping.getHandler(request).getHandler();
+		ModelAndView mav = handleAdapter.handle(request, response, handler);
 
-		Assert.assertEquals(dataset.getVariates().get(0).getName(), newDataset.getVariates().get(0).getName());
-		Assert.assertEquals(dataset.getVariates().get(0).getDescription(), newDataset.getVariates().get(0).getDescription());
-		Assert.assertEquals(dataset.getVariates().get(0).getProperty(), newDataset.getVariates().get(0).getProperty());
-		Assert.assertEquals(dataset.getVariates().get(0).getScale(), newDataset.getVariates().get(0).getScale());
-		Assert.assertEquals(dataset.getVariates().get(0).getMethod(), newDataset.getVariates().get(0).getMethod());
-		Assert.assertEquals(dataset.getVariates().get(0).getRole(), newDataset.getVariates().get(0).getRole());
-		Assert.assertEquals(dataset.getVariates().get(0).getDatatype(), newDataset.getVariates().get(0).getDatatype());
+		ModelAndViewAssert.assertViewName(mav, ManageNurseriesController.BASE_TEMPLATE_NAME);
+		ModelAndViewAssert.assertModelAttributeValue(mav, 
+		        SaveNurseryController.TEMPLATE_NAME_ATTRIBUTE, "NurseryManager/manageNurseries");
 
 	}
+
 }

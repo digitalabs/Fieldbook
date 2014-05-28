66790da664f83cab22098bf839b41a3e186ef491
Naymesh Mistry <naymesh@gmail.com>
2014-05-28 16:33:30 +1200

    Formatting cleanup and removal of unused code (e.g. ApplicationContextAware-ness) from the base controller.
    Issue: none
    Reviewer: Daniel J

--- src/main/java/com/efficio/fieldbook/web/AbstractBaseFieldbookController.java
+++ src/main/java/com/efficio/fieldbook/web/AbstractBaseFieldbookController.java
@@ -18,9 +18,6 @@ import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.pojos.workbench.Tool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.ui.Model;
 
 import com.efficio.fieldbook.service.api.WorkbenchService;
@@ -30,202 +27,152 @@ import com.efficio.fieldbook.web.util.AppConstants;
 import com.efficio.fieldbook.web.util.ExternalToolInfo;
 
 /**
- * The Class AbstractBaseFieldbookController.
+ * Base controller encapsulaitng common functionality between all the Fieldbook controllers.
  */
-public abstract class AbstractBaseFieldbookController implements ApplicationContextAware {
+public abstract class AbstractBaseFieldbookController {
 
-    /** The Constant BASE_TEMPLATE_NAME. */
-    public static final String BASE_TEMPLATE_NAME = "/template/base-template";
-    public static final String ERROR_TEMPLATE_NAME = "/template/error-template";
-    
-    /** The Constant TEMPLATE_NAME_ATTRIBUTE. */
-    public static final String TEMPLATE_NAME_ATTRIBUTE = "templateName";
-    
-    /** The Constant GIT_INFO_ATTRIBUTE. */
-    public static final String GIT_INFO_ATTRIBUTE = "gitInfo";
-    
-    /** The Constant EXTERNAL_INFO_ATTRIBUTE. */
-    public static final String EXTERNAL_INFO_ATTRIBUTE = "externalInfo";
-    
-    /** The Constant USER_SELECTION_ID_SUFFIX. */
-    public static final String USER_SELECTION_ID_SUFFIX = "TestUserSelection";
-    
-  
-    
-    /** The external tool info. */
-    @Resource
-    public ExternalToolInfo externalToolInfo;
+	public static final String BASE_TEMPLATE_NAME = "/template/base-template";
+	public static final String ERROR_TEMPLATE_NAME = "/template/error-template";
+	public static final String TEMPLATE_NAME_ATTRIBUTE = "templateName";
+	public static final String EXTERNAL_INFO_ATTRIBUTE = "externalInfo";
 
-    
-    /** The application context. */
-    private ApplicationContext applicationContext;
-    
-    /** The workbench data manager. */
-    @Resource
-    private WorkbenchService workbenchService;
-    @Resource 
-    private ProjectActivityService projectActivityService; 
-    
-    private static final Logger LOG = LoggerFactory.getLogger(AbstractBaseFieldbookController.class);
+	@Resource
+	public ExternalToolInfo externalToolInfo;
 
-    private static Tool oldFbTool = null;
-    
-    @Resource
-    private PaginationListSelection paginationListSelection;
-    /**
-     * Gets the content name.
-     *
-     * @return the content name
-     */
-    public abstract String getContentName();
-    
-    /**
-     * Sets the up model info.
-     *
-     * @param model the new up model info
-     */
-    protected void setupModelInfo(Model model){
-        
-        model.addAttribute(EXTERNAL_INFO_ATTRIBUTE, externalToolInfo);
-    }
-        
-    
-   
+	@Resource
+	private WorkbenchService workbenchService;
+	
+	@Resource
+	private ProjectActivityService projectActivityService;
 
-    /**
-     * Gets the current project id.
-     *
-     * @return the current project id
-     */
-    public String getCurrentProjectId(){
-        if(externalToolInfo != null){
-            return externalToolInfo.getCurrentProjectId();
-        }
-        return "";
-    }
-    
-    /**
-     * Gets the old fieldbook path.
-     *
-     * @return the old fieldbook path
-     */
-    public String getOldFieldbookPath(){
-    	
-    	if(oldFbTool == null){
-			try {
-				oldFbTool = workbenchService.getToolWithName(
-				        AppConstants.TOOL_NAME_OLD_FIELDBOOK.getString());
-			} catch (MiddlewareQueryException e) {
-			    LOG.error(e.getMessage(), e);
-			}
-    	}
-		if(oldFbTool != null)
-			return oldFbTool.getPath();
+	private static final Logger LOG = LoggerFactory.getLogger(AbstractBaseFieldbookController.class);
+
+	private static Tool oldFbTool = null;
+
+	@Resource
+	private PaginationListSelection paginationListSelection;
+
+	/**
+	 * Implemented by the sub controllers to specify the html view that they render into the base template.
+	 * 
+	 */
+	public abstract String getContentName();
+
+	protected void setupModelInfo(Model model) {
+		model.addAttribute(EXTERNAL_INFO_ATTRIBUTE, externalToolInfo);
+	}
+
+	public String getCurrentProjectId() {
+		if (externalToolInfo != null) {
+			return externalToolInfo.getCurrentProjectId();
+		}
 		return "";
-    	/*
-        if(externalToolInfo != null){
-            return externalToolInfo.getOldFieldbookPath();
-        }
-        return "";
-        */
-    }
-    
-    public Tool getNurseryTool(){
-    	Tool tool = null;
-		try {
-			tool = workbenchService.getToolWithName(
-			        AppConstants.TOOL_NAME_NURSERY_MANAGER_WEB.getString());
-		} catch (MiddlewareQueryException e) {
-		    LOG.error(e.getMessage(), e);
+	}
+
+	public String getOldFieldbookPath() {
+
+		if (oldFbTool == null) {
+			try {
+				oldFbTool = workbenchService.getToolWithName(AppConstants.TOOL_NAME_OLD_FIELDBOOK.getString());
+			} catch (MiddlewareQueryException e) {
+				LOG.error(e.getMessage(), e);
+			}
 		}
-    	return tool;
-    }
-    public Tool getTrialTool(){
-    	Tool tool = null;
-		try {
-			tool = workbenchService.getToolWithName(
-			        AppConstants.TOOL_NAME_TRIAL_MANAGER_WEB.getString());
-		} catch (MiddlewareQueryException e) {
-		    LOG.error(e.getMessage(), e);
+		if (oldFbTool != null) {
+			return oldFbTool.getPath();
 		}
-    	return tool;
-    }
-    
-    /**
-     * Base functionality for displaying the page.
-     *
-     * @param model the model
-     * @return the string
-     */
-    public String show(Model model) {
-        setupModelInfo(model);
-        model.addAttribute(TEMPLATE_NAME_ATTRIBUTE, getContentName());
-        return BASE_TEMPLATE_NAME;
-    }
-    
-    public String showCustom(Model model,String contentName) {
-        setupModelInfo(model);
-        model.addAttribute(TEMPLATE_NAME_ATTRIBUTE, contentName);
-        return BASE_TEMPLATE_NAME;
-    }
-    
-    /**
-     * Base functionality for displaying the page.
-     *
-     * @param model the model
-     * @return the string
-     */
-    public String showError(Model model) {
-        setupModelInfo(model);
-        //model.addAttribute(TEMPLATE_NAME_ATTRIBUTE, getContentName());
-        return ERROR_TEMPLATE_NAME;
-    }
-    
-    /**
-     * Base functionality for displaying the page.
-     *
-     * @param model the model
-     * @param ajaxPage the ajax page
-     * @return the string
-     */
-    public String showAjaxPage(Model model, String ajaxPage) {
-        setupModelInfo(model);
-        //model.addAttribute(TEMPLATE_NAME_ATTRIBUTE, getContentName());        
-        return ajaxPage;
-    }
-    
-    /* (non-Javadoc)
-     * @see org.springframework.context.ApplicationContextAware#setApplicationContext(org.springframework.context.ApplicationContext)
-     */
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
-    }
-    
-    /**
-     * Convert favorite location to json.
-     *
-     * @param locations the locations
-     * @return the string
-     */
-    protected String convertObjectToJson(Object objectList) {
-        if (objectList!= null) {
-            try {
-                ObjectMapper mapper = new ObjectMapper();
-                return mapper.writeValueAsString(objectList);
-            } catch(Exception e) {
-                LOG.error(e.getMessage(), e);
-            }
-        }
-        return "[]";
-    }
+		return "";
+	}
+
+	public Tool getNurseryTool() {
+		Tool tool = null;
+		try {
+			tool = workbenchService.getToolWithName(AppConstants.TOOL_NAME_NURSERY_MANAGER_WEB.getString());
+		} catch (MiddlewareQueryException e) {
+			LOG.error(e.getMessage(), e);
+		}
+		return tool;
+	}
+
+	public Tool getTrialTool() {
+		Tool tool = null;
+		try {
+			tool = workbenchService.getToolWithName(AppConstants.TOOL_NAME_TRIAL_MANAGER_WEB.getString());
+		} catch (MiddlewareQueryException e) {
+			LOG.error(e.getMessage(), e);
+		}
+		return tool;
+	}
+
+	/**
+	 * Base functionality for displaying the page.
+	 * 
+	 * @param model
+	 *            the model
+	 * @return the string
+	 */
+	public String show(Model model) {
+		setupModelInfo(model);
+		model.addAttribute(TEMPLATE_NAME_ATTRIBUTE, getContentName());
+		return BASE_TEMPLATE_NAME;
+	}
+
+	public String showCustom(Model model, String contentName) {
+		setupModelInfo(model);
+		model.addAttribute(TEMPLATE_NAME_ATTRIBUTE, contentName);
+		return BASE_TEMPLATE_NAME;
+	}
+
+	/**
+	 * Base functionality for displaying the error page.
+	 * 
+	 * @param model
+	 *            the model
+	 * @return the string
+	 */
+	public String showError(Model model) {
+		setupModelInfo(model);
+		return ERROR_TEMPLATE_NAME;
+	}
+
+	/**
+	 * Base functionality for displaying the page.
+	 * 
+	 * @param model
+	 *            the model
+	 * @param ajaxPage
+	 *            the ajax page
+	 * @return the string
+	 */
+	public String showAjaxPage(Model model, String ajaxPage) {
+		setupModelInfo(model);
+		return ajaxPage;
+	}
+
+	/**
+	 * Convert favorite location to json.
+	 * 
+	 * @param locations
+	 *            the locations
+	 * @return the string
+	 */
+	protected String convertObjectToJson(Object objectList) {
+		if (objectList != null) {
+			try {
+				ObjectMapper mapper = new ObjectMapper();
+				return mapper.writeValueAsString(objectList);
+			} catch (Exception e) {
+				LOG.error(e.getMessage(), e);
+			}
+		}
+		return "[]";
+	}
 
 	public PaginationListSelection getPaginationListSelection() {
 		return paginationListSelection;
 	}
 
-	public void setPaginationListSelection(
-			PaginationListSelection paginationListSelection) {
+	public void setPaginationListSelection(PaginationListSelection paginationListSelection) {
 		this.paginationListSelection = paginationListSelection;
 	}
 
@@ -233,5 +180,4 @@ public abstract class AbstractBaseFieldbookController implements ApplicationCont
 		return projectActivityService;
 	}
 
-    
 }

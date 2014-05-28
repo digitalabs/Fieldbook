3babdfc95298dd22cb8e97014f6c81c841bd6ba5
Naymesh Mistry <naymesh@gmail.com>
2014-05-29 10:47:44 +1200

    Cleanup. Removed commented out/unused code and unrelated comments that were copy pasted from elsewhere.
    Reviewer: none
    Issue: none

--- src/main/java/com/efficio/fieldbook/web/error/ErrorController.java
+++ src/main/java/com/efficio/fieldbook/web/error/ErrorController.java
@@ -15,8 +15,6 @@ import java.util.Locale;
 
 import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,57 +22,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.efficio.fieldbook.web.AbstractBaseFieldbookController;
 
-/**
- * The Class LabelPrintingController.
- * 
- * This class would handle the label printing for the pdf and excel generation.
- */
 @Controller
-@RequestMapping({ErrorController.URL})
-public class ErrorController extends AbstractBaseFieldbookController{
- 
-     /** The Constant LOG. */
-     private static final Logger LOG = LoggerFactory.getLogger(ErrorController.class);
-    
-    /** The Constant URL. */
-    public static final String URL = "/error";
-    
-   
-   
-    /**
-     * Show trial label details.
-     *
-     * @param form the form
-     * @param model the model
-     * @param session the session
-     * @param id the id
-     * @param locale the locale
-     * @return the string
-     */
-    @RequestMapping(method = RequestMethod.GET)
-    public String showGeneralError(
-            Model model, HttpSession session, Locale locale) {
-    	    	
-        return super.showError(model);
-    }
+@RequestMapping({ ErrorController.URL })
+public class ErrorController extends AbstractBaseFieldbookController {
 
+	public static final String URL = "/error";
 
+	@RequestMapping(method = RequestMethod.GET)
+	public String showGeneralError(Model model, HttpSession session, Locale locale) {
+		return super.showError(model);
+	}
 
 	@Override
 	public String getContentName() {
-		// TODO Auto-generated method stub
 		return null;
 	}
-    
-    
-    /* (non-Javadoc)
-     * @see com.efficio.fieldbook.web.AbstractBaseFieldbookController#getContentName()
-     */
-    /*
-    @Override
-    public String getContentName() {
-        return "error/error";
-    }
-    */
-    
+
 }

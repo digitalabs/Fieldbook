7e522368d047ae2593af21155325311a2609a6bf
Daniel Jao <daniel@efficio.us.com>
2014-06-16 20:08:03 +0800

    Added new error template for showing message Update the ontology screen to use the new notification
    Reviewer: Tiff Go (post commit)
    
    Issue: none

--- src/main/webapp/WEB-INF/static/js/base.js
+++ src/main/webapp/WEB-INF/static/js/base.js
@@ -2,12 +2,15 @@ $(document).ready(function() {
 	$('.help-tooltip').tooltip();
 	$(".help-tooltip-nursery").tooltip();			
 });
+function createErrorNotification( titleDisplay, textDisplay){
+	var $container = createNotification('default-error-notification',titleDisplay,textDisplay);
+}
 function createSuccessNotification( titleDisplay, textDisplay){
 	createNotification('default-notification',titleDisplay,textDisplay);
 }
 function createNotification( template, titleDisplay, textDisplay){
 	var $container = $('#notification-container').notify();
-	return $container.notify('create', template, {title: titleDisplay, text: textDisplay}, {expires: 3000, click: function(e,instance){
+	return $container.notify('create', template, {title: titleDisplay, text: textDisplay}, {expires: false, click: function(e,instance){
 		instance.close();
 	}});
 }

e7ce9c86019355b48c8962b6ecdf4d6dbfe621e7
Daniel Villafuerte <dannyvonline@gmail.com>
2014-06-18 10:54:03 +0800

    Provided helper class and interface for capturing text within an input given a regular expression
    Post commit code review : Cyrus Venn Casada

---
+++ src/test/java/com/efficio/fieldbook/util/ExpressionHelperTest.java
@@ -0,0 +1,26 @@
+package com.efficio.fieldbook.util;
+
+import org.junit.Test;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Daniel Villafuerte
+ * Date: 6/18/2014
+ * Time: 10:28 AM
+ */
+public class ExpressionHelperTest {
+
+    @Test
+    public void testExpressionHelper() {
+        String input = "[Season]*-[AnotherClass]";
+
+        ExpressionHelper.evaluateExpression(input, "\\[([^\\]]*)]", new ExpressionHelperCallback() {
+            @Override
+            public void evaluateCapturedExpression(String capturedText, String originalInput, int start, int end) {
+                System.out.println(capturedText);
+            }
+        });
+    }
+}

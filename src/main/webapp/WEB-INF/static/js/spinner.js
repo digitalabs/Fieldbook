e57bf8a0609f236c3aa55ef15aa7b1ed1534af86
Helen Durrant <hh.durrant@gmail.com>
2014-06-19 17:44:52 +1200

    Adding a delay into spin.js to ensure the throbber does not appear unnecessarily.
    Issue: GCP-9036
    Reviewer: Rebecca B

--- src/main/webapp/WEB-INF/static/js/spinner.js
+++ src/main/webapp/WEB-INF/static/js/spinner.js
@@ -2,26 +2,46 @@
 /*
 Loading throbber.
 */
-window.Spinner = {
+window.Spinner = (function() {
 
-	active: false,
+	// Delay in miliseconds
+	var DELAY = 1000,
+		timeout;
 
-	play: function() {
+	function startThrobber() {
 		$.blockUI({ message: '<div id="spinner-wrap"><img src="/Fieldbook/static/img/loading-animation.gif"/></div>' });
 		$('.blockPage').css('border', '0px');
-		this.active = true;
-	},
-
-	stop: function() {
-		$.unblockUI();
-		this.active = false;
-	},
-
-	toggle: function() {
-		if (this.active) {
-			this.stop();
-		} else {
-			this.play();
-		}
 	}
-};
+
+	return {
+
+		play: function() {
+			// If there is already a waiting throbber, don't bother adding another one.
+			if (!timeout) {
+				timeout = setTimeout(startThrobber, DELAY);
+			}
+		},
+
+		// This method is intended to be called when your asynchronous method has returned (e.g. you have hit the server and come back)
+		stop: function() {
+			// If there is an existing timeout, we want to clear it. This will cancel any waiting timeouts (for example, if you come back
+			// within the delay) - ensuring the throbber does not appear unnecessarily.
+			if (timeout) {
+				clearTimeout(timeout);
+				timeout = null;
+			}
+
+			// Stop the throbber if it has already started playing.
+			$.unblockUI();
+		},
+
+		toggle: function() {
+			if (timeout) {
+				this.stop();
+			} else {
+				this.play();
+			}
+		}
+	};
+
+}());

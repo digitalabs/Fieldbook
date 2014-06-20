8499b0333487187efcf4c88081cdce433f8c7eed
Daniel Villafuerte <dannyvonline@gmail.com>
2014-06-20 17:15:01 +0800

    Removed calls to Spinner.play, Spinner.stop, and Spinner.toggle in the code aside from that in the new SpinnerManager.js
    Removed commented code from spinner.js
    
    GCP-9036
    
    Post commit code review : Cyrus Venn Casada

--- src/main/webapp/WEB-INF/static/js/spinner.js
+++ src/main/webapp/WEB-INF/static/js/spinner.js
@@ -1,13 +1,8 @@
-'use strict';
 /*
 Loading throbber.
 */
 window.Spinner = (function() {
-
-	// Delay in miliseconds
-	var DELAY = 1000,
-		timeout;
-
+    'use strict';
 	function startThrobber() {
 		$.blockUI({ message: '<div id="spinner-wrap"><img src="/Fieldbook/static/img/loading-animation.gif"/></div>' });
 		$('.blockPage').css('border', '0px');
@@ -16,32 +11,13 @@ window.Spinner = (function() {
 	return {
 
 		play: function() {
-			// If there is already a waiting throbber, don't bother adding another one.
-			/*if (!timeout) {
-				timeout = setTimeout(startThrobber, DELAY);
-			}*/
             startThrobber();
 		},
 
 		// This method is intended to be called when your asynchronous method has returned (e.g. you have hit the server and come back)
 		stop: function() {
-			// If there is an existing timeout, we want to clear it. This will cancel any waiting timeouts (for example, if you come back
-			// within the delay) - ensuring the throbber does not appear unnecessarily.
-			/*if (timeout) {
-				clearTimeout(timeout);
-				timeout = null;
-			}*/
-
 			// Stop the throbber if it has already started playing.
 			$.unblockUI();
-		},
-
-		toggle: function() {
-			if (timeout) {
-				this.stop();
-			} else {
-				this.play();
-			}
 		}
 	};
 

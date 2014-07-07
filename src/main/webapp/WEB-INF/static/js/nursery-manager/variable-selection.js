0ae042178fd5c2dccc6a2f8f070f9bc55a0c7660
Helen Durrant <hh.durrant@gmail.com>
2014-07-07 15:29:16 +1200

    Adding ability to search over variable names and class as well as property name in the property drop down. Also styled the drop down result display.
    Issue: GCP-9122
    Reviewer: Rebecca B (post commit)

--- src/main/webapp/WEB-INF/static/js/nursery-manager/variable-selection.js
+++ src/main/webapp/WEB-INF/static/js/nursery-manager/variable-selection.js
@@ -44,24 +44,37 @@ BMS.NurseryManager.VariableSelection = (function($) {
 		}
 	});
 
-	function formatResult(item) {
-		var formattedItem,
+	function formatResult(item, container, query) {
+		var searchTerm = query.term,
+			regex,
+			propertyName,
+			className,
+			formattedItem,
 			variables,
 			i;
 
-		formattedItem  = '<p><strong>' + item.name + '</strong> (' + item.traitClass.traitClassName + ')<br/>';
+		regex = new RegExp(searchTerm, 'gi');
+
+		propertyName = item.name.replace(regex, '<strong>$&</strong>');
+		className = item.traitClass.traitClassName.replace(regex, '<strong>$&</strong>');
+
+		formattedItem  = '<p><span class="var-select-result-prop">' + propertyName + '</span> (<span class="var-select-result-class">' +
+			className + '</span>)<br/>';
 
 		variables = item.standardVariables.sort(function(a, b) {
 			return a.name.localeCompare(b.name);
 		});
 
+		formattedItem += '<span class="var-select-result-vars">';
+
 		for (i = 0; i < variables.length; i++) {
 			if (i !== 0) {
 				formattedItem += ', ';
 			}
-			formattedItem += variables[i].name;
+
+			formattedItem += variables[i].name.replace(regex, '<strong>$&</strong>');
 		}
-		return formattedItem + '</p>';
+		return formattedItem + '</span></p>';
 	}
 
 	function formatSelection(item) {
@@ -85,7 +98,40 @@ BMS.NurseryManager.VariableSelection = (function($) {
 				return {
 					id: item.propertyId
 				};
-			}
+			},
+			query: function(options) {
+
+				var searchTerm = options.term.toLocaleLowerCase(),
+					result = {};
+
+				result.results = $.grep(properties, function(prop) {
+
+					var propContainsTerm,
+						varNameContainsTerm;
+
+					// Include the object in the results if either the property name or class name contains the search term
+					propContainsTerm = prop.name.toLocaleLowerCase().indexOf(searchTerm) > -1 ||
+						prop.traitClass.traitClassName.toLocaleLowerCase().indexOf(searchTerm) > -1;
+
+					if (propContainsTerm) {
+						return true;
+					}
+
+					// Also include the object if any of the property's variables have the search term in their name
+					$.each(prop.standardVariables, function(index, variable) {
+						varNameContainsTerm = variable.name.toLocaleLowerCase().indexOf(searchTerm) > -1;
+
+						// Returning false will stop the loop - we want to stop when we've found the first variable
+						// in this property that contains the search term
+						return !varNameContainsTerm;
+					});
+
+					return varNameContainsTerm;
+				});
+
+				options.callback(result);
+			},
+			dropdownCssClass: 'var-select-results'
 		});
 	}
 

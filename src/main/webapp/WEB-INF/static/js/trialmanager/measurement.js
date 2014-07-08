5bc25660c0c77dcddc54a436201e9181b6903f7b
Cyrus Venn Casada <cvpcasada@outlook.com>
2014-07-08 15:35:41 +0800

    Initial measurements tab trial component implementation
    Reviewer: Dan V.

--- src/main/webapp/WEB-INF/static/js/trialmanager/measurement.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/measurement.js
@@ -1,3 +1,24 @@
 /**
  * Created by cyrus on 7/2/14.
  */
+
+/* global angular */
+(function(){
+    'use strict';
+
+    angular.module('manageTrialApp').controller('MeasurementsCtrl',
+        ['$scope', 'TrialManagerDataService', function($scope, TrialManagerDataService) {
+
+            $scope.data = TrialManagerDataService.currentData.measurements;
+
+            $scope.updateOccurred = false;
+
+            $scope.$on('deleteOccurred', function() {
+                $scope.updateOccurred = true;
+            });
+
+            $scope.$on('variableAdded', function() {
+                $scope.updateOccurred = true;
+            });
+        }]);
+})();

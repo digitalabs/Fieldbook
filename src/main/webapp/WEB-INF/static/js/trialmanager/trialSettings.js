76982edd271625e0948112855052130de639d26f
Cyrus Venn Casada <cvpcasada@outlook.com>
2014-07-09 18:18:57 +0800

    implemented ordered hash data structure on the trial settings
    Reviewer: Dan V.

--- src/main/webapp/WEB-INF/static/js/trialmanager/trialSettings.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/trialSettings.js
@@ -33,66 +33,6 @@
 
     }]);
 
-    // factory and services
-    manageTrialApp.factory('TrialSettingsService',function() {
-        return {
-            data : {
-                managementDetails : {
-
-                },
-                userInput : {}
-            },
-
-            addManagementDetailVar : function(cvTermId,item) {
-                this.data.managementDetails[cvTermId] = item;
-                this.data.managementDetails[cvTermId].variableLabelCount = 0;
-                this.data.managementDetails[cvTermId].variableLabels = [];
-            },
-
-            removeUserInputByIndex : function(cvTermId) {
-                if (cvTermId !== undefined && cvTermId !== null && this.userInput[cvTermId] !== undefined) {
-                    delete this.userInput[cvTermId];
-                }
-            },
-
-            removeManagementDetailVarByIndex : function(index) {
-                if (index !== undefined && index !== null) {
-                    delete this.data.managementDetails[index];
-                }
-            },
-
-            addDummyData : function() {
-                this.addManagementDetailVar(8195,{
-                    variable: {
-                        cvTermId: 8195,
-                        name: 'SITE_CODE',
-                        description: 'Site code - assigned (text)',
-                        property: 'Location',
-                        scale: 'Code',
-                        method: 'Assigned',
-                        role: 'Trial environment information',
-                        dataType: 'Character variable',
-                        traitClass: 'Trial environment',
-                        cropOntologyId: '',
-                        dataTypeId: 1120,
-                        minRange: null,
-                        maxRange: null,
-                        widgetType: 'CTEXT',
-                        operation: 'ADD'
-                    },
-                    possibleValues: [ ],
-                    possibleValuesFavorite: null,
-                    possibleValuesJson: null,
-                    possibleValuesFavoriteJson: null,
-                    value: null,
-                    order: 0,
-                    group: null,
-                    deletable: true,
-                    favorite: false
-                });
-            }
-        };
-    });
 
 
 })();

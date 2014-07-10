85a8ab006c7db94d57b47c1465dcdcb13c5d9d33
Daniel Villafuerte <dannyvonline@gmail.com>
2014-07-10 17:31:59 +0800

    Created test class to facilitate testing / debugging in case of errors converting JSON data into an equivalent object in the back end
    Code review : Cyrus Venn Casada

---
+++ src/test/java/com/efficio/fieldbook/util/ObjectMapperTest.java
@@ -0,0 +1,39 @@
+package com.efficio.fieldbook.util;
+
+import com.efficio.fieldbook.web.trial.bean.TrialData;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Daniel Villafuerte
+ * Date: 7/10/2014
+ * Time: 3:48 PM
+ */
+@RunWith(value = JUnit4.class)
+public class ObjectMapperTest {
+    @Test
+    public void testMappingOfConsolidatedForm() {
+        ObjectMapper mapper = new ObjectMapper();
+
+
+        try {
+            TrialData data = mapper.readValue("{\"trialSettings\":{\"userInput\":{}},\"environments\":{\"environments\":[{\"managementDetailValues\":{\"8170\":null,\"8192\":\"2\",\"8194\":\"1\"},\"" +
+                    "trialDetailValues\":{}},{\"managementDetailValues\":{\"8170\":null,\"8192\":\"4\",\"8194\":\"3\"},\"trialDetailValues\":{}},{\"managementDetailValues\":{\"8170\":null,\"8192\":\"" +
+                    "6\",\"8194\":\"5\"},\"trialDetailValues\":{}}],\"noOfEnvironments\":\"3\"},\"basicDetails\":{\"basicDetails\":{\"8005\":\"new trial 6\",\"8007\":\"test\",\"8030\":\"\",\"8050\":\"" +
+                    "07/10/2014\",\"8060\":\"\"},\"folderId\":1,\"folderName\":\"Program Trials\",\"folderNameLabel\":\"Program Trials\",\"userID\":-1,\"userName\":\"Daniel Villafuerte\"}}", TrialData.class);
+
+            assertNotNull(data);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+
+    }
+}

40e02957ae124f2040e2053e1a1058d3439080a7
Cyrus Venn Casada <cvpcasada@outlook.com>
2014-07-10 18:39:49 +0800

    Initial UI implementation for Experimental Design
    Reviewer: Dan V

--- src/main/webapp/WEB-INF/static/js/trialmanager/experimentalDesign.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/experimentalDesign.js
@@ -1,3 +1,71 @@
 /**
  * Created by cyrus on 7/2/14.
  */
+
+/* global angular */
+(function(){
+    'use strict';
+
+    angular.module('manageTrialApp')
+        .constant("EXPERIMENTAL_DESIGN_PARTIALS_LOC", "/Fieldbook/static/angular-templates/experimentalDesignPartials/")
+        .controller('ExperimentalDesignCtrl',['$scope','$state','EXPERIMENTAL_DESIGN_PARTIALS_LOC',function($scope,$state,EXPERIMENTAL_DESIGN_PARTIALS_LOC){
+            $scope.designTypes = [
+                {name: 'Randomized Complete Block Design',params :'randomizedCompleteBlockParams.html',
+                    data: {
+                        'replicationsCount' : 5
+                    },
+                    settings: {
+                        trialEnvironments: 4,
+                        factors : {},
+                        treatmentFactors : {}
+                    }
+                },
+                {name: 'Incomplete Block Design', params: 'incompleteBlockParams.html',
+                    withResolvable: true,
+                    showAdvancedOptions: false,
+                    data: {
+                        'isResolvable' : true,
+                        'replicationsCount' : 0,
+                        'blockSize' : 0,
+                        'useLatenized' : true,
+                        'contiguousBlocksToLatenize' : 0,
+                        'replicationsPerCol' : 0
+                    },
+                    settings: {
+                        trialEnvironments: 6,
+                        treatments: 24,
+                        blocks: 6,
+                        factors : {}
+                    }
+                },
+                {name: 'Row-and-Column',params:'rowAndColumnParams.html',
+                    withResolvable: true,
+                    showAdvancedOptions: false,
+                    data: {
+                        'isResolvable' : true,
+                        'replicationsCount': 0,
+                        'rowsPerReplications' : 0,
+                        'colsPerReplications' : 0,
+                        'contiguousRowsToLatenize':0,
+                        'contiguousColToLatenize': 0
+                    },
+                    settings: {
+                        trialEnvironments: 3,
+                        treatments: 24,
+                        factors : {}
+                    }
+                }];
+
+            $scope.currentDesignType = $scope.designTypes[0];
+
+            $scope.currentParams = EXPERIMENTAL_DESIGN_PARTIALS_LOC + $scope.currentDesignType.params;
+
+            $scope.onSwitchDesignTypes = function(selectedDesignType) {
+               $scope.currentParams = EXPERIMENTAL_DESIGN_PARTIALS_LOC + selectedDesignType.params;
+               $scope.currentDesignType = selectedDesignType;
+            };
+    }]);
+
+})();
+
+

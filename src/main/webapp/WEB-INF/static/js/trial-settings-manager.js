ddf854fe3c7f6f2adbfab26afb4ea120fe307f87
Daniel Villafuerte <dannyvonline@gmail.com>
2014-07-17 13:06:31 +0800

    For filtering of properties shown in select variable popup based on specified group / variable type
    Code review : Cyrus Venn Casada

--- src/main/webapp/WEB-INF/static/js/trial-settings-manager.js
+++ src/main/webapp/WEB-INF/static/js/trial-settings-manager.js
@@ -1,32 +1,270 @@
-window.TrialSettingsManager = (function() {
-	'use strict';
+window.TrialSettingsManager = (function () {
+    'use strict';
 
-	var MODES = {
-			TRIAL_SETTINGS: 1,
-			GERMPLASM_FACTORS: 8,
-			MEASUREMENT_TRAITS: 3,
-			TRIAL_ENVIRONMENT: 4,
-			TREATMENT_FACTORS: 5,
-			TRIAL_CONDITIONS: 7
-		},
-        modalSelector = '.vs-modal',
-		variableSelectionGroups = {},
-		TrialSettingsManager;
+    var MODES = {
+            TRIAL_SETTINGS: 1,
+            GERMPLASM_FACTORS: 8,
+            MEASUREMENT_TRAITS: 3,
+            TRIAL_ENVIRONMENT: 4,
+            TREATMENT_FACTORS: 5,
+            TRIAL_CONDITIONS: 7
+        },
+        variableSelectionGroups = {},
+        TrialSettingsManager, properties, selectedVariables;
 
-	TrialSettingsManager = function(translations) {
+    /*
+     * Finds a property in a list by id.
+     *
+     * @param {string} propertyId the id of the property to look for
+     * @param {object[]} propertyList the list of properties to search
+     * @returns {number} the index of the found property, or -1 if it was not found
+     */
+    function _findPropertyById(propertyId, propertyList) {
 
-		// Look for any existing variables and instaniate our list of them
+        var index = -1;
 
-		$.each(MODES, function(key, value) {
-			variableSelectionGroups[value] = {
-				label: translations[value] ? translations[value].label : '',
-				placeholder: translations[value] ? translations[value].placeholder : ''
-			};
-		});
+        // Use each instead of grep to prevent the need to continue to iterate over the array once we've found what we're looking for
+        $.each(propertyList, function (i, propertyObj) {
 
-	};
+            var found = false;
 
-    TrialSettingsManager.prototype._initialiseVariableSelectionDialog = function() {
+            if (propertyObj.propertyId === propertyId) {
+                found = true;
+                index = i;
+            }
+            return !found;
+        });
+        return index;
+    }
+
+    /*
+     * Removes an item by item from a given list.
+     *
+     * @param {string} id the id of the item to remove
+     * @param {object[]} list the list of objects to search
+     * @param {string} idSelector the key with which the id property should be accessed on each item of the list (e.g. `id` or `propertyId`)
+     * @returns {object} the object that was removed or null if the property was not found
+     */
+    function _removeById(id, list, idSelector) {
+
+        var property = null;
+
+        // Use each instead of grep to prevent the need to continue to iterate over the array once we've found what we're looking for
+        $.each(list, function (index, obj) {
+
+            var found = false;
+
+            if (obj[idSelector] === id) {
+                found = true;
+                property = list.splice(index, 1)[0];
+            }
+            return !found;
+        });
+        return property;
+    }
+
+    /*
+     * For a list of given exclusions, ensure that if any of the variables are in a specified list of selected variables, their exclusions
+     * are removed from the given list of properties.
+     *
+     * @param {object} exclusions an exclusion object. Each key in the object represents a variable id that, if found, should have a matched
+     * variable removed. Each object in the exclusion should have two properties, `variableId` - the variable to remove, and `propertyId` -
+     * the id of the property of the variable to be removed.
+     * @param {object} selectedVariables an object representing all currently selected variables. Each key represents a variable id that is
+     * selected, and it's value the name of that variable.
+     * @param {object[]} properties a list of properties from which to remove variables / properties as appropriate
+     * @returns {object[]} a list of properties that were removed
+     */
+    function _performExclusions(exclusions, selectedVariables, properties) {
+
+        var removedProperties = [],
+            key,
+            exclusion,
+            id,
+            index;
+
+        for (key in exclusions) {
+            if (exclusions.hasOwnProperty(key)) {
+
+                id = parseInt(key, 10);
+                exclusion = exclusions[id];
+
+                // If any of the variables that need translating are selected, we must remove it's counterpart
+                if (typeof selectedVariables[id] !== 'undefined') {
+                    index = _findPropertyById(exclusion.propertyId, properties);
+
+                    // Remove the variable if we found the property. Weird if we can't find the property, but not an issue since we just
+                    // wanted to make sure one of it's variables was excluded
+                    if (index !== -1) {
+                        _removeById(exclusion.variableId, properties[index].standardVariables, 'id');
+
+                        // If the property has no more variables, remove it too
+                        if (properties[index].standardVariables.length === 0) {
+                            removedProperties.push(properties.splice(index, 1)[0]);
+                        }
+                    }
+                }
+            }
+        }
+        return removedProperties;
+    }
+
+    /*
+     * Remove the specified variables from the given list.
+     *
+     * @param {object[]} variableList a list of variables, each with a variableId and propertyId property
+     * @param {object[]} properties a list of properties from which to remove variables / properties as appropriate.
+     * @returns {object[]} a list of properties that were removed
+     */
+    function _removeVariables(variableList, properties) {
+
+        var removedProperties = [],
+            index;
+
+        $.each(variableList, function (i, variable) {
+
+            index = _findPropertyById(variable.propertyId, properties);
+
+            // Remove the variable if we found the property. Weird if we can't find the property, but not an issue since we just
+            // wanted to make sure one of it's variables was excluded
+            if (index !== -1) {
+                _removeById(variable.variableId, properties[index].standardVariables, 'id');
+
+                // If the property has no more variables, remove it too
+                if (properties[index].standardVariables.length === 0) {
+                    removedProperties.push(properties.splice(index, 1)[0]);
+                }
+            }
+        });
+        return removedProperties;
+    }
+
+    /* FIXME - this logic should be in the back end
+     *
+     * Filters a list of properties according to some hard coded rules (see comments for details).
+     *
+     * @param {object[]} properties the list of properties to filter
+     * @param {number} group properties and variables will be filtered to be specific to the group represented by this number
+     * @returns {object} an object with two properties, excluded and included - the list of properties that were removed and left
+     */
+    function _filterProperties(properties, selectedVariables, group) {
+
+        // Don't modify the original list
+        var filteredProperties = JSON.parse(JSON.stringify(properties)),
+
+            studyLevelBreedingMethodPropertyId = 2670,
+
+            managementDetailExclusions = {
+
+                // Don't allow user to select PI_NAME from the Person property if PI_ID is present
+                8110: {
+                    variableId: 8100,
+                    propertyId: 2080
+                },
+
+                // Don't allow user to select COOPERATOR from the PERSON property if COOPERATOR_ID is present
+                8372: {
+                    variableId: 8373,
+                    propertyId: 2080
+                },
+
+                // Don't allow user to select LOCATION_NAME from the Location property if LOCATION_ID is present
+                8190: {
+                    variableId: 8180,
+                    propertyId: 2110
+                }
+            },
+
+        // There are a basic set of details hard coded into the page that should not be presented as variables
+            basicDetails = [
+                {
+                    variableId: 8005, // STUDY_NAME
+                    propertyId: 2010  // Study
+                },
+                {
+                    variableId: 8007, // STUDY_TITLE
+                    propertyId: 2012  // Study title
+                },
+
+                {
+                    variableId: 8009, // STUDY_UPDATE
+                    propertyId: 2045  // Update date
+                },
+
+                {
+                    variableId: 8020, // Study_UID
+                    propertyId: 2002  // User
+                },
+
+                {
+                    variableId: 8030, // STUDY_OBJECTIVE
+                    propertyId: 2014  // Study objective
+                },
+
+                {
+                    variableId: 8050, // START_DATE
+                    propertyId: 2050  // Start date
+                },
+
+                {
+                    variableId: 8060, // END_DATE
+                    propertyId: 2052  // End date
+                }
+            ],
+
+            selectionExclusions = {
+                // Don't allow user to select BM_CODE_VTE from the Breeding method property if BM_ID_VTE is present
+                8262: {
+                    variableId: 8252,
+                    propertyId: 2670
+                }
+            },
+
+            exclusions = [],
+            removedProperty;
+
+        // Currently we only have special handling for management details (1) and selection strategy (6)
+        switch (group) {
+            case 1:
+                // This property must be excluded as the variables it contains are duplicated by a dropdown on the main page
+                removedProperty = _removeById(studyLevelBreedingMethodPropertyId, filteredProperties, 'propertyId');
+
+                // If we found the property to remove. Weird but not disastrous if we can't find the variable to remove it.
+                if (removedProperty) {
+                    exclusions.push(removedProperty);
+                }
+
+                // Remove variables and properties as necessary
+                exclusions = exclusions.concat(_performExclusions(managementDetailExclusions, selectedVariables, filteredProperties));
+                exclusions = exclusions.concat(_removeVariables(basicDetails, filteredProperties));
+                break;
+            case 6:
+                // Remove variables and properties as necessary
+                exclusions = exclusions.concat(_performExclusions(selectionExclusions, selectedVariables, filteredProperties));
+                break;
+            default:
+                break;
+        }
+        return {
+            exclusions: exclusions,
+            inclusions: filteredProperties
+        };
+    }
+
+    TrialSettingsManager = function (translations) {
+
+        // Look for any existing variables and instaniate our list of them
+
+        $.each(MODES, function (key, value) {
+            variableSelectionGroups[value] = {
+                label: translations[value] ? translations[value].label : '',
+                placeholder: translations[value] ? translations[value].placeholder : ''
+            };
+        });
+
+    };
+
+    TrialSettingsManager.prototype._initialiseVariableSelectionDialog = function () {
         this._variableSelection = new window.BMS.NurseryManager.VariableSelection({
             // FIXME pass in translated value with key variable.selection.unique.variable.error from html
             uniqueVariableError: 'This name has been used before, please enter a different name.'
@@ -36,46 +274,52 @@ window.TrialSettingsManager = (function() {
         return this._variableSelection;
     };
 
-	TrialSettingsManager.prototype._openVariableSelectionDialog = function(params) {
-		var groupId = params.variableType,
-			group = variableSelectionGroups[groupId],
-			groupTranslations = {
-				label: group.label,
-				placeholderLabel: group.placeholder
-			},
-			modal = this._variableSelection;
+    TrialSettingsManager.prototype._openVariableSelectionDialog = function (params) {
+        var groupId = parseInt(params.variableType, 10),
+            group = variableSelectionGroups[groupId],
+            groupTranslations = {
+                label: group.label,
+                placeholderLabel: group.placeholder
+            },
+            modal = this._variableSelection;
 
-		// Initialise a variable selection modal if we haven't done so before
-		if (!modal) {
-			modal = this._variableSelection = this._initialiseVariableSelectionDialog();
-		}
+        // Initialise a variable selection modal if we haven't done so before
+        if (!modal) {
+            modal = this._variableSelection = this._initialiseVariableSelectionDialog();
+        }
 
-		// If we haven't loaded data for this group before, then load it
-		if (!group.data) {
+        selectedVariables = params.retrieveSelectedVariableFunction();
 
-			$.getJSON('/Fieldbook/OntologyBrowser/settings/properties?groupId=' + groupId + '&useTrialFiltering=true', function(data) {
-				group.data = data;
+        // If we haven't loaded data for this group before, then load it
+        if (!group.data) {
 
-				// Initialise a new Variable Selection instance, passing through the properties, group type and groupTranslations
-				// TODO get variable usage
-				modal.show(groupId, groupTranslations, {
-					propertyData: group.data,
-					variableUsageData: [],
-					selectedVariables: params.retrieveSelectedVariableFunction()
-				});
-			});
+            $.getJSON('/Fieldbook/OntologyBrowser/settings/properties?groupId=' + groupId + '&useTrialFiltering=true', function (data) {
+                group.data = data;
+                properties = _filterProperties(group.data, selectedVariables, groupId);
 
-			// TODO Error handling
+                // Initialise a new Variable Selection instance, passing through the properties, group type and groupTranslations
+                // TODO get variable usage
+                modal.show(groupId, groupTranslations, {
+                    propertyData: properties.inclusions,
+                    excludedProperties: properties.exclusions,
+                    variableUsageData: [],
+                    selectedVariables: params.retrieveSelectedVariableFunction()
+                });
+            });
 
-		} else {
-			// We've shown this before, and have the data. Just show the dialog.
-			modal.show(groupId, groupTranslations, {
-				propertyData: group.data,
-				variableUsageData: [],
-				selectedVariables: params.retrieveSelectedVariableFunction()
-			});
-		}
-	};
+            // TODO Error handling
 
-	return TrialSettingsManager;
+        } else {
+            // We've shown this before, and have the data. Just show the dialog.
+            properties = _filterProperties(group.data, selectedVariables, groupId);
+            modal.show(groupId, groupTranslations, {
+                propertyData: properties.inclusions,
+                excludedProperties: properties.exclusions,
+                variableUsageData: [],
+                selectedVariables: params.retrieveSelectedVariableFunction()
+            });
+        }
+    };
+
+    return TrialSettingsManager;
 }());

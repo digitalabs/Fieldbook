74f57ccd43d411885224efc8fbab675282032f44
Daniel Jao <daniel@efficio.us.com>
2014-07-28 19:20:09 +0800

    Change the display in the summary details for row col exp design
    Reviewer: Cy Casada (post commit)
    
    Issue: GCP-9692

--- src/main/webapp/WEB-INF/static/js/trialmanager/experimentalDesign.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/experimentalDesign.js
@@ -39,7 +39,7 @@
 
 
                 $scope.$watchCollection('settings.datae',function(newArr,oldArr) {
-                   console.log(newArr);
+                   //console.log(newArr);
                 });
 
                 $scope.designTypes = [
@@ -317,7 +317,7 @@
                 var excludes = [
                     [8230, 8210, 8581, 8582],
                     [8581, 8582],
-                    [8210,8220,8200]
+                    [8220, 8200]
                 ];
 
                 var copyList = angular.copy(factorList);

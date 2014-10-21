91a5a9138d230d5a939c317a27a1417b1e903d19
Daniel Jao <daniel@efficio.us.com>
2014-10-21 13:19:17 +0800

    Added JUnit cases to test out refactored code in the CreateNurseryController
    GCP-10216
    
    Reviewer: Chezka Arevalo

--- src/test/java/com/efficio/fieldbook/web/nursery/controller/CreateNurseryControllerTest.java
+++ src/test/java/com/efficio/fieldbook/web/nursery/controller/CreateNurseryControllerTest.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.generationcp.middleware.domain.etl.MeasurementVariable;
 import org.generationcp.middleware.domain.oms.TermId;
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
+import org.generationcp.middleware.pojos.Location;
+import org.generationcp.middleware.pojos.Method;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -28,7 +32,7 @@ public class CreateNurseryControllerTest extends AbstractBaseControllerTest {
 	private CreateNurseryController controller;
 	
 	@Autowired
-	private UserSelection userSelection; 
+	private UserSelection userSelection; 	
 	
 	@Test
 	public void testGetReturnsCorrectModelAndView() throws Exception {
@@ -66,5 +70,105 @@ public class CreateNurseryControllerTest extends AbstractBaseControllerTest {
 		
 		String submitResponse = controller.submit(form, new ExtendedModelMap());
 		Assert.assertEquals("success", submitResponse);
+	}
+	
+	@Test 
+	public void testSetLocationVariableValueWhenVariableIsLocationAndHasANonEmptyValue() throws MiddlewareQueryException{
+		CreateNurseryController createNurseryController = new CreateNurseryController();
+		org.generationcp.middleware.service.api.FieldbookService fieldbookMiddlewareService = Mockito.mock(org.generationcp.middleware.service.api.FieldbookService.class);
+		createNurseryController.setFieldbookMiddlewareService(fieldbookMiddlewareService);
+		String locationId = "1001";
+		
+		Location location = new Location();		
+		location.setLocid(Integer.parseInt(locationId));
+		
+		Mockito.when(fieldbookMiddlewareService.getLocationById(Integer.parseInt(locationId))).thenReturn(location);
+		SettingDetail detail = new SettingDetail();
+		MeasurementVariable var = new MeasurementVariable();
+		var.setTermId(TermId.LOCATION_ID.getId());
+		var.setValue(locationId);
+		createNurseryController.setLocationVariableValue(detail, var);
+		Assert.assertEquals("Should be able to set the value in the setting detail if its a location",locationId, detail.getValue());
+	}
+	
+	@Test 
+	public void testSetLocationVariableValueWhenVariableIsLocationAndHasAnEmptyValue() throws MiddlewareQueryException{
+		CreateNurseryController createNurseryController = new CreateNurseryController();
+		org.generationcp.middleware.service.api.FieldbookService fieldbookMiddlewareService = Mockito.mock(org.generationcp.middleware.service.api.FieldbookService.class);
+		createNurseryController.setFieldbookMiddlewareService(fieldbookMiddlewareService);
+		String locationId = "1001";
+		
+		Location location = new Location();		
+		location.setLocid(Integer.parseInt(locationId));
+		
+		Mockito.when(fieldbookMiddlewareService.getLocationById(Integer.parseInt(locationId))).thenReturn(location);
+		SettingDetail detail = new SettingDetail();
+		MeasurementVariable var = new MeasurementVariable();
+		var.setTermId(TermId.LOCATION_ID.getId());
+		var.setValue("");
+		createNurseryController.setLocationVariableValue(detail, var);
+		Assert.assertEquals("Setting detail should have an empty value since the variable did not have a value itself","", detail.getValue());
+	}
+	
+	@Test
+	public void testSetSettingDetailsValueFromVariableWhenVariableLocationIdIsANonEmptyValue() throws NumberFormatException, MiddlewareQueryException{
+		CreateNurseryController createNurseryController = new CreateNurseryController();
+		org.generationcp.middleware.service.api.FieldbookService fieldbookMiddlewareService = Mockito.mock(org.generationcp.middleware.service.api.FieldbookService.class);
+		createNurseryController.setFieldbookMiddlewareService(fieldbookMiddlewareService);
+		String locationId = "1001";
+		Location location = new Location();		
+		location.setLocid(Integer.parseInt(locationId));
+		
+		Mockito.when(fieldbookMiddlewareService.getLocationById(Integer.parseInt(locationId))).thenReturn(location);
+	
+		SettingDetail detail = new SettingDetail();
+		MeasurementVariable var = new MeasurementVariable();
+		var.setTermId(TermId.LOCATION_ID.getId());
+		var.setValue(locationId);
+		
+		createNurseryController.setSettingDetailsValueFromVariable(var, detail);
+		Assert.assertEquals("Should be able to set the value in the setting detail if its a location",locationId, detail.getValue());
+	}
+	
+	@Test
+	public void testSetSettingDetailsValueFromVariableWhenVariableLocationIdIsAnEmptyValue() throws NumberFormatException, MiddlewareQueryException{
+		CreateNurseryController createNurseryController = new CreateNurseryController();
+		org.generationcp.middleware.service.api.FieldbookService fieldbookMiddlewareService = Mockito.mock(org.generationcp.middleware.service.api.FieldbookService.class);
+		createNurseryController.setFieldbookMiddlewareService(fieldbookMiddlewareService);
+		String locationId = "1001";
+		Location location = new Location();		
+		location.setLocid(Integer.parseInt(locationId));
+		
+		Mockito.when(fieldbookMiddlewareService.getLocationById(Integer.parseInt(locationId))).thenReturn(location);
+	
+		SettingDetail detail = new SettingDetail();
+		MeasurementVariable var = new MeasurementVariable();
+		var.setTermId(TermId.LOCATION_ID.getId());
+		var.setValue("");
+		
+		createNurseryController.setSettingDetailsValueFromVariable(var, detail);
+		Assert.assertEquals("Setting detail should have an empty value if the location variable is an empty value", "", detail.getValue());
+	}
+	
+	@Test
+	public void testSetSettingDetailsValueFromVariableWhenVariableBreedingMethodCodeIsANonEmptyValue() throws NumberFormatException, MiddlewareQueryException{
+		CreateNurseryController createNurseryController = new CreateNurseryController();
+		org.generationcp.middleware.service.api.FieldbookService fieldbookMiddlewareService = Mockito.mock(org.generationcp.middleware.service.api.FieldbookService.class);
+		createNurseryController.setFieldbookMiddlewareService(fieldbookMiddlewareService);
+		String bmCode = "TST_CODE";
+		Integer bmId = 10001;
+		Method method = new Method();		
+		method.setMcode(bmCode);
+		method.setMid(bmId);
+		
+		Mockito.when(fieldbookMiddlewareService.getMethodByCode(bmCode)).thenReturn(method);
+	
+		SettingDetail detail = new SettingDetail();
+		MeasurementVariable var = new MeasurementVariable();
+		var.setTermId(TermId.BREEDING_METHOD_CODE.getId());
+		var.setValue(bmCode);
+		
+		createNurseryController.setSettingDetailsValueFromVariable(var, detail);
+		Assert.assertEquals("Setting detail should the corresponding id of the matching method for the method code", bmId.toString(), detail.getValue());
 	}
 }

5ed357ec8cd1a40017c3ad0bd0bd1e77a5e38d7c
Daniel Villafuerte <dannyvonline@gmail.com>
2014-11-18 14:06:06 +0800

    Change resource access to using classpath
    GCP-10434
    
    Reviewer : Cyrus Venn Casada

--- src/test/java/com/efficio/fieldbook/AbstractBaseIntegrationTest.java
+++ src/test/java/com/efficio/fieldbook/AbstractBaseIntegrationTest.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"file:src/test/resources/Fieldbook-servlet-test.xml"})
+@ContextConfiguration(locations = {"classpath:Fieldbook-servlet-test.xml"})
 public abstract class AbstractBaseIntegrationTest extends AbstractJUnit4SpringContextTests {
 
 }

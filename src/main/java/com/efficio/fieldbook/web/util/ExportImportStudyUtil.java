3fda2b68d553acedbda13d60e9dad448f6505dda
Crisjaira Villarino <crisjaira.villarino@leafnode.io>
2014-12-02 13:53:15 +0800

    Refactored common methods used in Excel Export and CSV Export of Study
    BMS-168
    
    Reviewer: Daniel Jao

--- src/main/java/com/efficio/fieldbook/web/util/ExportImportStudyUtil.java
+++ src/main/java/com/efficio/fieldbook/web/util/ExportImportStudyUtil.java
@@ -5,12 +5,25 @@ import java.util.List;
 
 import org.apache.commons.lang3.math.NumberUtils;
 import org.generationcp.middleware.domain.dms.ValueReference;
+import org.generationcp.middleware.domain.etl.MeasurementData;
 import org.generationcp.middleware.domain.etl.MeasurementRow;
 import org.generationcp.middleware.domain.etl.MeasurementVariable;
 import org.generationcp.middleware.domain.etl.Workbook;
 import org.generationcp.middleware.domain.oms.TermId;
+import org.generationcp.middleware.exceptions.MiddlewareQueryException;
+import org.generationcp.middleware.service.api.OntologyService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ExportImportStudyUtil {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(ExportImportStudyUtil.class);
+	
+	private static final Integer[] REQUIRED_COLUMNS = {TermId.PLOT_NO.getId(), TermId.ENTRY_NO.getId(), TermId.DESIG.getId()};
+	
+	private ExportImportStudyUtil(){
+		// do nothing
+	}
 
 	public static String getCategoricalCellValue(String idValue, List<ValueReference> possibleValues) {
 		//With the New Data Table, the idValue will contain the long text instead of the id.
@@ -73,11 +86,9 @@ public class ExportImportStudyUtil {
 			}
 		}
 		
-		if (locationIds.isEmpty()) { //default is to show all
-			if (trialObservations != null) {
-				for (MeasurementRow trialObservation : trialObservations) {
-					locationIds.add((int) trialObservation.getLocationId());
-				}
+		if (locationIds.isEmpty() && trialObservations != null) {
+			for (MeasurementRow trialObservation : trialObservations) {
+				locationIds.add((int) trialObservation.getLocationId());
 			}
 		}
 		
@@ -86,7 +97,7 @@ public class ExportImportStudyUtil {
 	
     public static List<MeasurementRow> getApplicableObservations(Workbook workbook, List<MeasurementRow> observations,  List<Integer> instances) {
     	List<MeasurementRow> rows = null;
-    	if (instances != null && instances.size() != 0) {
+    	if (instances != null && !instances.isEmpty()) {
 	    	rows = new ArrayList<MeasurementRow>();
 	    	List<Integer> locationIds = getLocationIdsFromTrialInstances(workbook, instances);
 	    	for (MeasurementRow row : observations) {
@@ -99,4 +110,62 @@ public class ExportImportStudyUtil {
     	}
     	return rows;
     }
+    
+	public static String getSiteNameOfTrialInstance(MeasurementRow trialObservation, 
+			org.generationcp.middleware.service.api.FieldbookService fieldbookMiddlewareService)
+			throws MiddlewareQueryException {
+		if (trialObservation != null && trialObservation.getMeasurementVariables() != null) {
+			for (MeasurementData data : trialObservation.getDataList()) {
+				if (data.getMeasurementVariable().getTermId() == TermId.TRIAL_LOCATION.getId()) {
+					return "_" + data.getValue();
+				} else if (data.getMeasurementVariable().getTermId() == TermId.LOCATION_ID.getId()) {
+					return getSiteNameOfTrialInstanceBasedOnLocationID(
+							fieldbookMiddlewareService, data);
+				}
+			}
+		}
+		return "";
+	}
+
+	private static String getSiteNameOfTrialInstanceBasedOnLocationID(
+			org.generationcp.middleware.service.api.FieldbookService fieldbookMiddlewareService,
+			MeasurementData data) throws MiddlewareQueryException {
+		if (data.getValue() != null && !data.getValue().isEmpty() && NumberUtils.isNumber(data.getValue())) {
+			return "_" + fieldbookMiddlewareService.getLocationById(Integer.parseInt(data.getValue())).getLname();
+		} else {
+			return "";
+		}
+	}
+
+	public static boolean partOfRequiredColumns(int termId) {
+		for(int id : REQUIRED_COLUMNS){
+			if(termId == id){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static String getPropertyName(OntologyService ontologyService) {
+		String propertyName = "";
+		try {
+		    propertyName = ontologyService.getProperty(TermId.BREEDING_METHOD_PROP.getId()).getTerm().getName();
+		} catch (MiddlewareQueryException e) {
+			LOG.error(e.getMessage(), e);
+		}
+		return propertyName;
+	}
+	
+	public static boolean measurementVariableHasValue(MeasurementData dataCell) {
+		return dataCell.getMeasurementVariable() != null
+				&& dataCell.getMeasurementVariable().getPossibleValues() != null;
+	}
+	
+	public static boolean isColumnVisible(int termId, List<Integer> visibleColumns){
+		if(visibleColumns == null){
+			return true;
+		} else {
+			return partOfRequiredColumns(termId) || visibleColumns.contains(termId);
+		}
+	}
 }

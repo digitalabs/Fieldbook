9d138f5908c753dac69f4079bc2ca0786386b3bf
Cyrus Venn Casada <cvpcasada@outlook.com>
2014-12-19 12:58:35 +0800

    Stubs for LabelPrinting preset unit tests
    BMS-184
    
    Reviewer: Daniel Jao

---
+++ src/test/java/com/efficio/fieldbook/service/LabelPrintingServiceImplTest.java
@@ -0,0 +1,18 @@
+package com.efficio.fieldbook.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LabelPrintingServiceImplTest {
+
+	@Test
+	public void testGetAllLabelPrintingPresets() throws Exception {
+
+	}
+
+	@Test
+	public void testGetLabelPrintingPresetConfig() throws Exception {
+
+	}
+}
---
+++ src/test/java/com/efficio/fieldbook/service/WorkbenchServiceImplTest.java
@@ -0,0 +1,38 @@
+package com.efficio.fieldbook.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WorkbenchServiceImplTest {
+
+	@Test
+	public void testGetAllStandardPresets() throws Exception {
+
+	}
+
+	@Test
+	public void testGetStandardPresetByCrop() throws Exception {
+
+	}
+
+	@Test
+	public void testGetStandardPresetByCrop1() throws Exception {
+
+	}
+
+	@Test
+	public void testGetFieldbookWebTool() throws Exception {
+
+	}
+
+	@Test
+	public void testGetStandardPresetById() throws Exception {
+
+	}
+
+	@Test
+	public void testSaveOrUpdateStandardPreset() throws Exception {
+
+	}
+}

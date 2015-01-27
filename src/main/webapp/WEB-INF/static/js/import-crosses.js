14409fd3af43da9a382b255ea96bc3d4ccf51f50
Daniel Jao <daniel@efficio.us.com>
2015-01-27 15:22:37 +0800

    Added UI / JS changes to allow the popup of saving crosses list
    BMS-274
    
    Reviewer: Aldrin Batac

--- src/main/webapp/WEB-INF/static/js/import-crosses.js
+++ src/main/webapp/WEB-INF/static/js/import-crosses.js
@@ -67,6 +67,67 @@ var ImportCrosses = {
 					$('.crosses-list' + uniqueId+'-li').data('crosses-germplasm-list-id', crossesListId);
 				}
 			});
+		},
+		displayTabCrossesList: function (germplasmListId, crossesListId) {
+			'use script';			
+			var url = '/Fieldbook/SeedStoreManager/crosses/displayGermplasmDetails/' + germplasmListId;
+			url += '?isSnapshot=0';
+			
+			$.ajax({
+				url: url,
+				type: 'GET',
+				cache: false,
+				success: function(html) {
+					//ImportCrosses.displayCrossesList($(this).data('list-id'), $(this).data('list-id'), '', true);
+					
+					$('#saveListTreeModal').modal('hide');
+					$('#saveListTreeModal').data('is-save-crosses', '0');
+					$('#create-nursery-tabs .tab-pane.info').removeClass('active');
+					
+					var uniqueId,
+					close,
+					aHtml;
+					uniqueId = germplasmListId;
+					close = '<i class="glyphicon glyphicon-remove fbk-close-tab fbk-hide" id="'+uniqueId+'" onclick="javascript: closeAdvanceListTab(' + uniqueId +')"></i>';
+					aHtml = '<a id="advance-list'+uniqueId+'" role="tab" class="advanceList crossesList crossesList'+uniqueId+'" data-toggle="tab" href="#advance-list' + uniqueId + '" data-list-id="' + uniqueId + '">Crosses' + close + '</a>';
+					$('#create-nursery-tab-headers').append('<li id="advance-list' + uniqueId + '-li" class="advance-germplasm-items crosses-list">' + aHtml + '</li>');
+					$('#create-nursery-tabs').append('<div class="tab-pane info crosses-list'+uniqueId+'" id="advance-list' + uniqueId + '">' + html + '</div>');
+					$('a#advance-list'+uniqueId).tab('show');
+					$('#advance-list'+uniqueId+".tab-pane.info").addClass('active');
+					$('.nav-tabs').tabdrop('layout');
+					
+					$('a#advance-list'+uniqueId).on('click', function(){
+						$('#create-nursery-tabs .tab-pane.info').removeClass('active');
+						$('#advance-list'+uniqueId+".tab-pane.info").addClass('active');
+					});
+				}
+			});
+		},
+		openSaveListModal: function(){
+			'use strict';
+			var  germplasmTreeNode = $('#germplasmFolderTree').dynatree('getTree');
+			additionalLazyLoadUrl = '/1';
+			$.ajax(
+				{ 
+					url: '/Fieldbook/ListTreeManager/saveCrossesList/',
+					type: 'GET',
+					cache: false,
+					success: function(html) {
+						$('#saveGermplasmRightSection').html(html);
+						$('#saveListTreeModal').modal({
+							show: true,
+							keyboard: true,
+							backdrop: 'static'
+						});
+						$('#saveListTreeModal').data('is-save-crosses', '1');
+						//we preselect the program lists
+						if(germplasmTreeNode !== null && germplasmTreeNode.getNodeByKey('LOCAL') !== null){
+							germplasmTreeNode.getNodeByKey('LOCAL').activate();
+							germplasmTreeNode.getNodeByKey('LOCAL').expand()
+						}
+					}
+				}
+			);
 		}
 }
 

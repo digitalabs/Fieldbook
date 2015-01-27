ad351f3b3f6ea503da47ca95647e2d12e90f02da
Daniel Villafuerte <dannyvonline@gmail.com>
2015-01-27 17:22:18 +0800

    Front end and back end controller for cross import settings Updated Spring application context to provide access to necessary resource for controller
    BMS-268

---
+++ src/main/java/com/efficio/fieldbook/web/common/controller/CrossingSettingsController.java
@@ -0,0 +1,164 @@
+package com.efficio.fieldbook.web.common.controller;
+
+import com.efficio.fieldbook.service.api.WorkbenchService;
+import com.efficio.fieldbook.web.AbstractBaseFieldbookController;
+import com.efficio.fieldbook.web.common.bean.CrossImportSettings;
+import com.efficio.fieldbook.web.common.bean.UserCrossImportSelection;
+import org.generationcp.commons.constant.ToolSection;
+import org.generationcp.commons.context.ContextConstants;
+import org.generationcp.commons.context.ContextInfo;
+import org.generationcp.commons.service.SettingsPresetService;
+import org.generationcp.commons.settings.CrossSetting;
+import org.generationcp.middleware.exceptions.MiddlewareQueryException;
+import org.generationcp.middleware.manager.api.PresetDataManager;
+import org.generationcp.middleware.pojos.presets.ProgramPreset;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.WebUtils;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.bind.JAXBException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Daniel Villafuerte
+ * Date: 1/21/2015
+ * Time: 1:49 PM
+ */
+
+@Controller
+@RequestMapping(CrossingSettingsController.URL)
+public class CrossingSettingsController extends AbstractBaseFieldbookController {
+	private static final Logger LOG = LoggerFactory.getLogger(CrossingSettingsController.class);
+	public static final String URL = "/import/crosses";
+
+	@Resource
+	private WorkbenchService workbenchService;
+
+	@Resource
+	private PresetDataManager presetDataManager;
+
+	@Resource
+	private SettingsPresetService settingsPresetService;
+
+
+	@Override public String getContentName() {
+		return null;
+	}
+
+	@ResponseBody
+	@RequestMapping(value="/retrieveSettings", method = RequestMethod.GET, produces = "application/json")
+	public List<CrossImportSettings> getAvailableCrossImportSettings(HttpServletRequest request) {
+		List<CrossImportSettings> settings = new ArrayList<>();
+		/*settings.add(new CrossSetting());*/
+		try {
+		List<ProgramPreset> presets;
+
+			presets = presetDataManager.getProgramPresetFromProgramAndTool(getCurrentProgramID(request),
+					workbenchService.getFieldbookWebTool().getToolId().intValue(),
+					ToolSection.FBK_CROSS_IMPORT.name());
+
+			if (presets != null) {
+				for (ProgramPreset preset : presets) {
+					CrossSetting crossSetting = (CrossSetting) settingsPresetService.convertPresetFromXmlString(preset.getConfiguration(), CrossSetting.class);
+					CrossImportSettings importSettings = new CrossImportSettings(crossSetting.getName(), crossSetting.getCrossNameSetting().getPrefix(),
+							crossSetting.getBreedingMethodSetting().getMethodId(),
+							crossSetting.getCrossNameSetting().getSuffix(),
+							crossSetting.getCrossNameSetting().getNumOfDigits(),crossSetting.getCrossNameSetting().isAddSpaceBetweenSuffixAndCode(),
+							crossSetting.getCrossNameSetting().isAddSpaceBetweenPrefixAndCode(), crossSetting.getCrossNameSetting().getStartNumber(),
+							crossSetting.getCrossNameSetting().getSeparator());
+					settings.add(importSettings);
+				}
+			}
+
+		} catch (MiddlewareQueryException e) {
+			LOG.error(e.getMessage());
+		} catch (Exception e) {
+			LOG.error(e.getMessage());
+		}
+
+		return settings;
+	}
+
+	@ResponseBody
+	@RequestMapping(value="/submitAndSaveSetting", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+	public Map<String, Object> submitAndSaveCrossSettings(@RequestBody CrossSetting settings, HttpServletRequest request) {
+		Map<String, Object> returnVal = new HashMap<>();
+		try {
+			Integer programID = getCurrentProgramID(request);
+			saveCrossSetting(settings, programID);
+			return submitCrossSettings(settings);
+		} catch (MiddlewareQueryException e) {
+			LOG.error(e.getMessage());
+		} catch (JAXBException e) {
+			LOG.error(e.getMessage());
+		}
+
+		returnVal.put("success", "0");
+		return returnVal;
+	}
+
+	@ResponseBody
+	@RequestMapping(value="/submit", method = RequestMethod.POST)
+	public Map<String, Object> submitCrossSettings(@RequestBody CrossSetting settings) {
+		Map<String, Object> returnVal = new HashMap<>();
+
+		returnVal.put("success", new Integer(1));
+		return returnVal;
+	}
+
+	protected void saveCrossSetting(CrossSetting setting, Integer programID) throws MiddlewareQueryException,
+			JAXBException{
+
+		List<ProgramPreset> presets = presetDataManager
+				.getProgramPresetFromProgramAndTool(programID,
+						workbenchService.getFieldbookWebTool().getToolId().intValue(),
+						ToolSection.FBK_CROSS_IMPORT.name());
+
+		boolean found = false;
+		ProgramPreset forSaving = null;
+		for (ProgramPreset preset : presets) {
+			if (preset.getName().equals(setting.getName())) {
+				preset.setConfiguration(settingsPresetService.convertPresetSettingToXml(setting, CrossSetting.class));
+				found = true;
+				break;
+			}
+		}
+
+		if (!found) {
+			forSaving = new ProgramPreset();
+			forSaving.setName(setting.getName());
+			forSaving.setToolId(workbenchService.getFieldbookWebTool().getToolId().intValue());
+			forSaving.setProgramUuid(programID);
+			forSaving.setToolSection(ToolSection.FBK_CROSS_IMPORT.name());
+			forSaving.setConfiguration(settingsPresetService.convertPresetSettingToXml(setting, CrossSetting.class));
+		}
+
+
+		presetDataManager.saveOrUpdateProgramPreset(forSaving);
+	}
+
+	protected Integer getCurrentProgramID(HttpServletRequest request) {
+		final ContextInfo contextInfo = (ContextInfo) WebUtils
+				                    .getSessionAttribute(request,
+											ContextConstants.SESSION_ATTR_CONTEXT_INFO);
+		if (contextInfo != null) {
+			return contextInfo.getSelectedProjectId().intValue();
+		} else {
+			return 2;
+		}
+	}
+
+
+
+}

cb993b31d02c51433278346da9f81386589bdb43
Daniel Villafuerte <dannyvonline@gmail.com>
2015-03-10 15:58:24 +0800

    Updated ValueRangeValidatorTest to reflect change in validator, allowing blank / empty list of valid values, as well as allowing for skip if empty flags
    BMS-143
    
    Reviewer : Cyrus Venn Casada

--- src/test/java/com/efficio/fieldbook/util/parsing/validation/ValueRangeValidatorTest.java
+++ src/test/java/com/efficio/fieldbook/util/parsing/validation/ValueRangeValidatorTest.java
@@ -30,11 +30,6 @@ public class ValueRangeValidatorTest {
 	}
 
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testIllegalConstructorArgumentEmpty() {
-		dut = new ValueRangeValidator(new ArrayList<String>());
-	}
-
 	@Test
 	public void testValueInRange() {
 		assertTrue(dut.isParsedValueValid("B"));
@@ -43,5 +38,28 @@ public class ValueRangeValidatorTest {
 	@Test
 	public void testValueNotInRange() {
 		assertFalse(dut.isParsedValueValid("Z"));
+	}
+
+	@Test
+	public void testBlankValueAndSkipIfEmptyTrue() {
+		assertTrue(dut.isParsedValueValid(null));
+	}
+
+	@Test
+	public void testBlankValueAndSkipIfEmptyFalse() {
+		List<String> validValues = new ArrayList<>();
+		validValues.add("A");
+		validValues.add("B");
+		validValues.add("C");
+		dut = new ValueRangeValidator(validValues, false);
+
+		assertFalse(dut.isParsedValueValid(null));
+	}
+
+	@Test
+	public void testValueCheckAgainstBlankValid() {
+		dut = new ValueRangeValidator(null);
+
+		assertTrue(dut.isParsedValueValid("ZZ"));
 	}
 }

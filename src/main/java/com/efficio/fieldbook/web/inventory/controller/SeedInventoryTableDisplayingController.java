c8fc657cffab4657bb8b8cdb083b154f4ac82fa5
Daniel Villafuerte <dannyvonline@gmail.com>
2015-03-24 17:22:28 +0800

    Defined a parent class that contains the logic for displaying the table headers for seed store inventory.
    Extracted logic for header calculation from SeedStoreManagerController
    
    Made ImportAdvanceInventoryController a subclass of the new class so that it can properly display imported inventory items
    
    BMS-143
    
    Reviewer : Cyrus Venn Casada

---
+++ src/main/java/com/efficio/fieldbook/web/inventory/controller/SeedInventoryTableDisplayingController.java
@@ -0,0 +1,52 @@
+package com.efficio.fieldbook.web.inventory.controller;
+
+import com.efficio.fieldbook.web.AbstractBaseFieldbookController;
+import com.efficio.fieldbook.web.common.bean.TableHeader;
+import org.generationcp.commons.constant.ColumnLabels;
+import org.generationcp.middleware.manager.api.OntologyDataManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Daniel Villafuerte
+ * Date: 3/24/2015
+ * Time: 4:55 PM
+ */
+public abstract class SeedInventoryTableDisplayingController
+		extends AbstractBaseFieldbookController {
+	protected static final String TABLE_HEADER_LIST = "tableHeaderList";
+	/**
+	 * The ontology manager.
+	 */
+	@Resource protected OntologyDataManager ontologyDataManager;
+
+	/**
+	 * The message source.
+	 */
+	@Autowired
+	public MessageSource messageSource;
+
+	protected List<TableHeader> getSeedInventoryTableHeader(){
+		Locale locale = LocaleContextHolder.getLocale();
+		List<TableHeader> tableHeaderList = new ArrayList<TableHeader>();
+
+		tableHeaderList.add(new TableHeader(ColumnLabels.ENTRY_ID.getTermNameFromOntology(ontologyDataManager), messageSource.getMessage("seed.entry.number", null, locale)));
+		tableHeaderList.add(new TableHeader(ColumnLabels.DESIGNATION.getTermNameFromOntology(ontologyDataManager), messageSource.getMessage("seed.entry.designation", null, locale)));
+		tableHeaderList.add(new TableHeader(ColumnLabels.PARENTAGE.getTermNameFromOntology(ontologyDataManager), messageSource.getMessage("seed.entry.parentage", null, locale)));
+		tableHeaderList.add(new TableHeader(ColumnLabels.GID.getTermNameFromOntology(ontologyDataManager), messageSource.getMessage("seed.inventory.gid", null, locale)));
+		tableHeaderList.add(new TableHeader(ColumnLabels.SEED_SOURCE.getTermNameFromOntology(ontologyDataManager), messageSource.getMessage("seed.inventory.source", null, locale)));
+		tableHeaderList.add(new TableHeader(ColumnLabels.LOT_LOCATION.getTermNameFromOntology(ontologyDataManager), messageSource.getMessage("seed.inventory.table.location", null, locale)));
+		tableHeaderList.add(new TableHeader(ColumnLabels.AMOUNT.getTermNameFromOntology(ontologyDataManager), messageSource.getMessage("seed.inventory.amount", null, locale)));
+		tableHeaderList.add(new TableHeader(ColumnLabels.SCALE.getTermNameFromOntology(ontologyDataManager), messageSource.getMessage("seed.inventory.table.scale", null, locale)));
+		tableHeaderList.add(new TableHeader(ColumnLabels.COMMENT.getTermNameFromOntology(ontologyDataManager), messageSource.getMessage("seed.inventory.comment", null, locale)));
+
+		return tableHeaderList;
+	}
+}

6b25f3f3cf072dcb949b1671cc4a156016618336
Crisjaira Villarino <crisjaira.villarino@leafnode.io>
2015-04-29 14:11:34 +0800

    Update the GermplasmListController for the retrieval of Cross List Details and List Data Project
    BMS-651
    
    Reviewer: Daniel Jao

--- src/main/java/com/efficio/fieldbook/web/common/controller/GermplasmListController.java
+++ src/main/java/com/efficio/fieldbook/web/common/controller/GermplasmListController.java
@@ -1,11 +1,18 @@
 package com.efficio.fieldbook.web.common.controller;
 
-import com.efficio.fieldbook.web.common.bean.TableHeader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
 import org.generationcp.commons.constant.ColumnLabels;
 import org.generationcp.middleware.domain.gms.GermplasmListType;
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.manager.api.GermplasmListManager;
 import org.generationcp.middleware.manager.api.OntologyDataManager;
+import org.generationcp.middleware.pojos.GermplasmList;
 import org.generationcp.middleware.pojos.ListDataProject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,11 +24,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
+import com.efficio.fieldbook.web.common.bean.TableHeader;
 
 /**
  * Created by IntelliJ IDEA.
@@ -54,21 +57,52 @@ public class GermplasmListController {
 		processGermplasmList(listId, GermplasmListType.ADVANCED.name(), req, model);
 		return "/NurseryManager/savedFinalAdvanceList";
 	}
+	
+	@RequestMapping(value = "/crosses/{listId}", method = RequestMethod.GET)
+	public String displayCrossGermplasmList(@PathVariable Integer listId, HttpServletRequest req,
+			Model model) {
+		processGermplasmList(listId, GermplasmListType.CROSSES.name(), req, model);
+		return "/NurseryManager/savedFinalCrossesList";
+	}
 
 	protected void processGermplasmList(Integer listId, String germplasmListType,
 			HttpServletRequest req, Model model) {
 		try {
-			List<ListDataProject> listData = germplasmListManager.retrieveSnapshotListData(listId);
-			model.addAttribute(TABLE_HEADER_LIST, getGermplasmListTableHeaders());
+			GermplasmList germplasmList = germplasmListManager.getGermplasmListById(listId);
+			List<ListDataProject> listData = getListDataProjectByListType(listId,germplasmListType);
+			
+			model.addAttribute(TABLE_HEADER_LIST, getGermplasmListTableHeaders(germplasmListType));
 			model.addAttribute(GERMPLASM_LIST, listData);
 			model.addAttribute("totalNumberOfGermplasms", listData.size());
 			model.addAttribute("listId", listId);
+			model.addAttribute("listName", germplasmList.getName());
+			model.addAttribute("listNotes", germplasmList.getNotes());
+			model.addAttribute("listType", germplasmList.getType());
+			
 		} catch (MiddlewareQueryException e) {
 			LOG.error(e.getMessage(), e);
 		}
 	}
 
-	protected List<TableHeader> getGermplasmListTableHeaders() {
+	private List<ListDataProject> getListDataProjectByListType(Integer listId,
+			String germplasmListType) {
+		List<ListDataProject> listData = new ArrayList<ListDataProject>();
+		
+		try {	
+			if(germplasmListType.equals(GermplasmListType.ADVANCED.name())){
+				listData = germplasmListManager.retrieveSnapshotListData(listId);
+			} else if(germplasmListType.equals(GermplasmListType.CROSSES.name())){
+				listData = germplasmListManager.retrieveSnapshotListDataWithParents(listId);
+			}
+		
+		} catch (MiddlewareQueryException e) {
+			LOG.error(e.getMessage(),e);
+		}
+		
+		return listData;
+	}
+
+	protected List<TableHeader> getGermplasmListTableHeaders(String germplasmListType) {
 		Locale locale = LocaleContextHolder.getLocale();
 		List<TableHeader> tableHeaderList = new ArrayList<>();
 
@@ -81,12 +115,37 @@ public class GermplasmListController {
 		tableHeaderList.add(new TableHeader(
 				ColumnLabels.PARENTAGE.getTermNameFromOntology(ontologyDataManager),
 				messageSource.getMessage("seed.entry.parentage", null, locale)));
-		tableHeaderList
-				.add(new TableHeader(ColumnLabels.GID.getTermNameFromOntology(ontologyDataManager),
+		
+		if(germplasmListType.equals(GermplasmListType.CROSSES.name())){
+			tableHeaderList.add(new TableHeader(
+					ColumnLabels.FEMALE_PARENT.getTermNameFromOntology(ontologyDataManager),
+					messageSource.getMessage("germplasm.list.female.parent", null, locale)));
+			
+			tableHeaderList.add(new TableHeader(
+					ColumnLabels.FGID.getTermNameFromOntology(ontologyDataManager),
+					messageSource.getMessage("germplasm.list.fgid", null, locale)));
+			
+			tableHeaderList.add(new TableHeader(
+					ColumnLabels.MALE_PARENT.getTermNameFromOntology(ontologyDataManager),
+					messageSource.getMessage("germplasm.list.male.parent", null, locale)));
+			
+			tableHeaderList.add(new TableHeader(
+					ColumnLabels.MGID.getTermNameFromOntology(ontologyDataManager),
+					messageSource.getMessage("germplasm.list.mgid", null, locale)));
+		}
+		
+		tableHeaderList.add(new TableHeader(ColumnLabels.GID.getTermNameFromOntology(ontologyDataManager),
 						messageSource.getMessage("seed.inventory.gid", null, locale)));
 		tableHeaderList.add(new TableHeader(
 						ColumnLabels.SEED_SOURCE.getTermNameFromOntology(ontologyDataManager),
 						messageSource.getMessage("seed.inventory.source", null, locale)));
+		
+		
+		if(germplasmListType.equals(GermplasmListType.CROSSES.name())){
+			tableHeaderList.add(new TableHeader(
+					messageSource.getMessage("germplasm.list.cross.duplicate", null, locale),
+					messageSource.getMessage("germplasm.list.cross.duplicate", null, locale)));
+		}
 		return tableHeaderList;
 	}
 }

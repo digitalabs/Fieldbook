aede80901e80b7c66f8e335df26bc77c65457b50
Daniel Villafuerte <dannyvonline@gmail.com>
2015-05-07 16:05:17 +0800

    Javascript functions used when saving the stock list as well as display of stock list
    BMS-657
    
    Reviewer : Mae Turiana

--- src/main/webapp/WEB-INF/static/js/stock-list.js
+++ src/main/webapp/WEB-INF/static/js/stock-list.js
@@ -11,6 +11,93 @@ if (typeof StockIDFunctions === 'undefined') {
             $('#stockIDModalSourceListID').val(sourceId);
         },
 
+        saveStockList : function(listType) {
+            var stockGenerationSettings = {
+                // use the default identifier of SID when user provides no value
+                breederIdentifier : $('#breederIdentifierField').val().trim() === '' ? 'SID': $('#breederIdentifierField').val().trim(),
+                // default separator for now is -
+                separator : StockIDFunctions.defaultSeparator
+            };
+
+            $.ajax(
+                {
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    url: '/Fieldbook/stock/generateStockList/' + listType + '/' + $('#stockIDModalSourceListID').val(),
+                    type: 'POST',
+                    cache: false,
+                    contentType : 'application/json',
+                    data : JSON.stringify(stockGenerationSettings),
+                    success: function (result) {
+                        if (result.isSuccess === '1') {
+
+                        } else {
+
+                        }
+                    }
+                }
+            );
+
+        },
+
+        generateStockListTabIfNecessary : function(listId) {
+            'use strict';
+
+            var url = '/Fieldbook/stock/generateStockTabIfNecessary/' + listId;
+
+            $.ajax({
+                url: url,
+                type: 'GET',
+                cache: false,
+                success: function(html) {
+                    if (html && html.length > 0) {
+                        $('#advance-list' + listId + '-li').after(html);
+                        $('#stock-list-anchor' + listId).on('shown.bs.tab', function() {
+                            if ($(this).data('has-loaded') !== '1') {
+                                $(this).data('has-loaded', '1');
+                                StockIDFunctions.displayStockList($(this).data('list-id'));
+                            }
+                        });
+                    }
+                }
+            });
+        },
+
+        displayStockList : function(listId) {
+            'use strict';
+
+            var url = '/Fieldbook/germplasm/list/stock/' + listId;
+
+            $.ajax({
+                url: url,
+                type: 'GET',
+                cache: false,
+                success: function(html) {
+                    $('#stock-list' + listId).html(html);
+                    //we just show the button
+                    $('.export-advance-list-action-button').removeClass('fbk-hide');
+                    $('#stock-list' + listId+'-li').addClass('advance-germplasm-items');
+                    $('#stock-list' + listId+'-li').data('advance-germplasm-list-id', listId);
+                }
+            });
+        },
+
+        closeStockList : function(listId) {
+            'use strict';
+            $('li#stock-list' + listId + '-li').remove();
+
+            if($('#list'+listId).length === 1){
+                $('#list' + listId).remove();
+            }
+
+            setTimeout(function() {
+                $('#create-nursery-tab-headers li:eq(0) a').tab('show');
+                $('.nav-tabs').tabdrop('layout');
+            }, 100);
+        },
+
         openSaveListModal: function () {
             'use strict';
 

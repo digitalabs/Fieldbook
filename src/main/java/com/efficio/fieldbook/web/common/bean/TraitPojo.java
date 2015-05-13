dd245f6ea0266dcc674733eadb038e0b3a6b89b7
Daniel Jao <daniel@efficio.us.com>
2015-05-13 14:50:05 +0800

    Fixed the issue with the add variable modal after merging the cimmyt jasper branch, since it included a library for reflection, it affected the pojo to json process
    BMS-850
    
    Reviewer: Crisjaira Vilarino

---
+++ src/main/java/com/efficio/fieldbook/web/common/bean/TraitPojo.java
@@ -0,0 +1,25 @@
+package com.efficio.fieldbook.web.common.bean;
+
+public class TraitPojo {
+	private Integer traitClassId;
+	
+	private String traitClassName;
+
+	public Integer getTraitClassId() {
+		return traitClassId;
+	}
+
+	public void setTraitClassId(Integer traitClassId) {
+		this.traitClassId = traitClassId;
+	}
+
+	public String getTraitClassName() {
+		return traitClassName;
+	}
+
+	public void setTraitClassName(String traitClassName) {
+		this.traitClassName = traitClassName;
+	}
+
+	
+}

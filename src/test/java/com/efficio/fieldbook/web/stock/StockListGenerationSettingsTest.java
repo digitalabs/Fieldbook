1bfd8e66ac2d08b97287fc9904d50dcf3c2897dc
Daniel Villafuerte <dannyvonline@gmail.com>
2015-05-14 12:56:12 +0800

    Renamed StockIDGenerationSettings POJO gto StockListGenerationSettings
    BMS-654
    
     Reviewer : Cyrus Venn Casada

--- src/test/java/com/efficio/fieldbook/web/stock/StockListGenerationSettingsTest.java
+++ src/test/java/com/efficio/fieldbook/web/stock/StockListGenerationSettingsTest.java
@@ -12,38 +12,38 @@ import static org.junit.Assert.*;
  */
 
 @RunWith(JUnit4.class)
-public class StockIDGenerationSettingsTest {
-    StockIDGenerationSettings dut;
+public class StockListGenerationSettingsTest {
+    StockListGenerationSettings dut;
 
     public static final String TEST_IDENTIFIER = "ABC";
     public static final String TEST_SEPARATOR = "-";
 
     @Before
     public void setUp() throws Exception {
-        dut = new StockIDGenerationSettings();
+        dut = new StockListGenerationSettings();
     }
 
     @Test
     public void testValidateNumbersInIdentifier() {
         dut.setBreederIdentifier("AB12");
-        assertEquals(StockIDGenerationSettings.NUMBERS_FOUND, dut.validateSettings());
+        assertEquals(StockListGenerationSettings.NUMBERS_FOUND, dut.validateSettings());
     }
 
     @Test
     public void testValidateSpacesInIdentifier() {
         dut.setBreederIdentifier("AB C");
-        assertEquals(StockIDGenerationSettings.SPACE_FOUND, dut.validateSettings());
+        assertEquals(StockListGenerationSettings.SPACE_FOUND, dut.validateSettings());
     }
 
     @Test
     public void testValidBreederIdentifier() {
         dut.setBreederIdentifier("ABC");
-        assertEquals(StockIDGenerationSettings.VALID_SETTINGS, dut.validateSettings());
+        assertEquals(StockListGenerationSettings.VALID_SETTINGS, dut.validateSettings());
     }
 
     @Test
     public void testCopySettings() {
-        StockIDGenerationSettings copy = new StockIDGenerationSettings(TEST_IDENTIFIER, TEST_SEPARATOR);
+        StockListGenerationSettings copy = new StockListGenerationSettings(TEST_IDENTIFIER, TEST_SEPARATOR);
         dut.copy(copy);
 
         assertEquals(TEST_IDENTIFIER, dut.getBreederIdentifier());

6254d7b3bfe82ed42eca7686345b4d023a772984
Daniel Villafuerte <dannyvonline@gmail.com>
2015-05-19 15:12:25 +0800

    Cleanup : removed duplication  of code by calling existing method
     Reviewer : Mae Turiana

--- src/main/java/com/efficio/fieldbook/web/common/util/GermplasmListWebUIUtil.java
+++ src/main/java/com/efficio/fieldbook/web/common/util/GermplasmListWebUIUtil.java
@@ -33,16 +33,6 @@ public class GermplasmListWebUIUtil {
     public static String getCSSClassForDuplicateType(InventoryDetails inventoryDetails) {
         ListDataProject project = new ListDataProject();
         project.setDuplicate(inventoryDetails.getDuplicate());
-        if (project.isPedigreeDupe()) {
-            return PEDIGREE_DUPE_CLASS;
-        } else if (project.isPedigreeRecip()) {
-            return PEDIGREE_RECIPROCAL_CLASS;
-        } else if (project.isPlotDupe()) {
-            return PLOT_DUPE_CLASS;
-        } else if (project.isPlotRecip()) {
-            return PLOT_RECIPROCAL_CLASS;
-        } else {
-            return "";
-        }
+        return getCSSClassForDuplicateType(project);
     }
 }

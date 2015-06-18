7b17c727cdce363a5cc7aa754b671f194c78d385
Daniel Villafuerte <dannyvonline@gmail.com>
2015-06-18 14:51:48 +0800

    Refactored ProcessCodeFactory to make it easier to add expressions via component scan
    BMS-466
    
    Reviewer : Jai Villarino

--- src/main/java/com/efficio/fieldbook/web/naming/impl/ProcessCodeFactory.java
+++ src/main/java/com/efficio/fieldbook/web/naming/impl/ProcessCodeFactory.java
@@ -2,31 +2,25 @@
 package com.efficio.fieldbook.web.naming.impl;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import com.efficio.fieldbook.web.naming.expression.Expression;
 
 public class ProcessCodeFactory {
 
-	private List<Expression> registeredExpressions;
-
 	private Map<String, Expression> expressionMap;
 
 	public void init() {
-		assert this.registeredExpressions != null;
 
 		this.expressionMap = new HashMap<>();
-		for (Expression registeredExpression : this.registeredExpressions) {
-			this.expressionMap.put(registeredExpression.getExpressionKey(), registeredExpression);
-		}
+
 	}
 
 	public Expression create(String key) {
 		return this.expressionMap.get(key.toUpperCase());
 	}
 
-	public void setRegisteredExpressions(List<Expression> registeredExpressions) {
-		this.registeredExpressions = registeredExpressions;
+	public void addExpression(Expression expression) {
+		expressionMap.put(expression.getExpressionKey(), expression);
 	}
 }

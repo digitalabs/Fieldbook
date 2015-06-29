7ccabafbf94a4261f52e42ae7270f193a2d49f15
Daniel Jao <daniel@efficio.us.com>
2015-06-29 17:37:03 +0800

    Reverting code change since the fix in measurement variable get label would handle this one already
    BMS-1071
    
    Reviewer: Mae Turiana

--- src/main/java/com/efficio/fieldbook/web/common/service/impl/ExcelExportStudyServiceImpl.java
+++ src/main/java/com/efficio/fieldbook/web/common/service/impl/ExcelExportStudyServiceImpl.java
@@ -366,9 +366,6 @@ public class ExcelExportStudyServiceImpl implements ExcelExportStudyService {
 				if (isVariate) {
 					variable.setLabel(ExcelExportStudyServiceImpl.PLOT);
 				}
-				if("".equalsIgnoreCase(variable.getLabel()) && variable.getRole() != null){
-					variable.setLabel(variable.getRole().getLabelList().get(0));
-				}
 				
 				this.writeSectionRow(rowNumIndex++, xlsSheet, variable);
 			}

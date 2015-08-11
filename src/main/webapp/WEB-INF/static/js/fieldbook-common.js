851b72510aec949d1eeba9974308819aedfec82c
Cyrus Venn Casada <cvpcasada@outlook.com>
2015-08-11 13:53:07 +0800

    Updated the help html
    BMS-1384
    
    Reviewer: Crisjaira Villarino

--- src/main/webapp/WEB-INF/static/js/fieldbook-common.js
+++ src/main/webapp/WEB-INF/static/js/fieldbook-common.js
@@ -64,11 +64,10 @@ $(function() {
 		.on('click','.fbk-help',function() {
 		   var helpModule = $(this).data().helpLink;
 			$.get('/ibpworkbench/controller/help/getUrl/' + helpModule).success(function(helpUrl) {
-				console.log(helpUrl);
 				if (!helpUrl || !helpUrl.length) {
 					$.when(
 						$.get('/ibpworkbench/controller/help/headerText'),
-						$.get('/ibpworkbench/VAADIN/themes/gcp-default/layouts/help.html')
+						$.get('/ibpworkbench/VAADIN/themes/gcp-default/layouts/help_not_installed.html')
 					).done(function(headerText,helpHtml) {
 							bootbox.dialog({
 								title: headerText[0],

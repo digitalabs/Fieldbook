cc43e2d6a1bc27d9245ea6c108e3a71ed2084cdf
Crisjaira Villarino <crisjaira.villarino@leafnode.io>
2015-11-11 14:29:19 +0800

    Added Null Pointer Checking when retrieving details fro selection variates
    Issue: BMS-1608
    Reviewer: Cyrus Venn Casada

--- src/main/java/com/efficio/fieldbook/web/nursery/controller/CreateNurseryController.java
+++ src/main/java/com/efficio/fieldbook/web/nursery/controller/CreateNurseryController.java
@@ -332,11 +332,15 @@ public class CreateNurseryController extends SettingsController {
 
 		List<SettingDetail> baselineTraits = form.getBaselineTraitVariables();
 		final List<SettingDetail> baselineTraitsSession = this.userSelection.getSelectionVariates();
+
 		// Make sure that selection varieties are marked as selection methods so that this information can be used to save data correctly
 		// into the projectprop table.
-		for (final SettingDetail settingDetail : form.getSelectionVariatesVariables()) {
-			settingDetail.setVariableType(VariableType.SELECTION_METHOD);
+		if (form.getSelectionVariatesVariables() != null) {
+			for (final SettingDetail settingDetail : form.getSelectionVariatesVariables()) {
+				settingDetail.setVariableType(VariableType.SELECTION_METHOD);
+			}
 		}
+
 		if (baselineTraits == null && form.getSelectionVariatesVariables() != null) {
 			baselineTraits = form.getSelectionVariatesVariables();
 			this.userSelection.getBaselineTraitsList().addAll(baselineTraitsSession);

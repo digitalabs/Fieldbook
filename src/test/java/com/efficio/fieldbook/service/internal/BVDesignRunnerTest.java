5df6521e3633a2dfe6e9aeb23c661147c53ad895
srajwadi <sunny@leafnode.io>
2015-12-11 15:51:32 +0530

    Adding Tests for ResolvableIncompleteBlockDesign with entry number and ResolvableRowAndColumnDesign with entry number.
    Issue: BMS-1930
    Reviewer: Keyur

--- src/test/java/com/efficio/fieldbook/service/internal/BVDesignRunnerTest.java
+++ src/test/java/com/efficio/fieldbook/service/internal/BVDesignRunnerTest.java
@@ -17,11 +17,11 @@ public class BVDesignRunnerTest {
 
 	@Test
 	public void testGetXMLStringForRandomizedCompleteBlockDesign() {
-		List<String> treatmentFactor = new ArrayList<String>();
+		List<String> treatmentFactor = new ArrayList<>();
 		treatmentFactor.add("ENTRY_NO");
 		treatmentFactor.add("FERTILIZER");
 
-		List<String> levels = new ArrayList<String>();
+		List<String> levels = new ArrayList<>();
 		levels.add("24");
 		levels.add("3");
 
@@ -158,6 +158,115 @@ public class BVDesignRunnerTest {
 		BVDesignRunner runner = new BVDesignRunner();
 		String xmlString = runner.getXMLStringForDesign(mainDesign);
 
+		this.assertXMLStringEqualsExpected(mainDesign, expectedString, xmlString);
+	}
+
+
+	@Test
+	public void testGetXMLStringForResolvableIncompleteBlockDesignWithEntryNumber() {
+		MainDesign mainDesign = ExpDesignUtil.createResolvableIncompleteBlockDesign("6", "24", "2", "ENTRY_NO", "Reps", "Subblocks", "Plots", 301, 245, "0", "", "", false);
+
+		String expectedString =
+				"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Templates><Template name=\"ResolvableIncompleteBlock\">"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.SEED_PARAM
+						+ "\" value=\":seedValue\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.BLOCKSIZE_PARAM
+						+ "\" value=\"6\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.NTREATMENTS_PARAM
+						+ "\" value=\"24\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.NREPLICATES_PARAM
+						+ "\" value=\"2\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.TREATMENTFACTOR_PARAM
+						+ "\" value=\"ENTRY_NO\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.INITIAL_TREATMENT_NUMBER_PARAM
+						+ "\" value=\"245\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.REPLICATEFACTOR_PARAM
+						+ "\" value=\"Reps\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.BLOCKFACTOR_PARAM
+						+ "\" value=\"Subblocks\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.PLOTFACTOR_PARAM
+						+ "\" value=\"Plots\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.INITIAL_PLOT_NUMBER_PARAM
+						+ "\" value=\"301\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.NBLATIN_PARAM
+						+ "\" value=\"0\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.TIMELIMIT_PARAM
+						+ "\" value=\"" + AppConstants.EXP_DESIGN_TIME_LIMIT.getString() + "\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.OUTPUTFILE_PARAM
+						+ "\" value=\":outputFile\"/></Template></Templates>";
+
+		BVDesignRunner runner = new BVDesignRunner();
+		String xmlString = runner.getXMLStringForDesign(mainDesign);
+
+		this.assertXMLStringEqualsExpected(mainDesign, expectedString, xmlString);
+	}
+
+	@Test
+	public void testGetXMLStringForResolvableRowColumnDesignWithEntryNumber() {
+		MainDesign mainDesign = ExpDesignUtil.createResolvableRowColDesign("24", "2", "5", "10", "ENTRY_NO", "Reps", "Rows", "Columns",
+				"Plots", 301, 245, "0", "0", "", "", false);
+
+		String expectedString =
+				"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Templates><Template name=\"ResolvableRowColumn\">"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.SEED_PARAM
+						+ "\" value=\":seedValue\"/><Parameter name=\""
+						+ ExpDesignUtil.NTREATMENTS_PARAM
+						+ "\" value=\"24\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.NREPLICATES_PARAM
+						+ "\" value=\"2\"/><Parameter name=\""
+						+ ExpDesignUtil.NROWS_PARAM
+						+ "\" value=\"5\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.NCOLUMNS_PARAM
+						+ "\" value=\"10\"/><Parameter name=\""
+						+ ExpDesignUtil.TREATMENTFACTOR_PARAM
+						+ "\" value=\"ENTRY_NO\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.INITIAL_TREATMENT_NUMBER_PARAM
+						+ "\" value=\"245\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.REPLICATEFACTOR_PARAM
+						+ "\" value=\"Reps\"/><Parameter name=\""
+						+ ExpDesignUtil.ROWFACTOR_PARAM
+						+ "\" value=\"Rows\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.COLUMNFACTOR_PARAM
+						+ "\" value=\"Columns\"/><Parameter name=\""
+						+ ExpDesignUtil.PLOTFACTOR_PARAM
+						+ "\" value=\"Plots\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.INITIAL_PLOT_NUMBER_PARAM
+						+ "\" value=\"301\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.NRLATIN_PARAM
+						+ "\" value=\"0\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.NCLATIN_PARAM
+						+ "\" value=\"0\"/>"
+						+ "<Parameter name=\""
+						+ ExpDesignUtil.TIMELIMIT_PARAM
+						+ "\" value=\""
+						+ AppConstants.EXP_DESIGN_TIME_LIMIT.getString()
+						+ "\"/>"
+						+ "<Parameter name=\"" + ExpDesignUtil.OUTPUTFILE_PARAM + "\" value=\":outputFile\"/></Template></Templates>";
+
+		BVDesignRunner runner = new BVDesignRunner();
+		String xmlString = runner.getXMLStringForDesign(mainDesign);
 		this.assertXMLStringEqualsExpected(mainDesign, expectedString, xmlString);
 	}
 

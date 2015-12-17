92bba223d530471b6b9aab49f09e770d6245481a
Daniel Villafuerte <dannyvonline@gmail.com>
2015-12-17 17:27:44 +0800

    Added processing of categorical variables during retrieval of selection trait value
    BMS-2064
    Reviewer : Cyrus  Venn Casada

--- src/main/java/com/efficio/fieldbook/web/naming/expression/dataprocessor/SelectionTraitExpressionDataProcessor.java
+++ src/main/java/com/efficio/fieldbook/web/naming/expression/dataprocessor/SelectionTraitExpressionDataProcessor.java
@@ -2,13 +2,16 @@ package com.efficio.fieldbook.web.naming.expression.dataprocessor;
 
 import com.efficio.fieldbook.web.nursery.bean.AdvancingNursery;
 import com.efficio.fieldbook.web.nursery.bean.AdvancingSource;
+import org.generationcp.commons.spring.util.ContextUtil;
 import org.generationcp.middleware.domain.dms.Study;
 import org.generationcp.middleware.domain.etl.MeasurementData;
 import org.generationcp.middleware.domain.etl.MeasurementRow;
 import org.generationcp.middleware.domain.etl.MeasurementVariable;
 import org.generationcp.middleware.domain.etl.Workbook;
+import org.generationcp.middleware.manager.ontology.api.OntologyVariableDataManager;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Component
@@ -16,11 +19,17 @@ public class SelectionTraitExpressionDataProcessor implements ExpressionDataProc
 
     public static final String SELECTION_TRAIT_PROPERTY = "Selection Criteria";
 
+    @Resource
+    private OntologyVariableDataManager ontologyVariableDataManager;
+
+    @Resource
+    private ContextUtil contextUtil;
+
     @Override
     public void processEnvironmentLevelData(AdvancingSource source, Workbook workbook, AdvancingNursery nurseryInfo, Study study) {
         for (MeasurementVariable condition : workbook.getConditions()) {
             if (condition.getProperty().equalsIgnoreCase(SELECTION_TRAIT_PROPERTY)) {
-                source.setSelectionTraitValue(condition.getValue());
+                source.setSelectionTraitValue(extractVariableValue(condition));
             }
         }
     }
@@ -31,9 +40,25 @@ public class SelectionTraitExpressionDataProcessor implements ExpressionDataProc
 
         for (MeasurementData measurementData : rowData) {
             if (measurementData.getMeasurementVariable().getProperty().equalsIgnoreCase(SELECTION_TRAIT_PROPERTY)) {
-                source.setSelectionTraitValue(measurementData.getValue());
+                source.setSelectionTraitValue(extractDataValue(measurementData));
             }
         }
     }
 
+    protected String extractVariableValue(MeasurementVariable variable) {
+        if (variable.getDataType().equalsIgnoreCase(MeasurementVariable.CATEGORICAL_VARIABLE_TYPE)) {
+            return this.ontologyVariableDataManager.retrieveVariableCategoricalValue(contextUtil.getCurrentProgramUUID(), variable.getTermId(), Integer.parseInt(variable.getValue()));
+        } else {
+            return variable.getValue();
+        }
+    }
+
+    protected String extractDataValue(MeasurementData measurementData) {
+        if (measurementData.getMeasurementVariable().getDataType().equalsIgnoreCase(MeasurementVariable.CATEGORICAL_VARIABLE_TYPE)) {
+            return this.ontologyVariableDataManager.retrieveVariableCategoricalValue(contextUtil.getCurrentProgramUUID(),
+                    measurementData.getMeasurementVariable().getTermId(), Integer.parseInt(measurementData.getValue()));
+        } else {
+            return measurementData.getValue();
+        }
+    }
 }

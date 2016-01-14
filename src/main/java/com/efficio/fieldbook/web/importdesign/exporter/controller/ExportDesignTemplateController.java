181392ef2f471f8ecc88da1eb7ccae76d3887436
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2016-01-14 15:19:23 +0800

    Added controller for Export Design Template Added tests for the ExportDesignTemplateController
    BMS-978
    
    Reviewer: Crisjaira Villarino

---
+++ src/main/java/com/efficio/fieldbook/web/importdesign/exporter/controller/ExportDesignTemplateController.java
@@ -0,0 +1,63 @@
+package com.efficio.fieldbook.web.importdesign.exporter.controller;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.generationcp.middleware.util.ResourceFinder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.efficio.fieldbook.web.AbstractBaseFieldbookController;
+import com.efficio.fieldbook.web.importdesign.controller.DesignImportController;
+
+@Controller
+@RequestMapping(ExportDesignTemplateController.URL)
+public class ExportDesignTemplateController  extends AbstractBaseFieldbookController {
+	
+	public static final String URL = "/DesignTemplate";
+	private static final String DESIGN_TEMPLATE = "DesignTemplate.csv";
+	private static final String OUTPUT_FILENAME = "outputFilename";	
+	private static final Logger LOG = LoggerFactory.getLogger(ExportDesignTemplateController.class);
+	public static final String IS_SUCCESS = "isSuccess";
+	private static final String ERROR_MESSAGE = "errorMessage";
+	
+	@Resource
+	private MessageSource messageSource;
+	
+	@Resource
+	private ResourceFinder resourceFinder;
+	
+	@ResponseBody
+	@RequestMapping(value = "/export", method = RequestMethod.GET)
+	public Map<String, Object> exportDesignTemplate(final HttpServletResponse response, final HttpServletRequest req){		
+		File designTemplateFile;
+		final Map<String, Object> results = new HashMap<String, Object>();
+		try {
+			designTemplateFile = new File(resourceFinder.locateFileWrapper(ExportDesignTemplateController.DESIGN_TEMPLATE).getFile());
+			results.put(OUTPUT_FILENAME, designTemplateFile.getAbsolutePath());
+			results.put(ExportDesignTemplateController.IS_SUCCESS, 1);
+		} catch (FileNotFoundException e) {
+			ExportDesignTemplateController.LOG.error(e.getMessage(), e);
+			results.put(DesignImportController.IS_SUCCESS, 0);
+			results.put(ERROR_MESSAGE, this.messageSource.getMessage("common.error.file.not.found", null, Locale.ENGLISH));
+		}
+		return results;
+	}
+	
+	@Override
+	public String getContentName() {	
+		return null;
+	}
+}
---
+++ src/test/java/com/efficio/fieldbook/web/common/controller/ExportDesignTemplateControllerTest.java
@@ -0,0 +1,67 @@
+package com.efficio.fieldbook.web.common.controller;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.generationcp.middleware.util.ResourceFinder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.context.MessageSource;
+
+import com.efficio.fieldbook.web.importdesign.exporter.controller.ExportDesignTemplateController;
+
+import junit.framework.Assert;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ExportDesignTemplateControllerTest {
+	
+	private static String DESIGN_TEMPLATE = "DesignTemplate.csv";
+
+	private static final String OUTPUT_FILENAME = "outputFilename";
+
+	private static final Object ERROR_MESSAGE = "errorMessage";	
+	
+	@Mock
+	private HttpServletRequest req;
+
+	@Mock
+	private HttpServletResponse resp;
+	
+	@Mock
+	private ResourceFinder resourceFinder;
+	
+	@Mock
+	private MessageSource messageSource;
+	
+	@InjectMocks
+	ExportDesignTemplateController exportDesignTemplateController;
+	
+	@Test
+	public void testExportDesignTemplateSuccess() throws FileNotFoundException, UnsupportedEncodingException{
+		File designTemplateFile = new File(ResourceFinder.locateFile(ExportDesignTemplateControllerTest.DESIGN_TEMPLATE ).getFile());
+		Mockito.when(resourceFinder.locateFileWrapper(Matchers.anyString())).thenReturn(ResourceFinder.locateFile(ExportDesignTemplateControllerTest.DESIGN_TEMPLATE ));
+		Map<String, Object> results = this.exportDesignTemplateController.exportDesignTemplate(this.resp, this.req);
+		
+		Assert.assertEquals("The absolute path should be " + designTemplateFile.getAbsolutePath(), designTemplateFile.getAbsolutePath(), results.get(ExportDesignTemplateControllerTest.OUTPUT_FILENAME));
+	}
+	
+	@Test
+	public void testExportDesignTemplateFileNotFound() throws FileNotFoundException, UnsupportedEncodingException{
+		Mockito.when(resourceFinder.locateFileWrapper(Matchers.anyString())).thenThrow(new FileNotFoundException());
+		Mockito.when(this.messageSource.getMessage("common.error.file.not.found", null, Locale.ENGLISH)).thenReturn("File Not Found");
+		Map<String, Object> results = this.exportDesignTemplateController.exportDesignTemplate(this.resp, this.req);
+		
+		Assert.assertNotNull("There should be an error message.", results.get(ExportDesignTemplateControllerTest.ERROR_MESSAGE));
+	}
+}

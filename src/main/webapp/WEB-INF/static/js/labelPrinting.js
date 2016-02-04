d8e015c838e6ad706d8404afb45ed7f237faebbb
Cyrus Venn Casada <cvpcasada@outlook.com>
2016-02-04 14:09:49 +0800

    Fix filename invalid character validation
    BMS-587
    
    Reviewer: Aldrin Batac

--- src/main/webapp/WEB-INF/static/js/labelPrinting.js
+++ src/main/webapp/WEB-INF/static/js/labelPrinting.js
@@ -518,10 +518,10 @@ LabelPrinting = {
 
 		}
 		var data = $safeId('#userLabelPrinting.filename').val();
-		var isValid = /[:\\\\/*?|<>]*$/i.test(data);
+		var isValid = /^[^\\\/\:\*\?\"\<\>\|\.]+(\.[^\\\/\:\*\?\"\<\>\|\.]+)+$/.test(data);
 
 		if (!isValid) {
-			showInvalidInputMessage(filenameErrorCharacter);
+			showInvalidInputMessage(filenameIsInvalid);
 			moveToTopScreen();
 			return false;
 		}

a8fa88a199230ccf8c4cc8802d5fed5dfb933fa2
Crisjaira Villarino <crisjaira.villarino@leafnode.io>
2016-02-17 12:13:25 +0800

    Fix merging issue: Added import for Objects class

--- src/main/java/com/efficio/fieldbook/web/trial/controller/BaseTrialController.java
+++ src/main/java/com/efficio/fieldbook/web/trial/controller/BaseTrialController.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.StringTokenizer;
 import javax.annotation.Resource;
 

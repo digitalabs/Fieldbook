31673ffe4afeed87931699bc2f20d6a1f74d09cf
Crisjaira Villarino <crisjaira.villarino@leafnode.io>
2016-03-02 20:08:36 +0800

    Added documentation for GenerateDesignInput class
    Issue: BMS-2113
    Reviewer: Mae Isabelle Turiana

--- src/main/java/com/efficio/fieldbook/web/common/bean/GenerateDesignInput.java
+++ src/main/java/com/efficio/fieldbook/web/common/bean/GenerateDesignInput.java
@@ -5,6 +5,10 @@ import org.generationcp.middleware.domain.dms.DesignTypeItem;
 
 import com.efficio.fieldbook.web.trial.bean.EnvironmentData;
 
+/**
+ * This class is use to contain all the variables needed in generating design for the following design types: Preset Design (e.g.
+ * E30-Rep2-Block6-5Ind, E30-Rep3-Block6-5Ind, E50-Rep2-Block5-10Ind) and Custom Import Design. Used mainly in DesignImport feature.
+ */
 public class GenerateDesignInput {
 
 	private EnvironmentData environmentData;

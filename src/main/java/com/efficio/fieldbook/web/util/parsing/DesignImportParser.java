5b94db209229c843f85757bc617ebb0b809a4bc9
Daniel Villafuerte <dannyvonline@gmail.com>
2016-03-14 18:54:57 +0800

    Fixed merging issue with master. Basically reverted to Aldrin's content
    BMS-1432

--- src/main/java/com/efficio/fieldbook/web/util/parsing/DesignImportParser.java
+++ src/main/java/com/efficio/fieldbook/web/util/parsing/DesignImportParser.java
@@ -1,18 +1,14 @@
 
 package com.efficio.fieldbook.web.util.parsing;
 
-import java.util.List;
-import java.util.Map;
-
 import javax.annotation.Resource;
 
-import org.generationcp.commons.parsing.AbstractCsvFileProcessor;
 import org.generationcp.commons.parsing.FileParsingException;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.efficio.fieldbook.web.common.bean.DesignImportData;
 
-public class DesignImportParser extends AbstractCsvFileProcessor<DesignImportData> {
+public class DesignImportParser {
 
 	public static final Integer FILE_TYPE_CSV = 1;
 	public static final Integer FILE_TYPE_EXCEL = 2;
@@ -59,17 +55,9 @@ public class DesignImportParser extends AbstractCsvFileProcessor<DesignImportDat
 		if (fileType == FILE_TYPE_CSV) {
 			designImportData = this.csvParser.parseFile(absoluteFilename);
 		} else if (fileType == FILE_TYPE_EXCEL) {
-			// FIXME : DanV to fix
-			//designImportData = this.excelParser.parseFile(absoluteFilename, null);
+			designImportData = this.excelParser.parseFile(absoluteFilename, null);
 		}
 
 		return designImportData;
 	}
-
-	@Override
-	public DesignImportData parseCsvMap(Map<Integer, List<String>> csvMap) throws FileParsingException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }

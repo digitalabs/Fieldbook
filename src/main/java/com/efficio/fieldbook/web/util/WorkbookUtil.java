841d01f056715ba4cdf3a806114ab956b7ddcbef
Daniel Villafuerte <dannyvonline@gmail.com>
2016-03-22 14:51:53 +0800

    Cleanup :  - Applied <> operator  - Removed null checking, since item will never be null
     BMS-2571

--- src/main/java/com/efficio/fieldbook/web/util/WorkbookUtil.java
+++ src/main/java/com/efficio/fieldbook/web/util/WorkbookUtil.java
@@ -65,7 +65,7 @@ public class WorkbookUtil {
 	}
 
 	public static List<MeasurementRow> filterObservationsByTrialInstance(final List<MeasurementRow> observations, final String trialInstance) {
-		final List<MeasurementRow> list = new ArrayList<MeasurementRow>();
+		final List<MeasurementRow> list = new ArrayList<>();
 		if (observations != null && !observations.isEmpty()) {
 			final List<MeasurementVariable> variables = observations.get(0).getMeasurementVariables();
 			for (final MeasurementRow row : observations) {
@@ -91,11 +91,11 @@ public class WorkbookUtil {
 
 	public static List<MeasurementRow> createMeasurementRows(final List<List<ValueReference>> list,
 			final List<MeasurementVariable> variables) {
-		final List<MeasurementRow> observations = new ArrayList<MeasurementRow>();
+		final List<MeasurementRow> observations = new ArrayList<>();
 
 		if (list != null && !list.isEmpty()) {
 			for (final List<ValueReference> row : list) {
-				final List<MeasurementData> dataList = new ArrayList<MeasurementData>();
+				final List<MeasurementData> dataList = new ArrayList<>();
 				for (final ValueReference ref : row) {
 					final MeasurementVariable var = WorkbookUtil.getMeasurementVariable(variables, ref.getId());
 					if (var != null) {
@@ -119,11 +119,11 @@ public class WorkbookUtil {
 	public static List<MeasurementRow> createMeasurementRowsFromEnvironments(final List<Environment> environments,
 			final List<MeasurementVariable> variables, final ExpDesignParameterUi params) {
 
-		final List<MeasurementRow> observations = new ArrayList<MeasurementRow>();
+		final List<MeasurementRow> observations = new ArrayList<>();
 
 		if (environments != null) {
 			for (final Environment environment : environments) {
-				final List<MeasurementData> dataList = new ArrayList<MeasurementData>();
+				final List<MeasurementData> dataList = new ArrayList<>();
 				for (final MeasurementVariable var : variables) {
 					String value = environment.getManagementDetailValues().get(Integer.toString(var.getTermId()));
 					Integer phenotypeId = null;
@@ -166,7 +166,7 @@ public class WorkbookUtil {
 	}
 
 	public static List<String> getAddedTraits(final List<MeasurementVariable> variables, final List<MeasurementRow> observations) {
-		final List<String> newTraits = new ArrayList<String>();
+		final List<String> newTraits = new ArrayList<>();
 		if (observations != null && !observations.isEmpty()) {
 			final List<MeasurementVariable> workbookVariables = observations.get(0).getMeasurementVariables();
 			if (workbookVariables != null && !workbookVariables.isEmpty()) {
@@ -191,7 +191,7 @@ public class WorkbookUtil {
 
 	public static List<MeasurementVariable> getAddedTraitVariables(final List<MeasurementVariable> variables,
 			final List<MeasurementRow> observations) {
-		final List<MeasurementVariable> newTraits = new ArrayList<MeasurementVariable>();
+		final List<MeasurementVariable> newTraits = new ArrayList<>();
 		if (observations != null && !observations.isEmpty()) {
 			final List<MeasurementVariable> workbookVariables = observations.get(0).getMeasurementVariables();
 			if (workbookVariables != null && !workbookVariables.isEmpty()) {
@@ -217,7 +217,7 @@ public class WorkbookUtil {
 
 	public static void clearNewlyAddedImportTraits(final List<MeasurementVariable> variables, final List<MeasurementRow> observations) {
 		final List<MeasurementVariable> newTraits = WorkbookUtil.getAddedTraitVariables(variables, observations);
-		final List<Integer> indexForRemoval = new ArrayList<Integer>();
+		final List<Integer> indexForRemoval = new ArrayList<>();
 		if (observations != null && !observations.isEmpty()) {
 			final List<MeasurementData> initialDataList = observations.get(0).getDataList();
 			for (final MeasurementData initialData : initialDataList) {
@@ -228,7 +228,7 @@ public class WorkbookUtil {
 					}
 				}
 			}
-			if (indexForRemoval != null && !indexForRemoval.isEmpty()) {
+			if (!indexForRemoval.isEmpty()) {
 				for (final MeasurementRow dataRow : observations) {
 					for (final Integer removedMeasurementDataIndex : indexForRemoval) {
 						dataRow.getDataList().remove(removedMeasurementDataIndex);
@@ -242,13 +242,13 @@ public class WorkbookUtil {
 	public static void resetWorkbookObservations(final Workbook workbook) {
 		if (workbook.getObservations() != null && !workbook.getObservations().isEmpty()) {
 			if (workbook.getOriginalObservations() == null || workbook.getOriginalObservations().isEmpty()) {
-				final List<MeasurementRow> origObservations = new ArrayList<MeasurementRow>();
+				final List<MeasurementRow> origObservations = new ArrayList<>();
 				for (final MeasurementRow row : workbook.getObservations()) {
 					origObservations.add(row.copy());
 				}
 				workbook.setOriginalObservations(origObservations);
 			} else {
-				final List<MeasurementRow> observations = new ArrayList<MeasurementRow>();
+				final List<MeasurementRow> observations = new ArrayList<>();
 				for (final MeasurementRow row : workbook.getOriginalObservations()) {
 					observations.add(row.copy());
 				}
@@ -439,7 +439,7 @@ public class WorkbookUtil {
 	 * @return the list
 	 */
 	private static List<ValueReference> transformPossibleValues(final List<Enumeration> enumerations) {
-		final List<ValueReference> list = new ArrayList<ValueReference>();
+		final List<ValueReference> list = new ArrayList<>();
 
 		if (enumerations != null) {
 			for (final Enumeration enumeration : enumerations) {
@@ -453,9 +453,9 @@ public class WorkbookUtil {
 	public static void manageExpDesignVariablesAndObs(final Workbook workbook, final Workbook tempWorkbook) {
 		// edit original factors to add/delete new/deleted variables based on tempWorkbook.getFactors
 		// create map of factors in tempWorkbook and factors in workbook
-		final Map<Integer, MeasurementVariable> tempFactorsMap = new HashMap<Integer, MeasurementVariable>();
-		final Map<Integer, MeasurementVariable> factorsMap = new HashMap<Integer, MeasurementVariable>();
-		final Map<Integer, StandardVariable> expDesignVariablesMap = new HashMap<Integer, StandardVariable>();
+		final Map<Integer, MeasurementVariable> tempFactorsMap = new HashMap<>();
+		final Map<Integer, MeasurementVariable> factorsMap = new HashMap<>();
+		final Map<Integer, StandardVariable> expDesignVariablesMap = new HashMap<>();
 
 		if (tempWorkbook.getFactors() != null) {
 			for (final MeasurementVariable var : tempWorkbook.getFactors()) {
@@ -494,7 +494,7 @@ public class WorkbookUtil {
 
 	public static Map<Integer, MeasurementVariable> createVariableList(final List<MeasurementVariable> factors,
 			final List<MeasurementVariable> variates) {
-		final Map<Integer, MeasurementVariable> observationVariables = new HashMap<Integer, MeasurementVariable>();
+		final Map<Integer, MeasurementVariable> observationVariables = new HashMap<>();
 		if (factors != null) {
 			for (final MeasurementVariable var : factors) {
 				observationVariables.put(var.getTermId(), var);
@@ -511,7 +511,7 @@ public class WorkbookUtil {
 	public static void deleteDeletedVariablesInObservations(final Map<Integer, MeasurementVariable> measurementDatasetVariables,
 			final List<MeasurementRow> observations) {
 
-		final List<Integer> deletedList = new ArrayList<Integer>();
+		final List<Integer> deletedList = new ArrayList<>();
 		if (observations != null && !observations.isEmpty()) {
 			for (final MeasurementData data : observations.get(0).getDataList()) {
 				if (measurementDatasetVariables.get(data.getMeasurementVariable().getTermId()) == null
@@ -557,7 +557,7 @@ public class WorkbookUtil {
 	}
 
 	public static List<MeasurementVariable> getExperimentalDesignVariables(final List<MeasurementVariable> conditions) {
-		final List<MeasurementVariable> expDesignVariables = new ArrayList<MeasurementVariable>();
+		final List<MeasurementVariable> expDesignVariables = new ArrayList<>();
 		if (conditions != null && !conditions.isEmpty()) {
 			final List<Integer> expDesignConstants = AppConstants.EXP_DESIGN_VARIABLES.getIntegerList();
 			for (final MeasurementVariable condition : conditions) {

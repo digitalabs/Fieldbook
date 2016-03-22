49287780c40dda6778c5b230866f0bcba1a932ef
Mae Isabelle Turiana <maeisabelle@gmail.com>
2016-03-22 17:09:10 +0800

    Cleanup: Use === instead of ==
    BMS-2561

--- src/main/webapp/WEB-INF/static/js/trialmanager/selectEnvironmentModal.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/selectEnvironmentModal.js
@@ -34,7 +34,7 @@
         $scope.trialSelectEnvironmentContinue = function(){
 
             // Do not go ahead for Advancing unless trial has experimental design & number of replications variables
-            if(TrialManagerDataService.currentData.experimentalDesign.designType == null) {
+            if(TrialManagerDataService.currentData.experimentalDesign.designType === null) {
                 showAlertMessage('', $.fieldbookMessages.advanceListUnableToGenerateWarningMessage);
                 return;
             }

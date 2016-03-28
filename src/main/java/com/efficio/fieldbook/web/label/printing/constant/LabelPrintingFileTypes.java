8bfef07bca9c0a8f0ce871ebbda59d9479b354c9
Cyrus Venn Casada <cvpcasada@outlook.com>
2016-03-28 16:33:06 +0800

    Updated enum, removed default constructor

--- src/main/java/com/efficio/fieldbook/web/label/printing/constant/LabelPrintingFileTypes.java
+++ src/main/java/com/efficio/fieldbook/web/label/printing/constant/LabelPrintingFileTypes.java
@@ -1,17 +1,11 @@
 package com.efficio.fieldbook.web.label.printing.constant;
 
 public enum LabelPrintingFileTypes {
-	PDF ("1", "PDF", ".pdf"), EXCEL ("2", "EXCEL", ".xls"), CSV ("3", "CSV", ".csv"), INVALID;
+	INVALID("0","INVALID","."), PDF ("1", "PDF", ".pdf"), EXCEL ("2", "EXCEL", ".xls"), CSV ("3", "CSV", ".csv");
 
 	private String formIndex;
 	private String extension;
 	private String type;
-
-	LabelPrintingFileTypes() {
-		this.formIndex = "0";
-		this.type = "INVALID";
-		this.extension = "";
-	}
 
 	LabelPrintingFileTypes(String formIndex, String type, String extension) {
 		this.formIndex = formIndex;

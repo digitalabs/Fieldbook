22a672adcea903ec8831207f7d4b450fff72a507
keyurbhatt <keyur@leafnode.io>
2016-04-05 17:21:53 +0530

    Added required members in AdvancingSource to avoid using and passing whole workbook. Filled data of new members.
    Issue: BMS-2599
    Reviewer: Sunny

--- src/main/java/com/efficio/fieldbook/web/naming/expression/BreedersCrossIDExpression.java
+++ src/main/java/com/efficio/fieldbook/web/naming/expression/BreedersCrossIDExpression.java
@@ -2,7 +2,6 @@ package com.efficio.fieldbook.web.naming.expression;
 
 import java.util.List;
 
-import com.efficio.fieldbook.web.common.bean.UserSelection;
 import com.efficio.fieldbook.web.nursery.bean.AdvancingSource;
 import org.generationcp.commons.service.impl.BreedersCrossIDGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +9,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class BreedersCrossIDExpression extends BaseExpression{
-
-	@Autowired
-	private UserSelection userSelection;
 
 	@Autowired
 	private BreedersCrossIDGenerator breedersCrossIDGenerator;
@@ -25,7 +21,8 @@ public class BreedersCrossIDExpression extends BaseExpression{
 	@Override
 	public void apply(List<StringBuilder> values, AdvancingSource source) {
 		for (StringBuilder container : values) {
-			String newValue = this.breedersCrossIDGenerator.generateBreedersCrossID(userSelection.getWorkbook(), source.getTrialInstanceNumber());
+			String newValue = this.breedersCrossIDGenerator.generateBreedersCrossID( source.getStudyType(), source.getConditions(),
+					source.getTrailInstanceObservation());
 			this.replaceExpressionWithValue(container, newValue);
 		}
 	}

f8ce4ec15bd7ee19cdac5b665758b7159475f332
Iryna Dziuban <iryna@leafnode.io>
2016-06-14 11:49:50 +1200

    Cleanup - unused globals and comments.
    Issue: BMS-2840
    Reviewer: Akhil

--- src/main/webapp/WEB-INF/static/js/trialmanager/experimentalDesign.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/experimentalDesign.js
@@ -1,8 +1,5 @@
-/**
- * Created by cyrus on 7/2/14.
- */
+/* global angular, showErrorMessage, showSuccessfulMessage, expDesignMsgs */
 
-/* global angular, showErrorMessage, showSuccessfulMessage, showMeasurementsPreview, expDesignMsgs */
 (function() {
 		'use strict';
 

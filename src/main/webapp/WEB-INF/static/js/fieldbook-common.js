70e16ade08782bf40d501ea64f795ca78fe49a70
Mae Isabelle Turiana <maeisabelle@gmail.com>
2016-06-15 15:36:22 +0800

    Fixed missing starting entry number when Add Types popup is closed that is causing the generate design to fail
    BMS-2727

--- src/main/webapp/WEB-INF/static/js/fieldbook-common.js
+++ src/main/webapp/WEB-INF/static/js/fieldbook-common.js
@@ -3131,6 +3131,7 @@ function reloadCheckListTable() {
 			$('#imported-germplasm-list').html(html);
 			$('#entries-details').css('display', 'block');
 			$('#numberOfEntries').html($('#totalGermplasms').val());
+			$('#txtStartingEntryNo').prop('disabled', false);
 		});
 	}
 }
--- src/main/webapp/WEB-INF/static/js/trialmanager/trial-data-manager.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/trial-data-manager.js
@@ -208,7 +208,8 @@
 			var VariablePairService = $resource('/Fieldbook/TrialManager/createTrial/retrieveVariablePairs/:id',
 				{id: '@id'}, {get: {method: 'get', isArray: true}});
 
-
+			var UpdateStartingEntryNoService = $resource('/Fieldbook/TrialManager/GermplasmList/startingEntryNo', {}, {});
+			
 			var service = {
 				// user input data and default values of standard variables
 				currentData: {
@@ -577,6 +578,7 @@
 				updateStartingEntryNoCount: function(newCountValue) {
 					service.currentData.experimentalDesign.startingEntryNo = newCountValue;
 					$('body').data('service.currentData.experimentalDesign.startingEntryNo', newCountValue);
+					UpdateStartingEntryNoService.save(newCountValue);
 				},
 
 				onUpdateSettings: function(key, updateFunction) {

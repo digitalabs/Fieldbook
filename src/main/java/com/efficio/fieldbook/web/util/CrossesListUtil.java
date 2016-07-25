a327ed209f69fafab90f00a1aa9cc38d5cce8dda
chavdahard <hard@leafnode.io>
2016-07-25 13:34:48 +0530

    Adding and deleting headers for preview cross list modal
    Adding FEMALE_PLOT,BREEDING_METHOD,MALE_NURSERY,MALE_PLOT,CROSSING_DATE and
    deleting ENTRY_CODE,FEMALE_PARENT,MALE_PARENT headers to be used in Preview Crossing modal.
    
    Issue : BMS-3037
    Reviewer : Abhishek

--- src/main/java/com/efficio/fieldbook/web/util/CrossesListUtil.java
+++ src/main/java/com/efficio/fieldbook/web/util/CrossesListUtil.java
@@ -18,16 +18,21 @@ public class CrossesListUtil {
 
 	public static final String TABLE_HEADER_LIST = "tableHeaderList";
 	public static final String LIST_DATA_TABLE = "listDataTable";
-
+	public static final String IS_IMPORT = "isImport";
 	public static final int ENTRY_INDEX = 0;
 	public static final int PARENTAGE_INDEX = 1;
-	public static final int ENTRY_CODE_INDEX = 2;
-	public static final int FEMALE_PARENT_INDEX = 3;
+	public static final int DUPLICATE_INDEX = 2;
+	public static final int BREEDING_METHOD_INDEX = 3;
 	public static final int FGID_INDEX = 4;
-	public static final int MALE_PARENT_INDEX = 5;
+	public static final int FEMALE_PLOT_INDEX = 5;
 	public static final int MGID_INDEX = 6;
-	public static final int SOURCE_INDEX = 7;
-	public static final int DUPLICATE_INDEX = 8;
+	public static final int MALE_NURSERY_INDEX = 7;
+	public static final int MALE_PLOT_INDEX = 8;
+	public static final int CROSSING_DATE_INDEX = 9;
+	public static final int NOTES_INDEX = 10;
+	public static final int SOURCE_INDEX = 11;
+
+
 
 	@Autowired
 	private OntologyDataManager ontologyDataManager;
@@ -55,13 +60,11 @@ public class CrossesListUtil {
 		dataMap.put(tableHeaderList.get(CrossesListUtil.ENTRY_INDEX), crossesData.getEntryId());
 		dataMap.put(tableHeaderList.get(CrossesListUtil.PARENTAGE_INDEX), crossesData.getFemaleParent() + CrossesListUtil.DEFAULT_SEPARATOR
 				+ crossesData.getMaleParent());
-		dataMap.put(tableHeaderList.get(CrossesListUtil.ENTRY_CODE_INDEX), crossesData.getEntryCode());
-		dataMap.put(tableHeaderList.get(CrossesListUtil.FEMALE_PARENT_INDEX), crossesData.getFemaleParent());
+		dataMap.put(tableHeaderList.get(CrossesListUtil.DUPLICATE_INDEX), "");
 		dataMap.put(tableHeaderList.get(CrossesListUtil.FGID_INDEX), crossesData.getFgid());
-		dataMap.put(tableHeaderList.get(CrossesListUtil.MALE_PARENT_INDEX), crossesData.getMaleParent());
 		dataMap.put(tableHeaderList.get(CrossesListUtil.MGID_INDEX), crossesData.getMgid());
 		dataMap.put(tableHeaderList.get(CrossesListUtil.SOURCE_INDEX), crossesData.getSeedSource());
-		dataMap.put(tableHeaderList.get(CrossesListUtil.DUPLICATE_INDEX), "");
+
 		return dataMap;
 	}
 
@@ -86,13 +89,16 @@ public class CrossesListUtil {
 
 		dataMap.put(tableHeaderList.get(CrossesListUtil.ENTRY_INDEX), importedCrosses.getEntryId());
 		dataMap.put(tableHeaderList.get(CrossesListUtil.PARENTAGE_INDEX), importedCrosses.getCross());
-		dataMap.put(tableHeaderList.get(CrossesListUtil.ENTRY_CODE_INDEX), importedCrosses.getEntryCode());
-		dataMap.put(tableHeaderList.get(CrossesListUtil.FEMALE_PARENT_INDEX), importedCrosses.getFemaleDesig());
-		dataMap.put(tableHeaderList.get(CrossesListUtil.FGID_INDEX), importedCrosses.getFemaleGid());
-		dataMap.put(tableHeaderList.get(CrossesListUtil.MALE_PARENT_INDEX), importedCrosses.getMaleDesig());
-		dataMap.put(tableHeaderList.get(CrossesListUtil.MGID_INDEX), importedCrosses.getMaleGid());
-		dataMap.put(tableHeaderList.get(CrossesListUtil.SOURCE_INDEX), importedCrosses.getSource());
 		dataMap.put(tableHeaderList.get(CrossesListUtil.DUPLICATE_INDEX), importedCrosses.getDuplicate());
+		dataMap.put(tableHeaderList.get(CrossesListUtil.BREEDING_METHOD_INDEX),importedCrosses.getRawBreedingMethod());
+		dataMap.put(tableHeaderList.get(CrossesListUtil.FGID_INDEX), importedCrosses.getFemaleGid());
+		dataMap.put(tableHeaderList.get(CrossesListUtil.FEMALE_PLOT_INDEX), importedCrosses.getFemalePlotNo());
+		dataMap.put(tableHeaderList.get(CrossesListUtil.MGID_INDEX), importedCrosses.getMaleGid());
+		dataMap.put(tableHeaderList.get(CrossesListUtil.MALE_NURSERY_INDEX), importedCrosses.getMaleStudyName());
+		dataMap.put(tableHeaderList.get(CrossesListUtil.MALE_PLOT_INDEX), importedCrosses.getMalePlotNo());
+		dataMap.put(tableHeaderList.get(CrossesListUtil.CROSSING_DATE_INDEX), importedCrosses.getCrossingDate());
+		dataMap.put(tableHeaderList.get(CrossesListUtil.NOTES_INDEX), importedCrosses.getNotes());
+		dataMap.put(tableHeaderList.get(CrossesListUtil.SOURCE_INDEX), importedCrosses.getSource());
 		return dataMap;
 
 	}
@@ -111,13 +117,16 @@ public class CrossesListUtil {
 
 		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.ENTRY_ID));
 		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.PARENTAGE));
-		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.ENTRY_CODE));
-		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.FEMALE_PARENT));
-		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.FGID));
-		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.MALE_PARENT));
-		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.MGID));
-		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.SEED_SOURCE));
 		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.DUPLICATE));
+		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.BREEDING_METHOD_NAME));
+		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.FGID));
+		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.FEMALE_PLOT));
+		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.MGID));
+		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.MALE_NURSERY_NAME));
+		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.MALE_PLOT));
+		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.CROSSING_DATE));
+		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.NOTES));
+		tableHeaderList.add(this.getTermNameFromOntology(ColumnLabels.SEED_SOURCE));
 
 		return tableHeaderList;
 	}

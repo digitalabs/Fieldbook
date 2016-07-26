99dbf5633d43cec7489cf265dc8ef22145fcb712
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2016-07-26 18:45:35 +0800

    Removed the getSeason method. Removed the checking for the study type since season variables for Trials can also be stored in the workbook conditions. Used hashmap in the implementation of the prioritization of the season variables
    BMS-2705

--- src/main/java/com/efficio/fieldbook/web/naming/expression/dataprocessor/SeasonExpressionDataProcessor.java
+++ src/main/java/com/efficio/fieldbook/web/naming/expression/dataprocessor/SeasonExpressionDataProcessor.java
@@ -1,11 +1,15 @@
 
 package com.efficio.fieldbook.web.naming.expression.dataprocessor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.Resource;
 
 import org.apache.commons.lang3.StringUtils;
 import org.generationcp.commons.spring.util.ContextUtil;
 import org.generationcp.middleware.domain.dms.Study;
+import org.generationcp.middleware.domain.etl.MeasurementData;
 import org.generationcp.middleware.domain.etl.MeasurementRow;
 import org.generationcp.middleware.domain.etl.MeasurementVariable;
 import org.generationcp.middleware.domain.etl.Workbook;
@@ -14,7 +18,6 @@ import org.generationcp.middleware.domain.oms.TermId;
 import org.generationcp.middleware.manager.ontology.api.OntologyVariableDataManager;
 import org.springframework.stereotype.Component;
 
-import com.efficio.fieldbook.util.FieldbookException;
 import com.efficio.fieldbook.web.nursery.bean.AdvancingNursery;
 import com.efficio.fieldbook.web.nursery.bean.AdvancingSource;
 
@@ -28,9 +31,14 @@ public class SeasonExpressionDataProcessor implements ExpressionDataProcessor {
 	private ContextUtil contextUtil;
 
 	@Override
-	public void processEnvironmentLevelData(AdvancingSource source, Workbook workbook, AdvancingNursery nurseryInfo, Study study)
-			throws FieldbookException {
-		source.setSeason(getSeason(workbook));
+	public void processEnvironmentLevelData(AdvancingSource source, Workbook workbook, AdvancingNursery nurseryInfo, Study study) {
+		Map<Integer, String> measurementVariablesValues = new HashMap<Integer, String>();
+		for (MeasurementVariable mv : workbook.getConditions()) {
+			if(StringUtils.isNotBlank(mv.getValue())){
+				measurementVariablesValues.put(mv.getTermId(), mv.getValue());
+			}
+		}
+		source.setSeason(this.getValueOfPrioritySeasonVariable(measurementVariablesValues));
 	}
 
 	@Override
@@ -38,38 +46,22 @@ public class SeasonExpressionDataProcessor implements ExpressionDataProcessor {
 		// no implementation, SeasonExpression does not need plot level data
 	}
 
-	String getSeason(Workbook workbook) throws FieldbookException {
+	private String getValueOfPrioritySeasonVariable(Map<Integer, String> measurementVariablesValues) {
 		String season = "";
-		if (workbook.getStudyDetails().getStudyType() == StudyType.N) {
-			for (MeasurementVariable mv : workbook.getConditions()) {
-				if (mv.getTermId() == TermId.SEASON.getId()) {
-					season = mv.getValue();
-				} else if (mv.getTermId() == TermId.SEASON_DRY.getId()) {
-					season = mv.getValue();
-				} else if (mv.getTermId() == TermId.SEASON_MONTH.getId()) {
-					season = mv.getValue();
-				} else if (mv.getTermId() == TermId.SEASON_VAR.getId()) {
-					// categorical variable - the value returned is the key to another term
-					if (mv.getValue().equals("")) {
-						// the user has failed to choose a season from the available choices
-						throw new FieldbookException("nursery.advance.no.code.selected.for.season");
-					}
-					// ambulance at the base of the cliff - we do not know if the season will be the numeric
-					// category code, or the text category description, so we will be safe here
-					if (StringUtils.isNumeric(mv.getValue())) {
-						// season is the numeric code referring to the category
-						season =
-								this.ontologyVariableDataManager.retrieveVariableCategoricalValue(contextUtil.getCurrentProgramUUID(),
-										mv.getTermId(), Integer.parseInt(mv.getValue()));
-					} else {
-						// season captured is the description
-						season = mv.getValue();
-					}
-				} else if (mv.getTermId() == TermId.SEASON_VAR_TEXT.getId()) {
-					season = mv.getValue();
-				} else if (mv.getTermId() == TermId.SEASON_WET.getId()) {
-					season = mv.getValue();
-				}
+		if(measurementVariablesValues.get(TermId.SEASON_MONTH.getId()) != null){
+			season = measurementVariablesValues.get(TermId.SEASON_MONTH.getId());
+		} else if(measurementVariablesValues.get(TermId.SEASON_VAR_TEXT.getId()) != null){
+			season = measurementVariablesValues.get(TermId.SEASON_VAR_TEXT.getId());
+		} else if(measurementVariablesValues.get(TermId.SEASON_VAR.getId()) != null){
+			String seasonVarValue = measurementVariablesValues.get(TermId.SEASON_VAR.getId());
+			if (StringUtils.isNumeric(seasonVarValue)) {
+				// season is the numeric code referring to the category
+				season =
+						this.ontologyVariableDataManager.retrieveVariableCategoricalValue(contextUtil.getCurrentProgramUUID(),
+								TermId.SEASON_VAR.getId(), Integer.parseInt(seasonVarValue));
+			} else {
+				// season captured is the description
+				season = seasonVarValue;
 			}
 		}
 		return season;

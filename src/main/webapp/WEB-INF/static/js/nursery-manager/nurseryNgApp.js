9eb69604831eb5fa2bac4c0e2a6fdd98e548e2ea
Iryna Dziuban <iryna@leafnode.io>
2016-08-03 15:02:13 +1200

    Localise ajax error messages, add timeouts to the ajax requests.
    Issue: BMS-3040
    Reviewer: Akhil

--- src/main/webapp/WEB-INF/static/js/nursery-manager/nurseryNgApp.js
+++ src/main/webapp/WEB-INF/static/js/nursery-manager/nurseryNgApp.js
@@ -18,8 +18,10 @@
 
 		// Register designImportGenerated handler that will activate when an importDesign is generated
 		$scope.$on('designImportGenerated', function() {
-			$http.get('/Fieldbook/DesignImport/getMappingSummary').success(function(data) {
+			$http.get('/Fieldbook/DesignImport/getMappingSummary', {timeout: 3000}).success(function(data) {
 				$scope.applicationData.importDesignMappedData = data;
+			}).error(function() {
+				showErrorMessage('', $.fieldbookMessages.errorNoMappingSummary);
 			});
 
 			retrieveCustomDesignImportData();
@@ -27,9 +29,11 @@
 
 		function retrieveCustomDesignImportData() {
 			// retrieve default values for custom design
-			$http.get('/Fieldbook/DesignImport/getCustomImportDesignTypeDetails').success(function(data) {
+			$http.get('/Fieldbook/DesignImport/getCustomImportDesignTypeDetails', {timeout: 3000}).success(function(data) {
 				$scope.currentDesignType.name = data.name;
 				$scope.currentDesignType.templateName = data.templateName;
+			}).error(function() {
+				showErrorMessage('', $.fieldbookMessages.errorNoDefaultValuesForCustomDesign);
 			});
 		}
 
@@ -41,28 +45,28 @@
 	nurseryNgApp.controller('selectUserCtrl', ['$scope', '$http', '_', function($scope, $http, _) {
 		$scope.currentProgramMembers = [];
 
-		$http.get('/Fieldbook/crosses/getCurrentUser').success(function(data) {
-            $scope.selectedUserId = data;
-        }).error(function() {
-        	showErrorMessage('', 'Could not resolve current user, please try again or contact the support.');
-        });
+		$http.get('/Fieldbook/crosses/getCurrentUser', {timeout: 3000}).success(function(data) {
+			$scope.selectedUserId = data;
+		}).error(function() {
+			showErrorMessage('', $.fieldbookMessages.errorNoCurrentUser);
+		});
 
-		$http.get('/Fieldbook/crosses/getCurrentProgramMembers').success(function(data) {
-        	$scope.currentProgramMembers= data;
-        	if (!$scope.selectedUserId) {
-        		$scope.selectedUserId = _.keys($scope.currentProgramMembers[0]);
-        	}
-        }).error(function() {
-        	showErrorMessage('', 'Could not resolve the list of members of the program, please try again or contact the support.');
-        });
+		$http.get('/Fieldbook/crosses/getCurrentProgramMembers', {timeout: 3000}).success(function(data) {
+			$scope.currentProgramMembers = data;
+			if (!$scope.selectedUserId) {
+				$scope.selectedUserId = _.keys($scope.currentProgramMembers[0]);
+			}
+		}).error(function() {
+			showErrorMessage('', $.fieldbookMessages.errorNoProgramMembers);
+		});
 
-        $scope.saveSelectedUser = function(){
-        	$http.post('/Fieldbook/crosses/submitListOwner', $scope.selectedUserId).success(function() {
-            	//do nothing or success message?
-            }).error(function() {
-            	showErrorMessage('', 'Could not associate User id with the list, please try again or contact the support.');
-            });
-        };
-    }]);
+		$scope.saveSelectedUser = function() {
+			$http.post('/Fieldbook/crosses/submitListOwner', $scope.selectedUserId, {timeout: 3000}).success(function() {
+				//do nothing or success message?
+			}).error(function() {
+				showErrorMessage('', $.fieldbookMessages.errorSubmittingListOwner);
+			});
+		};
+	}]);
 
 })();

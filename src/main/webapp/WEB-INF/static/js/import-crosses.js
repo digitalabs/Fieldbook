d46d616b9a88efe7f94d2b12937e1ea3299edccb
darla-leafnode <darla@leafnode.io>
2016-08-11 21:41:57 +0800

    Added null check before setting selected breeding method id to prevent validation error on automatic naming
    Issue: BMS-3182

--- src/main/webapp/WEB-INF/static/js/import-crosses.js
+++ src/main/webapp/WEB-INF/static/js/import-crosses.js
@@ -510,7 +510,7 @@ var ImportCrosses = {
 		settingObject.breedingMethodSetting = {};
 		settingObject.breedingMethodSetting.methodId = $('#breedingMethodDropdown').select2('val');
 
-		if(selectedBreedingMethodId != ''){
+		if(selectedBreedingMethodId !== null && selectedBreedingMethodId != ''){
 			settingObject.breedingMethodSetting.methodId = selectedBreedingMethodId;
 		}
 		else if (!settingObject.breedingMethodSetting.methodId || settingObject.breedingMethodSetting.methodId === '') {

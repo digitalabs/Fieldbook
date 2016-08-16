6e4c9736af3de2c2052be8e25b13c0e02b8fc1ec
Aldrin Batac <aldrin@leafnode.io>
2016-08-16 11:44:43 +0800

    Apply the logic that transforms duplicate string in Cross List view.
    BMS-3190

--- src/main/webapp/WEB-INF/static/js/fbk-data-table/fieldbook-datatable.js
+++ src/main/webapp/WEB-INF/static/js/fbk-data-table/fieldbook-datatable.js
@@ -1110,6 +1110,14 @@ BMS.Fieldbook.FinalAdvancedGermplasmListDataTable = (function($) {
 			columns.push({data: $(this).data('col-name')});
 			if (index === 0) {
 				aoColumnsDef.push({bSortable: false});
+			} else if ($(this).html() === 'DUPLICATE') {
+				aoColumnsDef.push({
+					defaultContent: '',
+					targets: index,
+					createdCell: function(td, cellData, rowData, row, col) {
+						transformDuplicateStringToColorCodedSpans(td);
+					}
+				});
 			} else {
 				aoColumnsDef.push(null);
 			}

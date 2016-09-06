acadfcf233de84f15363d3aa2fd2822a02f17dad
abhishekptl <abhishek@leafnode.io>
2016-09-06 10:06:51 +0530

    Creating utilty method to do manually logout
    This manually logout utility will set authentication to null and delete all cookies from the context path of application.
    
    Cookies needs to delete because PreAuthentication mechanism try to resolve pre-authenticated user from session,cookie and then database(workbench_runtime_data).
    Singing out from Workbench will clear session, authentication to null and clear cookies for all downstream applications.
    
    issue: BMS-3194
    reviewer: Sunny

--- src/main/java/com/efficio/fieldbook/web/LogoutController.java
+++ src/main/java/com/efficio/fieldbook/web/LogoutController.java
@@ -1,16 +1,13 @@
 package com.efficio.fieldbook.web;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.generationcp.commons.util.LogoutUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping(value = "/logout")
@@ -18,21 +15,8 @@ public class LogoutController {
 
 	@ResponseBody
 	@RequestMapping(method = RequestMethod.GET)
-	public String logout(HttpServletRequest request){
-
-		HttpSession context = request.getSession(false);
-		if(context != null) {
-			context.invalidate();
-		}
-
-		SecurityContext context1 = SecurityContextHolder.getContext();
-		context1.setAuthentication((Authentication)null);
-
-		for(Cookie cookie : request.getCookies()) {
-			cookie.setMaxAge(0);
-		}
-
-		SecurityContextHolder.clearContext();
+	public String logout(HttpServletRequest request, HttpServletResponse response){
+		LogoutUtil.manuallyLogout(request, response);
 		return "Fieodbook successfully logged out";
 	}
 

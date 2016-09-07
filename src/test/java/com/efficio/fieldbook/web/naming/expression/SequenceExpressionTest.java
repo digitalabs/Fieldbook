90c9090806358f4453acd4c3efa07b2a097c44d0
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2016-09-08 07:50:26 +0800

    Added test methods that handles the different scenarios of NonBulkingSequenceGeneration
    BMS-3268

--- src/test/java/com/efficio/fieldbook/web/naming/expression/SequenceExpressionTest.java
+++ src/test/java/com/efficio/fieldbook/web/naming/expression/SequenceExpressionTest.java
@@ -88,3 +88,45 @@ public class SequenceExpressionTest extends TestExpression {
 		final String resultDesig = GERMPLASM_NAME + SEPARATOR + 6;
 		Assert.assertEquals("The value after applying the processcodes should be equal to " + resultDesig, resultDesig, values.get(0).toString());
 	}
+	
+	@Test
+	public void testNonBulkingSequenceGenerationWithSequenceNumberEqualsTo5() {
+		Mockito.when(germplasmDataManager.getNextSequenceNumberForCrossName(Matchers.anyString())).thenReturn("5");
+		// final false refers to nonBulking
+		AdvancingSource source = this.createAdvancingSourceTestData(GERMPLASM_NAME, SEPARATOR, null, KEY, null, false);
+		source.setPlantsSelected(5);
+		source.setCurrentMaxSequence(5);
+		List<StringBuilder> values = this.createInitialValues(source);
+		Assert.assertFalse("The value before applying the processcodes should not be equal to " + GERMPLASM_NAME, GERMPLASM_NAME.equals(values.get(0).toString()));
+		expression.apply(values, source);
+		final String resultDesig = GERMPLASM_NAME + SEPARATOR + 10;
+		Assert.assertEquals("The value after applying the processcodes should be equal to " + resultDesig, resultDesig, values.get(0).toString());
+	}
+	
+	@Test
+	public void testNonBulkingSequenceGenerationWithNegativePlantsSelectedValue() {
+		Mockito.when(germplasmDataManager.getNextSequenceNumberForCrossName(Matchers.anyString())).thenReturn("5");
+		// final false refers to nonBulking
+		AdvancingSource source = this.createAdvancingSourceTestData(GERMPLASM_NAME, SEPARATOR, null, KEY, null, false);
+		source.setPlantsSelected(-1);
+		source.setCurrentMaxSequence(5);
+		List<StringBuilder> values = this.createInitialValues(source);
+		Assert.assertFalse("The value before applying the processcodes should not be equal to " + GERMPLASM_NAME, GERMPLASM_NAME.equals(values.get(0).toString()));
+		expression.apply(values, source);
+		final String resultDesig = GERMPLASM_NAME + SEPARATOR;
+		Assert.assertEquals("The value after applying the processcodes should be equal to " + resultDesig, resultDesig, values.get(0).toString());
+	}
+	
+	@Test
+	public void testNonBulkingSequenceGenerationWithNegativeCurrentMaxSequencedValue() {
+		// final false refers to nonBulking
+		AdvancingSource source = this.createAdvancingSourceTestData(GERMPLASM_NAME, SEPARATOR, null, KEY, null, false);
+		source.setPlantsSelected(1);
+		source.setCurrentMaxSequence(-5);
+		List<StringBuilder> values = this.createInitialValues(source);
+		Assert.assertFalse("The value before applying the processcodes should not be equal to " + GERMPLASM_NAME, GERMPLASM_NAME.equals(values.get(0).toString()));
+		expression.apply(values, source);
+		final String resultDesig = GERMPLASM_NAME + SEPARATOR + 1;
+		Assert.assertEquals("The value after applying the processcodes should be equal to " + resultDesig, resultDesig, values.get(0).toString());
+	}
+}

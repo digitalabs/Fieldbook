1183023396b10b29f6cc488cb9b4427a9c67d359
Naymesh <naymesh@leafnode.io>
2016-09-08 11:00:04 +1200

    Minor typo (Fieodbook ) correction.
    Issue: BMS-3194

--- src/main/java/com/efficio/fieldbook/web/LogoutController.java
+++ src/main/java/com/efficio/fieldbook/web/LogoutController.java
@@ -1,13 +1,13 @@
 package com.efficio.fieldbook.web;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.generationcp.commons.util.LogoutUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping(value = "/logout")
@@ -17,7 +17,7 @@ public class LogoutController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String logout(HttpServletRequest request, HttpServletResponse response){
 		LogoutUtil.manuallyLogout(request, response);
-		return "Fieodbook successfully logged out";
+		return "Fieldbook successfully logged out";
 	}
 
 }

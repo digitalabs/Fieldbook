5a3e99a0d9c6ea9e62e626fa8a91dc2cdf636b6c
darla-leafnode <darla@leafnode.io>
2016-09-12 14:13:45 +0800

    Improve documentation

--- src/main/java/com/efficio/fieldbook/web/naming/expression/NumberSequenceExpression.java
+++ src/main/java/com/efficio/fieldbook/web/naming/expression/NumberSequenceExpression.java
@@ -39,17 +39,22 @@ public abstract class NumberSequenceExpression extends BaseExpression {
 			int startCount = 1;
 
 			if (source.getCurrentMaxSequence() > -1) {
-				// Will only go to this if statement if: 
-				// count process code is [SEQUENCE] (the [NUMBER] process code does not require the count to be consecutive), 
-				// the current max sequence is 0 (to avoid multiple middleware calls), 
-				// and there is/are plant/s selected
+				/*
+				 * Query from DB the next available sequence # for cross name when:
+				 * 1) process code is [SEQUENCE] (the [NUMBER] process code does not require the count to be consecutive)
+				 * 2) and the current max sequence is 0, meaning only for the first entry, to avoid multiple Middleware calls
+				 * 3) and # of plants selected > 0 (for crosses it is always set to 1, for advancing it is set to value of NPSEL trait)
+				 */
 				if (this.getExpressionKey().equals(SequenceExpression.KEY) && source.getCurrentMaxSequence() == 0 && source.getPlantsSelected() != null && source.getPlantsSelected() > 0) {
+					// Extract the root name. Sample value of first StringBuilder is <prefix>[SEQUENCE] 
+					// so replace process code [SEQUENCE] with blank string
 					final StringBuilder value = new StringBuilder(values.get(0));
 					this.replaceExpressionWithValue(value, "");
 					
 					startCount = Integer.parseInt(this.germplasmDataManager.getNextSequenceNumberForCrossName(value.toString()));
-					// The Next Sequence Number  is startCount, so the current max sequence is (startCount - 1)
+					// The current max sequence is set to the last used sequence # before current entry, thus set to (startCount - 1)
 					source.setCurrentMaxSequence(startCount - 1);
+					
 				} else {
 					startCount = source.getCurrentMaxSequence() + 1;
 				}

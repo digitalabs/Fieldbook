f918984721dbcb10823524f7db112fadc17e99c9
Aldrin Batac <aldrin@leafnode.io>
2016-09-16 18:05:57 +0800

    Make the top level 'Lists' node expanded by default if there's no previous tree state.
    BMS-3299

--- src/main/webapp/WEB-INF/static/js/tree-persist.js
+++ src/main/webapp/WEB-INF/static/js/tree-persist.js
@@ -104,19 +104,13 @@ var TreePersist = {
 		return deferred.promise();
 	},
 
-	collapseTopLevelListsNode : function () {
-		// We have to expand the node in order for us to collapse it.
-		$('#germplasmTreeTable').treetable('expandNode', 'LISTS');
-		$('#germplasmTreeTable').treetable('collapseNode', 'LISTS');
-	},
-
 	preLoadTreeTableState: function(listType, isSaveList) {
 		'use strict';
 		TreePersist.retrievePreviousTreeState(listType, isSaveList).done(function(expandedNodes) {
 			TreePersist.traverseNodes(expandedNodes, listType, expandGermplasmListInTreeTable);
 		}).fail(function () {
-			// If there's no previous tree state, the top level 'Lists' node should be collapsed by default.
-			TreePersist.collapseTopLevelListsNode();
+			// If there's no previous tree state, the top level 'Lists' node should be expanded by default.
+			$('#germplasmTreeTable').treetable('expandNode', 'LISTS');
 		});
 	},
 

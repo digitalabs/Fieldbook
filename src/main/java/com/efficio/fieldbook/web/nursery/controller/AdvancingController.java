c83e249518d1b4fc4e8050b90dcf1d19f4431d3b
Naymesh <naymesh@leafnode.io>
2016-12-22 12:45:42 +1300

    Fixing the NPE reported by failing AdvancingControllerTest cases for postAdvanceNursery() method.
    Issue: BMS-3215
    Reviewer: IrynaD

--- src/main/java/com/efficio/fieldbook/web/nursery/controller/AdvancingController.java
+++ src/main/java/com/efficio/fieldbook/web/nursery/controller/AdvancingController.java
@@ -292,7 +292,9 @@ public class AdvancingController extends AbstractBaseFieldbookController {
 		} finally {
 			// Important to clear out the observations collection from user session, once we are done with it to keep heap memory under
 			// control. For large trials/nurseries the observations collection can be huge.
-			this.userSelection.getWorkbook().getObservations().clear();
+			if (this.userSelection.getWorkbook() != null) {
+				this.userSelection.getWorkbook().getObservations().clear();
+			}
 		}
 
 		return results;

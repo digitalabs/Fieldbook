03245ac3d854ecd4df8b6a3d0c4aa98c9deb4120
Vanina Maletta <vmaletta@droptek.com>
2016-12-26 17:28:07 -0300

    Correct initial entryId for crossing when you are using automatic naming rules.
    Issue: BMS-3699
    Reviewer: Clarysabel

--- src/main/java/com/efficio/fieldbook/web/common/service/impl/CrossingServiceImpl.java
+++ src/main/java/com/efficio/fieldbook/web/common/service/impl/CrossingServiceImpl.java
@@ -140,7 +140,7 @@ public class CrossingServiceImpl implements CrossingService {
 	public boolean applyCrossSettingWithNamingRules(final CrossSetting crossSetting, final ImportedCrossesList importedCrossesList,
 			final Integer userId, final Workbook workbook) {
 
-		int entryIdCounter = 1;
+		int entryIdCounter = 0;
 		for (final ImportedCrosses importedCross : importedCrossesList.getImportedCrosses()) {
 			populateSeedSource(importedCross, workbook);
 			importedCross.setEntryCode(String.valueOf(entryIdCounter++));

c0abdcc576cbbe24c33f4c8ab6258d898d04818a
Nahuel Soldevilla <nsoldevilla@droptek.com>
2017-01-24 13:28:22 -0300

    Call save list modal for create crosses flow
    Move the saving of the final cross list to the nursery side of the flow
    
    Issue: BMS-3784 / BMS-3786

--- src/main/webapp/WEB-INF/static/js/nursery-manager/saveAdvanceList.js
+++ src/main/webapp/WEB-INF/static/js/nursery-manager/saveAdvanceList.js
@@ -54,31 +54,6 @@ var SaveAdvanceList = {};
 		);
 	};
 
-	SaveAdvanceList.updateGermplasmList = function() {
-		$.ajax({
-			url: '/Fieldbook/ListTreeManager/updateCrossesList/',
-			type: 'POST',
-			data: null,
-			cache: false,
-			success: function(data) {
-				if (data.isSuccess === 1) {
-					$('#saveListTreeModal').modal('hide');
-					ImportCrosses.displayTabCrossesList(data.germplasmListId, data.crossesListId,  data.listName);
-					$('#saveListTreeModal').data('is-save-crosses', '0');
-					showSuccessfulMessage('', saveListSuccessfullyMessage);
-				} else {
-					showErrorMessage('page-save-list-message-modal', data.message);
-				}
-				if (data.isTrimed === 1) {
-					showAlertMessage('page-save-list-message-modal', crossesWarningMessage, 10000);
-				}
-			},
-			error: function() {
-				showErrorMessage('page-save-list-message-modal', $.fieldbookMessages.errorImportFailed);
-			}
-		});
-	};
-
 	SaveAdvanceList.saveGermplasmList = function() {
 		var chosenNodeFolder = $('#' + getDisplayedTreeName()).dynatree('getTree').getActiveNode();
 		var errorMessageDiv = 'page-save-list-message-modal';

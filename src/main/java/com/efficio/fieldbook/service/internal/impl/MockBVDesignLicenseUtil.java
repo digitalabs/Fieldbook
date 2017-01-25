fa13640abe3936c1dbfcf2aa8d91ca0417370727
Aldrin Batac <aldrin@leafnode.io>
2017-01-25 10:34:33 +0800

    Updated the implementation of MockBVDesignLicenseUtil.
    BMS-3791

--- src/main/java/com/efficio/fieldbook/service/internal/impl/MockBVDesignLicenseUtil.java
+++ src/main/java/com/efficio/fieldbook/service/internal/impl/MockBVDesignLicenseUtil.java
@@ -2,6 +2,7 @@ package com.efficio.fieldbook.service.internal.impl;
 
 import com.efficio.fieldbook.service.internal.DesignLicenseUtil;
 import com.efficio.fieldbook.service.internal.breedingview.BVDesignLicenseInfo;
+import com.efficio.fieldbook.service.internal.breedingview.BVLicenseParseException;
 
 import java.io.IOException;
 
@@ -18,7 +19,7 @@ public class MockBVDesignLicenseUtil implements DesignLicenseUtil {
 	}
 
 	@Override
-	public BVDesignLicenseInfo retrieveLicenseInfo() throws IOException {
+	public BVDesignLicenseInfo retrieveLicenseInfo() throws BVLicenseParseException {
 
 		return null;
 	}

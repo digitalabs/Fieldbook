d088f4e93445ae338d6200109b72ff5147b0a50d
cuenyad <cuenya.diego@gmail.com>
2017-09-08 14:02:43 -0300

    removed the enum type for datakapture and fieldlog and fieldroid.
    ISSUE BMS-4404
    Reviewer Nahuel

--- src/main/java/com/efficio/fieldbook/web/study/ImportStudyType.java
+++ src/main/java/com/efficio/fieldbook/web/study/ImportStudyType.java
@@ -2,7 +2,7 @@
 package com.efficio.fieldbook.web.study;
 
 public enum ImportStudyType {
-	IMPORT_NURSERY_FIELDLOG_FIELDROID(1), IMPORT_NURSERY_EXCEL(3), IMPORT_DATAKAPTURE(4), IMPORT_KSU_EXCEL(5),
+	IMPORT_NURSERY_EXCEL(3), IMPORT_KSU_EXCEL(5),
     IMPORT_KSU_CSV(6), IMPORT_NURSERY_CSV(7);
 
 	private int type;

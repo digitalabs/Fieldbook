28196b9eeaba2126c057c83ec5428c2a1d65bc39
cuenyad <cuenya.diego@gmail.com>
2017-11-06 14:48:37 -0300

    Adding new test class.
    ISSUE BMS-4516
    Reviewer Nahuel

---
+++ src/test/java/com/efficio/fieldbook/web/common/controller/ExportSampleListControllerTest.java
@@ -0,0 +1,93 @@
+package com.efficio.fieldbook.web.common.controller;
+
+import com.efficio.fieldbook.web.common.service.CsvExportSampleListService;
+import com.efficio.fieldbook.web.util.AppConstants;
+import com.efficio.fieldbook.web.util.FieldbookProperties;
+import com.efficio.fieldbook.web.util.SampleListUtilTest;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.generationcp.middleware.domain.sample.SampleDetailsDTO;
+import org.generationcp.middleware.service.api.SampleListService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class ExportSampleListControllerTest {
+
+	private static final String SAMPLE_TRIAL_FILENAME = "Trial33-SampleList";
+	private static final String CSV_EXT = ".csv";
+	private static String UPLOAD_DIRECTORY = "";
+
+	@Mock
+	private FieldbookProperties fieldbookProperties;
+
+	@Mock
+	private CsvExportSampleListService csvExportSampleListService;
+
+	@Mock
+	private SampleListService sampleListService;
+
+	@Mock
+	private HttpServletRequest req;
+
+	@Mock
+	private HttpServletResponse resp;
+
+	@InjectMocks
+	private ExportSampleListController exportSampleListController;
+
+	private static final String CSV_CONTENT_TYPE = "text/csv";
+
+	@Before
+	public void setUp() {
+		Mockito.doReturn(ExportSampleListControllerTest.UPLOAD_DIRECTORY).when(this.fieldbookProperties).getUploadDirectory();
+	}
+
+	@Test
+	public void testDoExportSampleListInCSVFormat()
+		throws IOException {
+		List<SampleDetailsDTO> sampleDetailsDTOs = SampleListUtilTest.initSampleDetailsDTOs();
+		final String outputFilename = ExportSampleListControllerTest.SAMPLE_TRIAL_FILENAME + ExportSampleListControllerTest.CSV_EXT;
+		Mockito.doReturn(sampleDetailsDTOs).when(this.sampleListService).getSampleDetailsDTOs(Matchers.anyInt());
+		Mockito.when(this.csvExportSampleListService.export(Matchers.any(List.class), Matchers.anyString(), Matchers.any(List.class)))
+			.thenReturn(outputFilename);
+		Mockito.when(this.resp.getContentType()).thenReturn(ExportSampleListControllerTest.CSV_CONTENT_TYPE);
+
+		final Integer exportType = AppConstants.EXPORT_CSV.getInt();
+		final Map<String, String> data = this.getData();
+
+		final String returnedValue = this.exportSampleListController.exportSampleList(data, exportType, this.req, this.resp);
+		final HashMap<String, Object> result = new ObjectMapper().readValue(returnedValue, HashMap.class);
+
+		assertThat(true,equalTo((Boolean) result.get(exportSampleListController.IS_SUCCESS)));
+		assertThat(ExportSampleListControllerTest.CSV_CONTENT_TYPE,equalTo(result.get("contentType")));
+		assertThat(outputFilename,equalTo(result.get("filename")));
+		assertThat(outputFilename,equalTo(result.get("outputFilename")));
+
+	}
+
+	private Map<String, String> getData() {
+		final Map<String, String> data = new HashMap<>();
+		data.put("visibleColumns", SampleListUtilTest.getColumnsExport());
+		data.put("listname", "SampleList");
+		data.put("listId", "35");
+		data.put("studyname", "Trial33");
+		return data;
+	}
+}

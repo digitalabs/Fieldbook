e35f5c0168934165e24ddf2e96ebec3088561639
cuenyad <cuenya.diego@gmail.com>
2017-11-08 17:00:36 -0300

    Refactoring, deleting duplicate code.
    ISSUE BMS-4512
    Reviewer Nahuel

--- src/test/java/com/efficio/fieldbook/web/nursery/controller/CreateNurseryControllerTest.java
+++ src/test/java/com/efficio/fieldbook/web/nursery/controller/CreateNurseryControllerTest.java
@@ -1,18 +1,14 @@
 
 package com.efficio.fieldbook.web.nursery.controller;
 
-import java.util.ArrayList;
-import java.util.Random;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
+import com.efficio.fieldbook.service.api.FieldbookService;
+import com.efficio.fieldbook.service.api.WorkbenchService;
+import com.efficio.fieldbook.web.common.bean.UserSelection;
+import com.efficio.fieldbook.web.nursery.form.CreateNurseryForm;
+import com.efficio.fieldbook.web.nursery.form.ImportGermplasmListForm;
+import com.efficio.fieldbook.web.util.FieldbookProperties;
 import org.generationcp.commons.spring.util.ContextUtil;
-import org.generationcp.middleware.domain.dms.Enumeration;
-import org.generationcp.middleware.domain.dms.StandardVariable;
-import org.generationcp.middleware.domain.dms.VariableConstraints;
-import org.generationcp.middleware.domain.oms.StudyType;
-import org.generationcp.middleware.domain.oms.Term;
+import org.generationcp.middleware.data.initializer.StandardVariableTestDataInitializer;
 import org.generationcp.middleware.pojos.workbench.CropType;
 import org.generationcp.middleware.pojos.workbench.Project;
 import org.junit.Before;
@@ -25,12 +21,8 @@ import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.ui.ExtendedModelMap;
 
-import com.efficio.fieldbook.service.api.FieldbookService;
-import com.efficio.fieldbook.service.api.WorkbenchService;
-import com.efficio.fieldbook.web.common.bean.UserSelection;
-import com.efficio.fieldbook.web.nursery.form.CreateNurseryForm;
-import com.efficio.fieldbook.web.nursery.form.ImportGermplasmListForm;
-import com.efficio.fieldbook.web.util.FieldbookProperties;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by cyrus on 21/10/2015.
@@ -77,7 +69,7 @@ public class CreateNurseryControllerTest {
 		Mockito.when(this.contextUtil.getProjectInContext()).thenReturn(project);
 		Mockito.when(this.contextUtil.getCurrentProgramUUID()).thenReturn(this.TEST_PROG_UUID);
 		Mockito.when(this.fieldbookMiddlewareService.getStandardVariable(Matchers.anyInt(), Matchers.eq(this.TEST_PROG_UUID))).thenReturn(
-				this.createTestVariable());
+			StandardVariableTestDataInitializer.createStandardVariable());
 	}
 
 	@Test
@@ -93,24 +85,4 @@ public class CreateNurseryControllerTest {
 		this.controller.useExistingNursery(new CreateNurseryForm(), 0, model, this.session, this.request);
 		SettingsControllerTest.checkVariableSecionIdModelAttributes(model);
 	}
-
-	private StandardVariable createTestVariable() {
-		final StandardVariable stdVariable = new StandardVariable();
-		stdVariable.setName("variable name " + new Random().nextInt(10000));
-		stdVariable.setDescription("variable description");
-		stdVariable.setProperty(new Term(2002, "User", "Database user"));
-		stdVariable.setMethod(new Term(4030, "Assigned", "Term, name or id assigned"));
-		stdVariable.setScale(new Term(61220, "DBCV", "Controlled vocabulary from a database"));
-		stdVariable.setDataType(new Term(1120, "Character variable", "variable with char values"));
-		stdVariable.setIsA(new Term(1050, "Study condition", "Study condition class"));
-		stdVariable.setEnumerations(new ArrayList<Enumeration>());
-		stdVariable.getEnumerations().add(new Enumeration(StudyType.N.getId(), StudyType.N.getName(),  StudyType.N.getLabel(), 1));
-		stdVariable.getEnumerations().add(new Enumeration(StudyType.HB.getId(), StudyType.HB.getName(),  StudyType.HB.getLabel(), 2));
-		stdVariable.getEnumerations().add(new Enumeration(StudyType.PN.getId(), StudyType.PN.getName(),  StudyType.PN.getLabel(), 3));
-		stdVariable.setConstraints(new VariableConstraints(100.0, 999.0));
-		stdVariable.setCropOntologyId("CROP-TEST");
-
-		return stdVariable;
-	}
-
 }

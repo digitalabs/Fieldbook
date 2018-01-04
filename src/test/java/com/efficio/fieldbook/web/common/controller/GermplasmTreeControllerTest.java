39b807a0574db8b71f8ed0cb4dde73930073daa4
Darla Hermo <darla@leafnode.io>
2018-01-04 15:40:23 +0800

    Remove code that references unused Workbench table "workbench_runtime_data"
    BMS-187 / BMS-4630

--- src/test/java/com/efficio/fieldbook/web/common/controller/GermplasmTreeControllerTest.java
+++ src/test/java/com/efficio/fieldbook/web/common/controller/GermplasmTreeControllerTest.java
@@ -37,7 +37,6 @@ import org.generationcp.middleware.pojos.Method;
 import org.generationcp.middleware.pojos.Name;
 import org.generationcp.middleware.pojos.UserDefinedField;
 import org.generationcp.middleware.pojos.workbench.Project;
-import org.generationcp.middleware.pojos.workbench.WorkbenchRuntimeData;
 import org.generationcp.middleware.service.api.FieldbookService;
 import org.junit.Before;
 import org.junit.Test;
@@ -128,9 +127,6 @@ public class GermplasmTreeControllerTest {
 	private AbstractBaseFieldbookController abstractFieldbookController;
 
 	@Mock
-	private WorkbenchRuntimeData workbenchRuntimeData;
-
-	@Mock
 	private UserTreeStateService userTreeStateService;
 
 	@InjectMocks
@@ -139,7 +135,6 @@ public class GermplasmTreeControllerTest {
 	@Before
 	public void setUp() throws MiddlewareQueryException {
 		Mockito.doReturn(this.getProject()).when(this.workbenchDataManager).getLastOpenedProjectAnyUser();
-		Mockito.doReturn(this.workbenchRuntimeData).when(this.workbenchDataManager).getWorkbenchRuntimeData();
 		Mockito.doReturn(this.createCrossSetting()).when(this.userSelection).getCrossSettings();
 		Mockito.doReturn(this.createImportedCrossesList()).when(this.userSelection).getImportedCrossesList();
 		Mockito.doReturn(this.createWorkBook()).when(this.userSelection).getWorkbook();

71ae468558ecbfdd069ee0022d895eeab9035b77
cuenyad <cuenya.diego@gmail.com>
2018-05-14 11:38:38 -0300

    Fixing test
    Issue: BMS-4886
    Reviewer: None

--- src/test/java/com/efficio/etl/web/controller/angular/AngularSelectSheetControllerTest.java
+++ src/test/java/com/efficio/etl/web/controller/angular/AngularSelectSheetControllerTest.java
@@ -1,13 +1,19 @@
 package com.efficio.etl.web.controller.angular;
 
 import junit.framework.Assert;
-import org.generationcp.middleware.domain.oms.StudyType;
+import org.generationcp.middleware.domain.study.StudyTypeDto;
+import org.generationcp.middleware.manager.api.StudyDataManager;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -16,14 +22,24 @@ public class AngularSelectSheetControllerTest {
 	@InjectMocks
 	private AngularSelectSheetController angularSelectSheetController;
 
+	@Mock
+	StudyDataManager studyDataManager;
+
+	@Before
+	public void init() {
+		List<StudyTypeDto> studyTypeDtoList = new ArrayList<>();
+		studyTypeDtoList.add(new StudyTypeDto(10000, "Nursery","N"));
+		studyTypeDtoList.add(new StudyTypeDto(10010, "Trial","T"));
+
+		Mockito.when(this.studyDataManager.getAllVisibleStudyTypes()).thenReturn(studyTypeDtoList);
+	}
+
 	@Test
 	public void testGetStudyTypes() {
 
 		final Map<String, String> expectedStudyTypesMap = new HashMap<>();
-
-		for (final StudyType type : StudyType.values()) {
-			expectedStudyTypesMap.put(type.getName(), type.getLabel());
-		}
+			expectedStudyTypesMap.put("N", "Nursery");
+			expectedStudyTypesMap.put("T", "Trial");
 
 		final Map<String, String> result = angularSelectSheetController.getStudyTypes();
 

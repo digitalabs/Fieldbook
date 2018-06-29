0dab56cb52cf1f501761f25ba52ce3cdb24a0fa0
Nahuel Soldevilla <nahuel@leafnode.io>
2018-06-29 19:18:13 -0300

    Draft of variable calculation
    - Remove transactional, not needed since we are using a dto now
    - Execute calculation and load observations with result
    
    Issue: BMS-4902 / BMS-4886 / BMS-4966
    Reviewer: Diego

--- src/main/java/com/efficio/fieldbook/web/common/controller/derived_variables/DerivedVariableController.java
+++ src/main/java/com/efficio/fieldbook/web/common/controller/derived_variables/DerivedVariableController.java
@@ -1,12 +1,15 @@
 package com.efficio.fieldbook.web.common.controller.derived_variables;
 
 import com.efficio.fieldbook.web.common.bean.UserSelection;
+import com.efficio.fieldbook.web.util.ExportImportStudyUtil;
 import com.efficio.fieldbook.web.util.WorkbookUtil;
 import com.google.common.base.Optional;
+import org.generationcp.commons.derivedvariable.DerivedVariableProcessor;
+import org.generationcp.middleware.domain.etl.MeasurementData;
 import org.generationcp.middleware.domain.etl.MeasurementRow;
 import org.generationcp.middleware.domain.etl.Workbook;
+import org.generationcp.middleware.domain.oms.TermId;
 import org.generationcp.middleware.domain.ontology.FormulaDto;
-import org.generationcp.middleware.pojos.derived_variables.Formula;
 import org.generationcp.middleware.service.api.FieldbookService;
 import org.generationcp.middleware.service.api.derived_variables.FormulaService;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -14,7 +17,6 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +25,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Controller
@@ -46,8 +47,6 @@ public class DerivedVariableController {
 		return this.messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
 	}
 
-	// TODO remove transational, see FormulaServiceImpl#getByTargetId
-	@Transactional
 	@ResponseBody
 	@RequestMapping(value = "/derived-variable/execute", method = RequestMethod.POST)
 	public ResponseEntity<Map<String, Object>> execute(@RequestBody final CalculateVariableRequest request, final BindingResult result) {
@@ -74,14 +73,49 @@ public class DerivedVariableController {
 
 		final Optional<FormulaDto> formula = this.formulaService.getByTargetId(request.getVariableId());
 
-		final Map<String, MeasurementRow> measurementRowsMap = new LinkedHashMap<>();
-		if (workbook.getObservations() != null) {
-			for (final MeasurementRow row : workbook.getObservations()) {
-				measurementRowsMap.put(row.getKeyIdentifier(), row);
-			}
+		if (!formula.isPresent()) {
+			// TODO
+			results.put("errorMessage", getMessage(""));
+			return new ResponseEntity<>(results, HttpStatus.BAD_REQUEST);
 		}
 
 		// Calculate
+
+		for (final MeasurementRow row : workbook.getObservations()) {
+			final DerivedVariableProcessor processor = new DerivedVariableProcessor();
+			final Map<String, Object> terms = processor.extractTermsFromFormula(formula.get().getDefinition());;
+
+			processor.fetchTermValuesFromMeasurement(terms, row);
+
+			String value = processor.evaluateFormula(formula.get().getDefinition(), terms, null);
+
+			final MeasurementData target = row.getMeasurementData(formula.get().getTargetTermId());
+
+			target.setAccepted(false);
+
+			// Process categorical data
+
+			final String categoricalValue = ExportImportStudyUtil.getCategoricalIdCellValue(value,
+				target.getMeasurementVariable().getPossibleValues(), true);
+
+			if (target.getMeasurementVariable().getDataTypeId() == TermId.CATEGORICAL_VARIABLE.getId()
+				&& !categoricalValue.equals(value)) {
+				value = categoricalValue;
+				target.setcValueId(value);
+			} else {
+				target.setcValueId(null);
+			}
+
+			// Set overwrite info
+
+			if (!target.getValue().equals(value)) {
+				if (!target.getValue().isEmpty()) {
+					workbook.setHasExistingDataOverwrite(true);
+				}
+				target.setValue(value);
+				target.setOldValue(value);
+			}
+		}
 
 		// Process response
 

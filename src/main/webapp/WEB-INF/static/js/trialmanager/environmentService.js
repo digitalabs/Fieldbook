a3f6c655acb183e8530dc7aa65d2287157be311c
cuenyad <cuenya.diego@gmail.com>
2018-10-02 14:58:19 -0300

    services to recover the environment through study.
    Issue: BMS-5191
    Reviewer: None

--- src/main/webapp/WEB-INF/static/js/trialmanager/environmentService.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/environmentService.js
@@ -1,22 +1,67 @@
 /*global angular*/
 
-(function() {
+(function () {
 	'use strict';
 
 	var manageTrialApp = angular.module('manageTrialApp');
 
-	manageTrialApp.factory('environmentService', ['$rootScope', 'TrialManagerDataService', function($rootScope, TrialManagerDataService) {
+	manageTrialApp.factory('environmentService', ['$rootScope', 'TrialManagerDataService', '$http', function ($rootScope, TrialManagerDataService, $http) {
 
 		var environmentService = {};
+		var service = {
+			environments: []
+			, getEnvironments: function () {
+				var xAuthToken = JSON.parse(localStorage["bms.xAuthToken"]).token;
+				var config = {
+					headers: {
+						'X-Auth-Token': xAuthToken
+					}
+				};
 
+				$http.get('/bmsapi/study/' + cropName + '/' + TrialManagerDataService.currentData.basicDetails.studyID + '/instances', config).success(function (data) {
+					service.environments = data;
+
+				}).error(function (data) {
+
+					if (data.status == 401) {
+						bmsAuth.handleReAuthentication();
+					}
+
+					showErrorMessage('', data.errors[0].message);
+					service.environments = [];
+				});
+			}
+		};
+
+		service.getEnvironments();
 		environmentService.environments = TrialManagerDataService.currentData.environments;
 
-		environmentService.changeEnvironments = function() {
+		environmentService.changeEnvironments = function () {
 			this.broadcastEnvironments();
 		};
 
-		environmentService.broadcastEnvironments = function() {
+		environmentService.broadcastEnvironments = function () {
 			$rootScope.$broadcast('changeEnvironments');
+		};
+
+		environmentService.getEnvironmentDetails = function () {
+			var environmentListView = [];
+
+			angular.forEach(service.environments, function (environment) {
+				environmentListView.push({
+					name: environment.locationName + ' - (' + environment.locationAbbreviation + ')',
+					abbrName: environment.locationAbbreviation,
+					customAbbrName: environment.customLocationAbbreviation,
+					trialInstanceNumber: environment.instanceNumber,
+					instanceDbId: environment.instanceDbId
+
+				});
+			});
+			return environmentListView;
+		};
+
+		environmentService.updateEnvironmentData = function(){
+			service.getEnvironments();
 		};
 
 		return environmentService;

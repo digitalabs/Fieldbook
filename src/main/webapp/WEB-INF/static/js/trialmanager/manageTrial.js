cb36bdaac4c86e33f4dbfc10624bcfd44064526a
cuenyad <cuenya.diego@gmail.com>
2018-10-19 18:54:05 -0300

    injector the config
    Issue: BMS-5192
    Reviewer: None

--- src/main/webapp/WEB-INF/static/js/trialmanager/manageTrial.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/manageTrial.js
@@ -9,7 +9,7 @@ stockListImportNotSaved, ImportDesign, isOpenStudy, displayAdvanceList, Inventor
 
 	var manageTrialApp = angular.module('manageTrialApp', ['designImportApp', 'leafnode-utils', 'fieldbook-utils',
 		'ct.ui.router.extras', 'ui.bootstrap', 'ngLodash', 'ngResource', 'ngStorage', 'datatables', 'datatables.buttons',
-		'showSettingFormElementNew', 'ngSanitize', 'ui.select','ngMessages']);
+		'showSettingFormElementNew', 'ngSanitize', 'ui.select', 'ngMessages', 'config']);
 
 	/*** Added to prevent Unsecured HTML error
 	   It is used by ng-bind-html ***/

4d9aa9af3ffd117023a7aa88b704c5bf60e1282d
cuenyad <cuenya.diego@gmail.com>
2019-01-23 13:41:46 -0300

    Formatting code.
    Issue: IBP-2107
    Reviewer: None

--- src/main/webapp/WEB-INF/static/js/trialmanager/executeCalculatedVariableModal.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/executeCalculatedVariableModal.js
@@ -11,71 +11,71 @@
 			$scope.selectedInstances = {};
 			$scope.isEmptySelection = false;
 
-		$scope.init = function () {
-			$scope.calculateVariableLocationForm.$setPristine();
-			$scope.environmentSelected = undefined;
-			$scope.variableSelected = undefined;
+			$scope.init = function () {
+				$scope.calculateVariableLocationForm.$setPristine();
+				$scope.environmentSelected = undefined;
+				$scope.variableSelected = undefined;
 
-			datasetService.getDataset(studyContext.measurementDatasetId).then(function (dataset) {
-				$scope.variableListView = buildVariableListView(dataset.variables);
-				$scope.instances = dataset.instances;
+				datasetService.getDataset(studyContext.measurementDatasetId).then(function (dataset) {
+					$scope.variableListView = buildVariableListView(dataset.variables);
+					$scope.instances = dataset.instances;
 
-			});
+				});
 
-		};
-
-		$scope.proceedExecution = function () {
-			$('.import-study-data').data('data-import', '1');
-			$('body').addClass('import-preview-measurements');
-			var columnsOrder = BMS.Fieldbook.MeasurementsTable.getColumnOrdering('measurement-table');
-			new BMS.Fieldbook.ImportPreviewMeasurementsDataTable('#import-preview-measurement-table', JSON.stringify(columnsOrder));
-			$('.fbk-discard-imported-data').removeClass('fbk-hide');
-			showSuccessfulMessage('', 'Calculated values for ' + $scope.variableSelected.name + ' were added successfully.');
-		};
-
-		$scope.execute = function () {
-			var geoLocationIds = [];
-
-			Object.keys($scope.selectedInstances).forEach(function (instanceDbId) {
-				var isSelected = $scope.selectedInstances[instanceDbId];
-				if (isSelected) {
-					geoLocationIds.push(instanceDbId);
-				}
-			});
-
-			var calculateData = {
-				variableId: $scope.variableSelected.cvTermId
-				, geoLocationIds: geoLocationIds
 			};
 
-			$http.post('/Fieldbook/DerivedVariableController/derived-variable/execute', JSON.stringify(calculateData))
-				.then(function (response) {
-					$('#executeCalculatedVariableModal').modal('hide');
-					if (response.data && response.data.inputMissingData) {
-						showAlertMessage('', response.data.inputMissingData, 15000);
-					}
-					if (response.data && response.data.hasDataOverwrite) {
-						$('#confirmOverrideCalculatedVariableModal').modal({backdrop: 'static', keyboard: true});
+			$scope.proceedExecution = function () {
+				$('.import-study-data').data('data-import', '1');
+				$('body').addClass('import-preview-measurements');
+				var columnsOrder = BMS.Fieldbook.MeasurementsTable.getColumnOrdering('measurement-table');
+				new BMS.Fieldbook.ImportPreviewMeasurementsDataTable('#import-preview-measurement-table', JSON.stringify(columnsOrder));
+				$('.fbk-discard-imported-data').removeClass('fbk-hide');
+				showSuccessfulMessage('', 'Calculated values for ' + $scope.variableSelected.name + ' were added successfully.');
+			};
 
-						// Add hide listener to confirmOverrideCalculatedVariableModal
-						$('#confirmOverrideCalculatedVariableModal').one('hidden.bs.modal', function (e) {
-							// When the confirmOverrideCalculatedVariableModal is closed, remove the bs.modal data
-							// so that the modal content is refreshed when it is opened again.
-							$(e.target).removeData('bs.modal');
-						});
-						angular.element('#confirmOverrideCalculatedVariableModal').scope();
-					} else {
-						$scope.proceedExecution();
-					}
-				}, function (response) {
-					if (response.data.errorMessage) {
-						showErrorMessage('', response.data.errorMessage);
-					} else {
-						showErrorMessage('', ajaxGenericErrorMsg);
+			$scope.execute = function () {
+				var geoLocationIds = [];
+
+				Object.keys($scope.selectedInstances).forEach(function (instanceDbId) {
+					var isSelected = $scope.selectedInstances[instanceDbId];
+					if (isSelected) {
+						geoLocationIds.push(instanceDbId);
 					}
 				});
 
-		};
+				var calculateData = {
+					variableId: $scope.variableSelected.cvTermId
+					, geoLocationIds: geoLocationIds
+				};
+
+				$http.post('/Fieldbook/DerivedVariableController/derived-variable/execute', JSON.stringify(calculateData))
+					.then(function (response) {
+						$('#executeCalculatedVariableModal').modal('hide');
+						if (response.data && response.data.inputMissingData) {
+							showAlertMessage('', response.data.inputMissingData, 15000);
+						}
+						if (response.data && response.data.hasDataOverwrite) {
+							$('#confirmOverrideCalculatedVariableModal').modal({backdrop: 'static', keyboard: true});
+
+							// Add hide listener to confirmOverrideCalculatedVariableModal
+							$('#confirmOverrideCalculatedVariableModal').one('hidden.bs.modal', function (e) {
+								// When the confirmOverrideCalculatedVariableModal is closed, remove the bs.modal data
+								// so that the modal content is refreshed when it is opened again.
+								$(e.target).removeData('bs.modal');
+							});
+							angular.element('#confirmOverrideCalculatedVariableModal').scope();
+						} else {
+							$scope.proceedExecution();
+						}
+					}, function (response) {
+						if (response.data.errorMessage) {
+							showErrorMessage('', response.data.errorMessage);
+						} else {
+							showErrorMessage('', ajaxGenericErrorMsg);
+						}
+					});
+
+			};
 
 			function buildVariableListView(variables) {
 				var variableListView = [];
@@ -87,7 +87,7 @@
 				return variableListView;
 			};
 
-	}]);
+		}]);
 
 	manageTrialApp.controller('ConfirmOverrideCalculatedVariableModalCtrl', ['$scope', '$http', function ($scope, $http) {
 

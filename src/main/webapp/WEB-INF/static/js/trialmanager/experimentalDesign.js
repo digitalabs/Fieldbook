159bcaeb0ffc755dfe2592ce0bfdd764005a9126
mariebawanan <bawananmarieflor@gmail.com>
2019-04-04 12:32:40 +0800

    Removed validation that checks the number of contiguous blocks per rep in row-column design with their corresponding error messages
    IBP-1735

--- src/main/webapp/WEB-INF/static/js/trialmanager/experimentalDesign.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/experimentalDesign.js
@@ -454,16 +454,6 @@
 
 								if ($scope.data.useLatenized) {
 
-									if (Number($scope.data.nrlatin) >= Number($scope.data.replicationsCount)) {
-										showErrorMessage('page-message', EXP_DESIGN_MSGS[15]);
-										return false;
-									}
-
-									if (Number($scope.data.nclatin) >= Number($scope.data.replicationsCount)) {
-										showErrorMessage('page-message', EXP_DESIGN_MSGS[16]);
-										return false;
-									}
-
 									if (Number($scope.data.nrlatin) <= 0 || Number($scope.data.nrlatin) >= Number($scope.data.rowsPerReplications)) {
 										showErrorMessage('page-message', EXP_DESIGN_MSGS[14]);
 										return false;

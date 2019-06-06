fc050531980baf18a22bdc23f9f738fe3a1d81f7
cuenyad <cuenya.diego@gmail.com>
2019-06-06 14:37:22 -0300

    Replacing trialMeasurement by studyStateService.hasGeneratedDesign()
    Issue:IBP-2789
    Review: none

--- src/main/webapp/WEB-INF/static/js/trialmanager/treatment.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/treatment.js
@@ -9,10 +9,10 @@
 
 	var manageTrialApp = angular.module('manageTrialApp');
 
-	manageTrialApp.controller('TreatmentCtrl', ['$scope', 'TrialManagerDataService', '_', '$q', '$http',
-		function($scope, TrialManagerDataService, _, $q, $http) {
+	manageTrialApp.controller('TreatmentCtrl', ['$scope', 'TrialManagerDataService', '_', '$q', '$http', 'studyStateService',
+		function($scope, TrialManagerDataService, _, $q, $http, studyStateService) {
 
-			$scope.disableTreatment = TrialManagerDataService.trialMeasurement.hasMeasurement;
+			$scope.disableTreatment = studyStateService.hasGeneratedDesign();
 
 			$scope.settings = TrialManagerDataService.settings.treatmentFactors;
 			$scope.data = TrialManagerDataService.currentData.treatmentFactors;
@@ -22,7 +22,7 @@
 			// store to $scope.currentData for the variable levels.
 
 			$scope.trialMeasurement = TrialManagerDataService.trialMeasurement;
-
+			$scope.trialMeasurement.hasMeasurement = studyStateService.hasGeneratedDesign();
 			TrialManagerDataService.onUpdateSettings('treatmentFactors', function(newValue) {
 				TrialManagerDataService.specialSettings.treatmentLevelPairs = $scope.settings.treatmentLevelPairs;
 			});
@@ -70,7 +70,7 @@
 
 			$scope.invalidBlockSizeMsg = '<b class="text-danger">Invalid Block Size</b>';
 			$scope.addVariable = function() {
-				return !TrialManagerDataService.trialMeasurement.hasMeasurement;
+				return !studyStateService.hasGeneratedDesign();
 			};
 
 			$scope.generateDropdownList = function(key) {

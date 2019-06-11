6af318130023fa964c0b2ec0ef2a493706df7f5a
cuenyad <cuenya.diego@gmail.com>
2019-06-11 10:37:53 -0300

    refactor code, adding unsaved data condition when add/remove variables and remove the condition to verify if exists observations doesn't make sense at study level.
    Issue:IBP-2789
    Review: none

--- src/main/webapp/WEB-INF/static/js/trialmanager/trialSettings.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/trialSettings.js
@@ -5,7 +5,7 @@
 
 	var manageTrialApp = angular.module('manageTrialApp');
 
-	manageTrialApp.controller('TrialSettingsCtrl', ['$scope', 'TrialManagerDataService', '_', '$filter', 'VARIABLE_TYPES', 'datasetService', 'studyContext', '$q', function ($scope, TrialManagerDataService, _, $filter, VARIABLE_TYPES, datasetService, studyContext, $q) {
+	manageTrialApp.controller('TrialSettingsCtrl', ['$scope', 'TrialManagerDataService', '_', '$filter', 'studyStateService', function ($scope, TrialManagerDataService, _, $filter, studyStateService) {
 
 		$scope.data = TrialManagerDataService.currentData.trialSettings;
 		$scope.addVariable = true;
@@ -27,50 +27,19 @@
 		};
 
 		$scope.removeSettings = function (variableType, variables, options) {
-			var checkedVariableTermIds = $scope.retrieveCheckedVariableTermIds(variables);
+			TrialManagerDataService.removeSettings(variableType, variables).then(function (data) {
+				_(data).each(function (ids) {
+					delete $scope.data.userInput[ids];
+				});
 
-			var promise = $scope.validateRemoveVariable(checkedVariableTermIds, observationVariableDeleteConfirmationText);
-
-			promise.then(function (doContinue) {
-				if (doContinue) {
-					TrialManagerDataService.removeSettings(variableType, variables).then(function (data) {
-						_(data).each(function (ids) {
-							delete $scope.data.userInput[ids];
-						});
-
-						options.selectAll = false;
-					});
-				}
+				options.selectAll = false;
+				studyStateService.updateOccurred();
 			});
 		};
 
-		$scope.retrieveCheckedVariableTermIds = function(_settings) {
-			var checkedCvtermIds = _.pairs(_settings.vals())
-				.filter(function(val) {
-					return _.last(val).isChecked;
-				})
-				.map(function(val) {
-					return parseInt(_.first(val));
-				});
-			return checkedCvtermIds;
-		};
-
-		$scope.validateRemoveVariable = function (deleteVariables, message) {
-			var deferred = $q.defer();
-			if (deleteVariables.length != 0) {
-				datasetService.observationCount(studyContext.measurementDatasetId, deleteVariables).then(function (response) {
-					var count = response.headers('X-Total-Count');
-					if (count > 0) {
-						var modalInstance = $scope.openConfirmModal(message,
-							environmentConfirmLabel);
-						modalInstance.result.then(deferred.resolve);
-					} else {
-						deferred.resolve(true);
-					}
-				});
-			}
-			return deferred.promise;
-		};
+		$scope.$on('variableAdded', function () {
+			studyStateService.updateOccurred();
+		});
 
 	}]);
 

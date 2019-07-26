126c56935dfbfd13fb2b233ac03243b69b82158b
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2019-07-26 14:48:02 +0800

    Assign the data to the js object
    IBP-2784

--- src/main/webapp/WEB-INF/static/js/trialmanager/chooseUser.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/chooseUser.js
@@ -4,20 +4,20 @@
 	'use strict';
 
 	var manageTrialApp = angular.module('manageTrialApp');
-	
+
 	manageTrialApp.controller('selectUserCtrl', ['$scope', '$http', '_', function($scope, $http, _) {
         $scope.currentProgramMembers = [];
 
         $http.get('/Fieldbook/crosses/getCurrentUser')
             .then(function(userId) {
-                $scope.selectedUserId = userId;
+                $scope.selectedUserId = userId.data;
             }).catch(function(response) {
                 showErrorMessage('', $.fieldbookMessages.errorNoCurrentUser);
             });
 
         $http.get('/Fieldbook/crosses/getCurrentProgramMembers')
             .then(function(programMembers) {
-                $scope.currentProgramMembers = programMembers;
+                $scope.currentProgramMembers = programMembers.data;
                 if (!$scope.selectedUserId) {
                     $scope.selectedUserId = _.keys($scope.currentProgramMembers[0]);
                 }

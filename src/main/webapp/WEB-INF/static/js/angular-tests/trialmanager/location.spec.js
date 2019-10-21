125f15be87d8526bb6a446b369220d5b847f5bb4
gelli-leafnode <gelli@leafnode.io>
2019-10-21 15:40:22 +0800

    Unit Test Changes: callback returns null should not throw an error, callback returns empty array should not throw an error IBP-3058-CheckResponseIfNull

--- src/main/webapp/WEB-INF/static/js/angular-tests/trialmanager/location.spec.js
+++ src/main/webapp/WEB-INF/static/js/angular-tests/trialmanager/location.spec.js
@@ -5,41 +5,100 @@ describe('Location', function () {
     var trialDataManagerService = {
         settings: {
             environments: [],
-            managementDetails: [8170]
+            managementDetails: [23057]
         },
         currentData: {
             environments: {
                 noOfEnvironments: 2
             }
+        }, 
+        onUpdateData: function () {
+
+        },
+        checkVariableIsUsedInCalculatedVariable: function () {
+            
         }
     };
-    var rootScopeMock = {
-        openConfirmModal: jasmine.createSpy('openConfirmModal')
+
+    var scope =  jasmine.createSpyObj('scope', ['checkVariableIsUsedInCalculatedVariable']);
+    var rootScope = jasmine.createSpyObj('$rootScope', ['openConfirmModal']);
+
+    var studyContext = {
+        studyId: 1,
+        cropName: 'maize',
+        measurementDatasetId: null
     };
-    var datasetServiceMock = {observationCountByInstance: jasmine.createSpy('observationCountByInstance')};
+
+    var datasetService = jasmine.createSpyObj('datasetService', ['getDatasetInstances', 'exportDataset']);
+    var derivedVariableService=  jasmine.createSpyObj('derivedVariableService', ['getFormulaVariables']);
+    var formulaVariables;
+
     beforeEach(function(){
+
+        module(function ($provide) {
+
+            $provide.value("studyContext", studyContext);
+            $provide.value("serviceUtilities", {});
+            $provide.value("DATASET_TYPES_OBSERVATION_IDS", [4, 5, 6, 7, 8]);
+            $provide.value("DATASET_TYPES", {});
+            $provide.value("derivedVariableService", derivedVariableService);
+            $provide.value("TrialManagerDataService", trialDataManagerService);
+
+        });
+
+
+        angular.module('subObservation');
+        angular.module('datasets-api');
+        angular.module('fieldbook-utils');
+        angular.module('ui.bootstrap');
+        angular.module('datasetOptionModal');
+        angular.module('leafnode-utils');
+        angular.module('derived-variable');
         module('manageTrialApp');
-        module(function ($provider) {
-            $provider.value("TrialManagerDataService", trialDataManagerService);
-        })
+
     });
 
-    beforeEach(function () {
-        inject(function ($rootScope, $controller) {
 
+    beforeEach(function () {
+        inject(function ($rootScope, $controller, $injector){
+
+            scope = $rootScope.$new();
+            datasetService = $injector.get('datasetService');
+            derivedVariableService.getFormulaVariables = function(dataSetId) {
+
+                return {
+                    then: function (callback) {
+                        dataSetId = '';
+                        return callback(formulaVariables);//force to return null
+                    }
+                };
+            };
             controller = $controller('EnvironmentCtrl',{
-                $rootScope: rootScopeMock,
-                $scope: $rootScope,
+                $rootScope: rootScope,
+                $scope: scope,
                 studyContext: studyContext,
-                datasetService: datasetServiceMock,
+                derivedVariableService: derivedVariableService,
                 TrialManagerDataService: trialDataManagerService,
                 environmentService: {},
                 LOCATION_ID: 1
             });
         });
     });
-    describe('Location Delete', function () {
-        it('should delete', function () {
+
+
+    describe('Location Detail Delete', function () {
+        it('unsaved study remove should not throw an error', function () {
+            formulaVariables = null;
+            expect(function(){
+                scope.checkVariableIsUsedInCalculatedVariable()
+            }).not.toThrow();
+        });
+
+        it('saved study remove should not throw an error', function () {
+            formulaVariables = [];
+            expect(scope.checkVariableIsUsedInCalculatedVariable()).not.toBeNull();
         });
     });
+
+
 });

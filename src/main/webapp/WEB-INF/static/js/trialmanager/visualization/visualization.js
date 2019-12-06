44dd72d394acb180fc12fe51d31a4ec6c405b468
Aldrin Batac <aldrin.batac@leafnode.io>
2019-12-06 08:09:51 +0800

    Reset x and y variable selection when plot type is changed. Add numbers-only directive to binwidth textbox so that it only accepts numbers.
    IBP-3202

--- src/main/webapp/WEB-INF/static/js/trialmanager/visualization/visualization.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/visualization/visualization.js
@@ -76,6 +76,11 @@
 				$scope.init();
 			}
 
+			$scope.$watch('selection.selectedRCall', function () {
+				$scope.selection.selectedVariableX = null;
+				$scope.selection.selectedVariableY = null;
+			});
+
 			$scope.init = function () {
 				var qplotPackageId = 3;
 				rPackageService.getRCallsObjects(qplotPackageId).then(function (data) {
@@ -170,5 +175,27 @@
 		};
 
 	}]);
+
+
+	visualizationModule.directive('numbersOnly', function () {
+		return {
+			require: 'ngModel',
+			link: function (scope, element, attr, ngModelCtrl) {
+				function fromUser(text) {
+					if (text) {
+						var transformedInput = text.replace(/[^0-9.]/g, '');
+
+						if (transformedInput !== text) {
+							ngModelCtrl.$setViewValue(transformedInput);
+							ngModelCtrl.$render();
+						}
+						return transformedInput;
+					}
+					return undefined;
+				}
+				ngModelCtrl.$parsers.push(fromUser);
+			}
+		};
+	});
 
 })();

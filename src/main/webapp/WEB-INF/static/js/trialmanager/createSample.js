499e33a12056d0c3632ec9b0949bed3a4b31109c
Darla Hermo <darla@leafnode.io>
2020-01-15 12:42:12 +0800

    Add cropName as request parameter to API call for filtering users per program
    IBP-3039

--- src/main/webapp/WEB-INF/static/js/trialmanager/createSample.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/createSample.js
@@ -143,7 +143,7 @@
 					}
 				});
 
-				$http.get('/bmsapi/users/filter?programUUID=' + currentProgramId).success(function (data) {
+				$http.get('/bmsapi/users/filter?cropName='+ cropName +'&programUUID=' + currentProgramId).success(function (data) {
 					$scope.users = data;
 
 					angular.forEach($scope.users, function (user) {

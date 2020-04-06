052ededb12b8e3563dbda8bba553f5937672229b
Nahuel Soldevilla <nahuel@leafnode.io>
2020-04-06 15:17:10 -0300

    GeoJSON Editor modal: first cut
    Selects an environment and opens geojson editor iframe for that
    instanceId.
    
    Issue: IBP-3183 / IBP-3218

--- src/main/webapp/WEB-INF/static/js/trialmanager/manageTrial.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/manageTrial.js
@@ -976,6 +976,40 @@ stockListImportNotSaved, ImportDesign, isOpenStudy, displayAdvanceList, Inventor
 				});
 			};
 
+			$scope.showGeoJSONModal = function (isViewGeoJSON) {
+				$uibModal.open({
+					template: '<single-instance-selector-modal instances="instances" ' +
+						' instance-id-property="instanceDbId" ' +
+						' selected="selected" ' +
+						' on-select-instance="onSelectInstance" ' +
+						' on-continue="onContinue" ' +
+						' ></single-instance-selector-modal>',
+					controller: function ($scope, $uibModalInstance) {
+						$scope.selected = {};
+						datasetService.getDatasetInstances(studyContext.measurementDatasetId).then(function (datasetInstances) {
+							$scope.instances = datasetInstances;
+						});
+
+						$scope.onContinue = function () {
+							$uibModal.open({
+								templateUrl: '/Fieldbook/static/angular-templates/geojson/geojson-modal.html',
+								size: 'lg',
+								controller: 'GeoJSONModalCtrl',
+								resolve: {
+									isViewGeoJSON: function () {
+										return Boolean(isViewGeoJSON);
+									},
+									instanceId: function () {
+										return $scope.selected.instanceDbId;
+									}
+								}
+							});
+						};
+					}
+
+				});
+			};
+
 			$scope.showCreateSampleListModal = function() {
 				createSampleModalService.openDatasetOptionModal();
 			}

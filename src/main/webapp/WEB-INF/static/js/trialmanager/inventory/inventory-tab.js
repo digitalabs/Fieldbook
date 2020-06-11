c6c48d67d44568c72cd4f3522260e7508ecfb64c
Nahuel Soldevilla <nahuel@leafnode.io>
2020-06-11 16:34:18 -0300

    Gid and designation links
    Issue: IBP-3752

--- src/main/webapp/WEB-INF/static/js/trialmanager/inventory/inventory-tab.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/inventory/inventory-tab.js
@@ -384,6 +384,14 @@
 					}
 				},
 				{
+					targets: "germplasm-link-column",
+					render: function (data, type, rowData, meta) {
+						return '<a class="gid-link" href="javascript: void(0)"'
+							+ ` onclick="openGermplasmDetailsPopopWithGidAndDesig('${rowData.lot.gid}','${rowData.lot.designation}')">`
+							+ EscapeHTML.escape(data) + '</a>';
+					}
+				},
+				{
 					targets: "_all",
 					orderable: false
 				}

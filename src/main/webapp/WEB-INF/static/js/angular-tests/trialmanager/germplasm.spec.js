ca2b2cb497a8b8d63ad0202a97d7aaf0551160f9
darla-leafnode <darla@leafnode.io>
2020-06-19 19:08:00 +0800

    Create frontend tests for replace germplasm
    IBP-3773

---
+++ src/main/webapp/WEB-INF/static/js/angular-tests/trialmanager/germplasm.spec.js
@@ -0,0 +1,73 @@
+'use strict';
+
+describe('Replace Germplasm Controller', function () {
+	var replaceGermplasmCtrl, scope, $q;
+	var studyGermplasmService = jasmine.createSpyObj('studyGermplasmService', ['replaceStudyGermplasm','getSelectedEntries']);
+	var studyContext = {
+		studyId: 1,
+		cropName: 'maize',
+		measurementDatasetId: 2009
+	};
+
+	var uibModalInstance = {
+		close: jasmine.createSpy('close'),
+		dismiss: jasmine.createSpy('dismiss'),
+		result: {
+			then: jasmine.createSpy('then')
+		}
+	};
+
+	beforeEach(function () {
+		module('manageTrialApp');
+		module(function ($provide) {
+			$provide.value("studyGermplasmService", studyGermplasmService);
+		});
+	});
+
+	beforeEach(function () {
+		inject(function ($injector) {
+			scope = $injector.get('$rootScope').$new();
+			var $controller = $injector.get('$controller');
+			$q = $injector.get('$q');
+			studyGermplasmService = $injector.get('studyGermplasmService');
+			replaceGermplasmCtrl = $controller('replaceGermplasmCtrl', {
+				$scope: scope,
+				$uibModalInstance: uibModalInstance,
+				studyContext: studyContext,
+				studyGermplasmService: studyGermplasmService,
+			});
+
+			spyOn(replaceGermplasmCtrl, 'showAlertMessage');
+			studyGermplasmService.getSelectedEntries.and.returnValue([56]);
+		})
+	});
+
+	describe('Perform Germplasm Replacement', function () {
+		describe('performGermplasmReplacement', function () {
+			it('should not replace germplasm if non-numeric GID', function () {
+				spyOn($.fn, 'val').and.callFake(function() {
+					return 'test';
+				});
+				scope.performGermplasmReplacement();
+				expect(replaceGermplasmCtrl.showAlertMessage).toHaveBeenCalledWith('','Please enter valid GID.');
+				expect(studyGermplasmService.replaceStudyGermplasm).not.toHaveBeenCalled();
+			});
+		});
+
+		describe('performGermplasmReplacement', function () {
+			it('should replace germplasm for valid GID', function () {
+				var response = {data: {}};
+				studyGermplasmService.replaceStudyGermplasm.and.returnValue($q.resolve(response));
+				spyOn($.fn, 'val').and.callFake(function() {
+					return '135';
+				});
+
+				scope.performGermplasmReplacement();
+				expect(replaceGermplasmCtrl.showAlertMessage).not.toHaveBeenCalled();
+				expect(studyGermplasmService.replaceStudyGermplasm).toHaveBeenCalledWith(56,'135');
+			});
+		});
+
+	});
+
+});

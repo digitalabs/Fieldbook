8e9aafad7799c9d8378f5cf32507b5757a02dbec
Darla Hermo <darla@leafnode.io>
2020-07-23 15:13:01 +0800

    Resolve compile error, minor code cleanup
    IBP-3876

--- src/main/java/com/efficio/fieldbook/web/common/controller/GermplasmTreeController.java
+++ src/main/java/com/efficio/fieldbook/web/common/controller/GermplasmTreeController.java
@@ -23,6 +23,7 @@ import com.efficio.fieldbook.web.trial.form.AdvancingStudyForm;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Table;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
@@ -489,7 +490,7 @@ public class GermplasmTreeController extends AbstractBaseFieldbookController {
 		Boolean isTrimed = false;
 		final Integer studyId = this.userSelection.getWorkbook().getStudyDetails().getId();
 		// Take the plot of the female parent as the source observation unit. It is possible for female plot #s to be null if source is from another study
-		final Set<Integer> sourcePlotNumbers = importedGermplasmList.stream().filter(g-> Objects.nonNull(g.getFemalePlotNo())).map(g -> Integer.valueOf(g.getFemalePlotNo())).collect(Collectors.toSet());
+		final Set<Integer> sourcePlotNumbers = importedGermplasmList.stream().filter(g-> Objects.nonNull(g.getFemalePlotNo())).map(ImportedCross::getFemalePlotNo).collect(Collectors.toSet());
 		Table<Integer, Integer, Integer> observationUnitIdsTable = HashBasedTable.create();
 		if (!CollectionUtils.isEmpty(sourcePlotNumbers)) {
 			// Take the observation units from the first trial instance

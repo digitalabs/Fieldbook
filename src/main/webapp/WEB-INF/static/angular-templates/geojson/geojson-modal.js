b518b3f5ce7e6b954e9eeabbaf33e5c97bea67ee
gellisamson <gelli@leafnode.io>
2020-08-11 13:21:46 +0800

    Help link for georeference dialog IBP-3701-IBP-3724-HelpLinks

--- src/main/webapp/WEB-INF/static/angular-templates/geojson/geojson-modal.js
+++ src/main/webapp/WEB-INF/static/angular-templates/geojson/geojson-modal.js
@@ -3,11 +3,22 @@
 
 	const module = angular.module('manageTrialApp');
 
-	module.controller('GeoJSONModalCtrl', ['$scope', 'studyContext', '$uibModalInstance',
-		function ($scope, studyContext, $uibModalInstance) {
+	module.controller('GeoJSONModalCtrl', ['$scope', 'studyContext', '$uibModalInstance', '$http',
+		function ($scope, studyContext, $uibModalInstance, $http) {
 			$scope.geoJSONEditorUrl = '/ibpworkbench/controller/brapi-fieldmap?'
 				+ 'instanceId=' + $scope.$resolve.instanceId
 				+ '&cropName=' + studyContext.cropName;
+
+			$http({
+				method: 'GET',
+				url: '/ibpworkbench/controller/help/getUrl/' + $scope.$resolve.helpModule,
+				responseType: 'text',
+				transformResponse: undefined
+			}).then(function(response){
+				$scope.helpToolUrl = response.data;
+			}, function (error){
+			});
+
 
 			if ($scope.$resolve.isViewGeoJSON) {
 				$scope.geoJSONEditorUrl += '&hasLayout=true';

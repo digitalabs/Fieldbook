b6d2e948df68b9991e1738fd7471b056002fe7d9
Aldrin Batac <aldrin.batac@leafnode.io>
2020-08-25 10:16:58 +0800

    Hide Cancel Pending Transactions button if user has no permission.
    IBP-3945

--- src/main/webapp/WEB-INF/static/js/trialmanager/inventory/inventory-tab.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/inventory/inventory-tab.js
@@ -6,12 +6,14 @@
 	const module = angular.module('manageTrialApp');
 
 	module.controller('InventoryTabCtrl', ['$rootScope', '$scope', '$q', 'DTOptionsBuilder', 'DTColumnBuilder', 'InventoryService', '$compile', '$timeout',
-		'$uibModal', 'studyInstanceService',
+		'$uibModal', 'studyInstanceService', 'HasAnyAuthorityService', 'PERMISSIONS',
 		function (
-			$rootScope, $scope, $q, DTOptionsBuilder, DTColumnBuilder, InventoryService, $compile, $timeout, $uibModal, studyInstanceService,
+			$rootScope, $scope, $q, DTOptionsBuilder, DTColumnBuilder, InventoryService, $compile, $timeout, $uibModal, studyInstanceService, HasAnyAuthorityService, PERMISSIONS
 		) {
 			$scope.nested = {};
 			$scope.nested.dtInstance = {};
+
+			$scope.hasAnyAuthority = HasAnyAuthorityService.hasAnyAuthority;
 
 			const dtOptionsDeferred = $q.defer();
 			$scope.dtOptions = dtOptionsDeferred.promise;

61cb5dec4fd6d26ec53a220b786bc3872de196b7
cuenyad <cuenya.diego@gmail.com>
2020-10-09 14:12:57 -0300

    invoked COP services from BMSAPI.
    Issue: IBP-3797
    Review: none

--- src/main/webapp/WEB-INF/static/js/trialmanager/observations/calculate-cop-service.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/observations/calculate-cop-service.js
@@ -8,32 +8,19 @@
 			var successHandler = serviceUtilities.restSuccessHandler,
 				failureHandler = serviceUtilities.restFailureHandler;
 
+			var BASE_URL = '/bmsapi/crops/' + studyContext.cropName + '/programs/' + studyContext.programId + '/studies/' + studyContext.studyId;
 			var service = {};
 
 			service.exportStudy = function (studyRequest) {
-				const url = 'http://ecs-services-496103597.us-east-1.elb.amazonaws.com/api/study-export/'
-				return $http({
-					method: 'POST',
-					url: url,
-					data: JSON.stringify(studyRequest),
-					headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-					overrideAuthToken: true
-				}).then(successHandler, failureHandler);
+				const url = BASE_URL + `/cop/export`;
+				return $http.post(url, studyRequest)
+					.then(successHandler, failureHandler);
 			};
 
 			service.permissions = function (apiRequest) {
-				const url = 'http://ecs-services-496103597.us-east-1.elb.amazonaws.com/api/brapi-permissions/'
-				return $http({
-					method: 'POST',
-					url: url,
-					data: JSON.stringify(apiRequest),
-					headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-					overrideAuthToken: true
-				}).then(successHandler, failureHandler);
-			};
-
-			service.grantProcessPermissions = function () {
-				return 'brapi-permissions'
+				const url = BASE_URL + `/cop/export/permissions`;
+				return $http.post(url, apiRequest)
+					.then(successHandler, failureHandler);
 			};
 
 			return service;

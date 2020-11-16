3ce307f0221d8a9a4970f7be28c77b3d3b9d2fa4
cuenyad <cuenya.diego@gmail.com>
2020-11-16 17:08:58 -0300

    Removing Deprecate fieldmap print labels.
    Issue: IBP-4039
    Review: none

--- src/test/java/com/efficio/fieldbook/web/data/initializer/FieldMapTrialInstanceInfoTestDataInitializer.java
+++ src/test/java/com/efficio/fieldbook/web/data/initializer/FieldMapTrialInstanceInfoTestDataInitializer.java
@@ -7,20 +7,9 @@ import java.util.List;
 import org.generationcp.middleware.data.initializer.FieldMapLabelTestDataInitializer;
 import org.generationcp.middleware.domain.fieldbook.FieldMapTrialInstanceInfo;
 
-import com.efficio.fieldbook.utils.test.LabelPrintingDataUtil;
-
 public class FieldMapTrialInstanceInfoTestDataInitializer {
 
 	public static final String TRIAL_INSTANCE_NO_STRING = "test1";
-
-	public static List<FieldMapTrialInstanceInfo> createTrialFieldMapList() {
-		final List<FieldMapTrialInstanceInfo> trialFieldMap = new ArrayList<>();
-		final FieldMapTrialInstanceInfo fieldMapInfo1 = LabelPrintingDataUtil.createFieldMapThirdTrialInstanceInfo();
-
-		fieldMapInfo1.setTrialInstanceNo(FieldMapTrialInstanceInfoTestDataInitializer.TRIAL_INSTANCE_NO_STRING);
-		trialFieldMap.add(fieldMapInfo1);
-		return trialFieldMap;
-	}
 
 	public static FieldMapTrialInstanceInfo createFieldMapTrialInstanceInfo() {
 			final FieldMapTrialInstanceInfo instanceInfo = new FieldMapTrialInstanceInfo();

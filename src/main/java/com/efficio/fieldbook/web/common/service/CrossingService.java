c6841a0ff30333fd62e8f3de1217be9c62b08a79
Leonel Kovacic <leonel@leafnode.io>
2021-03-18 12:01:50 -0300

    IBP-4326: code cleanup

--- src/main/java/com/efficio/fieldbook/web/common/service/CrossingService.java
+++ src/main/java/com/efficio/fieldbook/web/common/service/CrossingService.java
@@ -11,15 +11,13 @@ import org.generationcp.middleware.domain.etl.Workbook;
 import org.generationcp.middleware.pojos.Germplasm;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Map;
-
 public interface CrossingService {
 
 	ImportedCrossesList parseFile(MultipartFile file) throws FileParsingException;
 
 	String getCross(Germplasm germplasm, ImportedCross crosses, String separator, String cropName);
 
-	boolean applyCrossSetting(CrossSetting crossSetting, ImportedCrossesList importedCrossesList, Integer userId, Workbook workbook);
+	boolean applyCrossSetting(CrossSetting crossSetting, ImportedCrossesList importedCrossesList, Workbook workbook);
 
 	boolean applyCrossSettingWithNamingRules(CrossSetting crossSetting, ImportedCrossesList importedCrossesList, Integer userId, Workbook
 			workbook);
